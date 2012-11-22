00a92b254ba32e41f2a9158f9bdce1758b550205
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 19:53:33 +0000

    Data access manager interface and implementation. Move PhenotypeArchive to trunk All DAO changes. Phenotype Archive Project (Java/Maven/Spring/Hibernate/MySQL)

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/ProjectDAO.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ *
+ * Phenotyping project data access manager interface.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.util.List;
+
+import uk.ac.ebi.phenotype.pojo.Project;
+
+public interface ProjectDAO {
+
+	/**
+	 * Get all projects in the system
+	 * @return all projects
+	 */
+	public List<Project> getAllProjects();
+
+	/**
+	 * Find an project by its number.
+	 * @param name the project name
+	 * @return the project
+	 */
+	public Project getProjectByName(String name);
+}
