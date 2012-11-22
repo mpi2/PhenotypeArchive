59288551dce917da6ae1d5d99b5817c7c31a8edc
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 19:53:38 +0000

    Data access manager interface and implementation. Move PhenotypeArchive to trunk All DAO changes. Contains mapping to genomic features

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/SequenceRegionDAO.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ *
+ * Sequence region data access manager interface.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.util.HashMap;
+import java.util.List;
+
+import uk.ac.ebi.phenotype.pojo.SequenceRegion;
+
+
+
+public interface SequenceRegionDAO {
+
+	/**
+	 * Get all sequence region
+	 * @return all sequence region
+	 */
+	public List<SequenceRegion> getAllSequenceRegions();
+
+	/**
+	 * Get all sequence region in a map
+	 * @return all sequence region
+	 */
+	public HashMap<String, SequenceRegion> getSequenceRegionsMap();
+
+	/**
+	 * Find a sequence region by its name.
+	 * @param name the sequence region name
+	 * @return the sequence region
+	 */
+	public SequenceRegion getSequenceRegionByName(String name);
+}
