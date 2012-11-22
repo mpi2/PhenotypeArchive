6db5fc0e61f073959a3565ef1ead3304d17e75d1
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 19:52:56 +0000

    Data access manager interface and implementation. add finalize method to clean the session object if still there. Move PhenotypeArchive to trunk All DAO changes. Phenotype Archive Project (Java/Maven/Spring/Hibernate/MySQL)

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/HibernateDAOImpl.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ * 
+ * Parent data access manager implementation.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+/*
+* Implementation of the HibernateDAO interface
+*/
+
+@Transactional
+public class HibernateDAOImpl implements HibernateDAO {
+	
+	/**
+	 * The session factory used to query the database
+	 */
+	protected SessionFactory sessionFactory;
+	
+	/**
+	 * Method to get a connection from the session factory
+	 * This is deprecated and should be replaced!
+	 * @return
+	 */
+	public Connection getConnection() {
+		@SuppressWarnings("deprecation")
+		Session session = SessionFactoryUtils.doGetSession(sessionFactory, false);
+		Connection connection = session.connection(); 
+		return connection;
+	}
+	
+	/**
+	 * Method to get a session from the session factory
+	 * @return
+	 */
+	public Session getSession() {
+		Session session = SessionFactoryUtils.doGetSession(sessionFactory, false);
+		return session;
+	}
+	
+	/**
+	 * @return Returns the sessionFactory.
+	 */
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	public Collection executeNativeQuery(String sql) throws SQLException {
+		
+		LinkedList<Object> results = new LinkedList<Object>();
+		
+		Connection connection = getConnection();
+		Statement stmt = connection.createStatement();
+		ResultSet rs = null;        
+		rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+		  results.add(rs.getString(1));
+		}
+		
+		rs.close();
+		
+		return results;
+	}
+	
+	/**
+	 * Returns the session associated with the ongoing reward transaction.
+	 * @return the transactional session
+	 */
+	protected Session getCurrentSession() {
+		return sessionFactory.getCurrentSession();
+	}	
+
+	protected void finalize() {
+		
+		getCurrentSession().flush();
+		getCurrentSession().clear();
+		getCurrentSession().close();
+		
+	}
+	
+	
+	
+}
