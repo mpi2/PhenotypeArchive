b2b1f27d366b0422ba12cb168a96bb3e86772714
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 19:53:19 +0000

    Data access manager interface and implementation. Move PhenotypeArchive to trunk All DAO changes. Phenotype Archive Project (Java/Maven/Spring/Hibernate/MySQL)

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/OrganisationDAOImpl.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ * 
+ * Organisation (institute, company) data access manager implementation.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import uk.ac.ebi.phenotype.pojo.Organisation;
+
+public class OrganisationDAOImpl extends HibernateDAOImpl implements OrganisationDAO {
+
+	/**
+	 * Creates a new Hibernate organisation data access manager.
+	 * @param sessionFactory the Hibernate session factory
+	 */
+	public OrganisationDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Organisation> getAllOrganisations() {
+		return getCurrentSession().createQuery("from Organisation").list();
+	}
+
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public Organisation getOrganisationByName(String name) {
+		return (Organisation) getCurrentSession().createQuery("from Organisation as o where o.name= ?").setString(0, name).uniqueResult();
+	}
+
+}
