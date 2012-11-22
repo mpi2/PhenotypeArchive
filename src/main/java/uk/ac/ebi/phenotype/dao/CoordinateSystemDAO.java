2816a4175aa92c8f34de0bcaa4063aaf8b32f0ea
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 19:51:24 +0000

    Data access manager interface and implementation. Move PhenotypeArchive to trunk Coordinate system data access object Contains mapping to genomic features

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/CoordinateSystemDAO.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ * 
+ * Coordinate system data access manager interface
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.util.List;
+
+import uk.ac.ebi.phenotype.pojo.CoordinateSystem;
+
+
+
+public interface CoordinateSystemDAO {
+
+	/**
+	 * Get all coordinate systems
+	 * @return all coordinate system
+	 */
+	public List<CoordinateSystem> getAllCoordinateSystems();
+
+	/**
+	 * Find a coordinate system by its name.
+	 * @param name the coordinate system name
+	 * @return the coordinate system
+	 */
+	public CoordinateSystem getCoordinateSystemByName(String name);
+}
