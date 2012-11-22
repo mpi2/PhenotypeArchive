6375ef152e9e9dfd5b3cf41feb6af7b4e78e41ba
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 17:27:40 +0000

    Author and comments added. Move PhenotypeArchive to trunk Copyright notice changed Observation classes added Biological sample and model added.

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/ParameterIncrement.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+/**
+ * 
+ * A concrete representation of phenotype parameter increment.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class ParameterIncrement {
+
+	private String value;
+	private String dataType;
+	private String unit;
+	private String minimum;
+	/**
+	 * 
+	 */
+	public ParameterIncrement() {
+		super();
+	}
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+	/**
+	 * @return the dataType
+	 */
+	public String getDataType() {
+		return dataType;
+	}
+	/**
+	 * @param dataType the dataType to set
+	 */
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+	/**
+	 * @return the unit
+	 */
+	public String getUnit() {
+		return unit;
+	}
+	/**
+	 * @param unit the unit to set
+	 */
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	/**
+	 * @return the minimum
+	 */
+	public String getMinimum() {
+		return minimum;
+	}
+	/**
+	 * @param minimum the minimum to set
+	 */
+	public void setMinimum(String minimum) {
+		this.minimum = minimum;
+	}
+	
+	public String toString() {
+		return "Value: " + value + "; dataType: " + dataType + "; unit: " + unit + "; minimum: " + minimum;
+	}
+	
+}
