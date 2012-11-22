9e233895fcee571b462371294d3666dd380a1a80
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 17:27:40 +0000

    Author and comments added. Move PhenotypeArchive to trunk Copyright notice changed Observation classes added Biological sample and model added.

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/ParameterOption.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+/**
+ * 
+ * A concrete representation of phenotype parameter option.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @version $Revision: 941 $
+ *  @since February 2012
+ */
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class ParameterOption {
+	
+	private String name;
+	private String description;
+	
+	/**
+	 * 
+	 */
+	public ParameterOption() {
+		super();
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
