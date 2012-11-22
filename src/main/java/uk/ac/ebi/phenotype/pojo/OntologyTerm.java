599d915a07a4391aea45d27bf184100bfc7892b9
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 17:27:38 +0000

    Author and comments added. Move PhenotypeArchive to trunk Copyright notice changed Observation classes added Biological sample and model added. added synonyms to an ontology term. Contains mapping to genomic features

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/OntologyTerm.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+/**
+ * 
+ * Represents an ontology term in the database.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since May 2012
+ * @see Synonym
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ontology_term")
+public class OntologyTerm {
+
+	@EmbeddedId
+	@AttributeOverrides({
+	@AttributeOverride(name="accession", 
+					   column=@Column(name="acc")),
+	@AttributeOverride(name="databaseId", 
+	   column=@Column(name="db_id"))
+	})
+	DatasourceEntityId id;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "name")
+	private String name;	
+	
+	@ElementCollection
+	   @CollectionTable(name="synonym", 
+	   					joinColumns= {@JoinColumn(name="acc"),@JoinColumn(name="db_id"),}
+	   )
+	private List<Synonym> synonyms;
+	
+	public OntologyTerm() {
+		super();
+	}
+
+	/**
+	 * @return the id
+	 */
+	public DatasourceEntityId getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(DatasourceEntityId id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the synonyms
+	 */
+	public List<Synonym> getSynonyms() {
+		return synonyms;
+	}
+
+	/**
+	 * @param synonyms the synonyms to set
+	 */
+	public void setSynonyms(List<Synonym> synonyms) {
+		this.synonyms = synonyms;
+	}
+	
+	public void addSynonym(Synonym synonym) {
+		if (this.synonyms == null) {
+			this.synonyms = new LinkedList<Synonym>();
+		}
+	}
+	
+}
