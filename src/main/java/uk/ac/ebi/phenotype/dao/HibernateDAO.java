b72e89ac3ba7cf917a73cb00d76aff5b97548de1
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 19:52:53 +0000

    Data access manager interface and implementation. Move PhenotypeArchive to trunk All DAO changes. Phenotype Archive Project (Java/Maven/Spring/Hibernate/MySQL)

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/HibernateDAO.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ * 
+ * Parent data access manager interface.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import org.hibernate.Session;
+
+/**
+ * Please add a description about HibernateDAO.java
+ */
+
+public interface HibernateDAO {
+
+	public Session getSession();
+	
+	public Collection executeNativeQuery(String sql) throws SQLException;
+	
+}
