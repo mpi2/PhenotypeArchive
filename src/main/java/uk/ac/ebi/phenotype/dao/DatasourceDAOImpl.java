fc14648f968284771be05d3cb2d06b41885067e5
Gautier Koscielny <gkos.ebi@gmail.com>
2012-11-22 19:51:31 +0000

    Data access manager interface and implementation. Move PhenotypeArchive to trunk All DAO changes. Datasource concept to represent external resources we get information from.

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/DatasourceDAOImpl.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ * 
+ * External data source access manager implementation.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import uk.ac.ebi.phenotype.pojo.Datasource;
+
+
+
+public class DatasourceDAOImpl extends HibernateDAOImpl implements
+		DatasourceDAO {
+
+	public DatasourceDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Datasource> getAllDatasources() {
+		return getCurrentSession().createQuery("from Datasource").list();
+	}
+
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public Datasource getDatasourceByName(String name) {
+		return (Datasource) getCurrentSession().createQuery("from Datasource as d where d.name= ?").setString(0, name).uniqueResult();
+		
+	}
+
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public Datasource getDatasourceByShortName(String shortName) {
+		return (Datasource) getCurrentSession().createQuery("from Datasource as d where d.shortName= ?").setString(0, shortName).uniqueResult();
+		
+	}
+}
