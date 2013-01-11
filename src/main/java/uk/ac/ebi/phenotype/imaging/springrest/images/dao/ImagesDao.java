a99b4768df7c4f36adbf0ecbb28e81405a2745fb
Gautier Koscielny <gkos.ebi@gmail.com>
2013-01-11 17:46:44 +0000

    Change copyright in Apache 2 license to 2013 documented documented images rest service returns one imagerecord if id requested , id removed from url make ImaImageRecord queryDatabaseById(String id)  method public for use in testing imaging java classes added

---
+++ src/main/java/uk/ac/ebi/phenotype/imaging/springrest/images/dao/ImagesDao.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.imaging.springrest.images.dao;
+
+import java.util.List;
+
+import uk.ac.ebi.phenotype.imaging.persistence.ImaImageRecord;
+import uk.ac.ebi.phenotype.imaging.springrest.images.Image;
+import uk.ac.ebi.phenotype.imaging.springrest.images.Images;
+/**
+ * Images dao interface used by currently hibernate and our images restful web service
+ * @author jwarren
+ *
+ */
+public interface ImagesDao {
+	
+	public Images getAllImages(int start,int length, String search);
+
+	public ImaImageRecord getImageWithId(int id);
+	
+	public long getTotalNumberOfImages();
+
+	public ImaImageRecord queryDatabaseById(String id);
+}
