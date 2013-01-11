a9c39a477f2dcfe79123ca2d6850836f62ede23c
Gautier Koscielny <gkos.ebi@gmail.com>
2013-01-11 17:46:37 +0000

    EMBL EBI Apache 2 license added imaging java classes added

---
+++ src/main/java/uk/ac/ebi/phenotype/imaging/persistence/ImaImportContext.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.imaging.persistence;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+
+/**
+ * The persistent class for the IMA_IMPORT_CONTEXT database table.
+ * 
+ */
+@Entity
+@Table(name="IMA_IMPORT_CONTEXT")
+public class ImaImportContext implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	private int id;
+	private String name;
+
+    public ImaImportContext() {
+    }
+
+    @JsonIgnore
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
