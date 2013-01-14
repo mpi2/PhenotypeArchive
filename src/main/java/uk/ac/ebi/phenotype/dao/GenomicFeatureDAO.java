78d5fa2a9b98928edaccc1ceb71ce5dc1ae54186
Gautier Koscielny <gkos.ebi@gmail.com>
2013-01-14 09:58:45 +0000

    2013 licence added

--- src/main/java/uk/ac/ebi/phenotype/dao/GenomicFeatureDAO.java
+++ src/main/java/uk/ac/ebi/phenotype/dao/GenomicFeatureDAO.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License.  
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 
 import uk.ac.ebi.phenotype.bean.GenomicFeatureBean;
-import uk.ac.ebi.phenotype.pojo.CoordinateSystem;
 import uk.ac.ebi.phenotype.pojo.GenomicFeature;
 
 public interface GenomicFeatureDAO extends HibernateDAO {
