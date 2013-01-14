3913cb8a993db8ca6d025a2f274efb15b9718fbb
Gautier Koscielny <gkos.ebi@gmail.com>
2013-01-14 15:00:02 +0000

    add licence

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -1,3 +1,19 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 -- KOMP2 database definition
 
 ALTER DATABASE komp2 CHARACTER SET utf8 COLLATE utf8_general_ci;
@@ -71,9 +87,9 @@ CREATE TABLE project (
 CREATE TABLE organisation (
 
     id                          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	name                        VARCHAR(255) NOT NULL DEFAULT '',
-	fullname                    VARCHAR(255) NOT NULL DEFAULT '',
-	country                     VARCHAR(50),
+    name                        VARCHAR(255) NOT NULL DEFAULT '',
+    fullname                    VARCHAR(255) NOT NULL DEFAULT '',
+    country                     VARCHAR(50),
 
     PRIMARY KEY (id),
     UNIQUE KEY name_idx (name)
@@ -96,10 +112,10 @@ CREATE TABLE participant (
  */
 CREATE TABLE ilar (
 
-	labcode 					VARCHAR(50) NOT NULL,
-	status 						ENUM('active', 'pending', 'retired'),
-	investigator 				VARCHAR(255) NOT NULL DEFAULT '',
-	organisation 				VARCHAR(255) NOT NULL DEFAULT '',
+    labcode                     VARCHAR(50) NOT NULL,
+    status                      ENUM('active', 'pending', 'retired'),
+    investigator                VARCHAR(255) NOT NULL DEFAULT '',
+    organisation                VARCHAR(255) NOT NULL DEFAULT '',
 	
 	PRIMARY KEY (labcode)
 	
