564cd6890febb990961af8cea2ea364a791c9f79
Gautier Koscielny <gkos.ebi@gmail.com>
2013-01-14 09:58:35 +0000

    2013 licence added

--- src/main/java/uk/ac/ebi/phenotype/pojo/ParameterOption.java
+++ src/main/java/uk/ac/ebi/phenotype/pojo/ParameterOption.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2011-2012 EMBL - European Bioinformatics Institute
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License.  
@@ -20,7 +20,7 @@ package uk.ac.ebi.phenotype.pojo;
  * A concrete representation of phenotype parameter option.
  * 
  * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
- * @version $Revision: 941 $
+ * @version $Revision: 1291 $
  *  @since February 2012
  */
 
