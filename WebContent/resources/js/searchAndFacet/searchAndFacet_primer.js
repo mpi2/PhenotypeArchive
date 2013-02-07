992789fe4eec4ec78b14968a90307b6fc25762a3
Gautier Koscielny <gkos.ebi@gmail.com>
2013-02-07 15:40:19 +0000

    commented out hiding facet count

--- WebContent/resources/js/searchAndFacet/searchAndFacet_primer.js
+++ WebContent/resources/js/searchAndFacet/searchAndFacet_primer.js
@@ -21,63 +21,24 @@ $(document).ready(function(){
 	'use strict';	
 
 	$('span.facetCount').text(''); // default when page loads
+
+	var pathname = MPI2.searchAndFacetConfig.pathname;
 	
-	// custom 404 page does not know about baseUrl
-	var path = window.location.pathname.replace(/^\//,"");
-	path = '/' + path.substring(0, path.indexOf('/'));
-	var trailingPath = '/search';
-	var trailingPathDataTable = '/dataTable';
-	
-	var pathname = typeof baseUrl == 'undefined' ? path + trailingPath : baseUrl + trailingPath;
-	var dataTablePath = typeof baseUrl == 'undefined' ? path + trailingPathDataTable : baseUrl + trailingPathDataTable;
-	
+	//console.log('start at primer js href: ' + window.location.href);
+	//console.log('start at primer js hash: ' + window.location.hash);
+		
 	// auto-complete is always created
 	// load total facetCount of all data types by default when searchAndFacet page loads
 	window.jQuery('input#userInput').mpi2AutoComplete({
 		
+			solrBaseURL_bytemark: MPI2.searchAndFacetConfig.solrBaseURL_bytemark,			
+			solrBaseURL_ebi: MPI2.searchAndFacetConfig.solrBaseURL_ebi,
 			search_pathname: pathname,			        
-			loadDataTable: function(event, data){				
-				// calls Sanger search grid
-				
-				// do not use qf: 'auto_suggest', defType: 'edismax' for gene, as the config is different at Sanger				
-				var facetParams = MPI2.searchAndFacetConfig.facetParams;
-								
-				if ( facetParams[data.type] && data.type != 'gene'){
-					data.fq = facetParams[data.type].fq;
-					data.qf = facetParams[data.type].qf;
-					data.defType = facetParams[data.type].defType;
-					data.wt = facetParams[data.type].wt;
-				}
 			
-			    //var solrParams = data.explaination ? {q:data.queryString, explaination: data.explaination} : {q:data.queryString};
-				if ( data.type != 'undefined' ){
-					// invoke sanger grid
-					//window.jQuery('#mpi2-search').trigger('search', [{type: data.type, solrParams: data}]);
-					
-					// invoke dataTable
-					//console.log(data);
-					
-					var oInfos = {};
-			 		oInfos.params = $.fn.stringifyJsonAsUrlParams(data);
-			 		
-			 		oInfos.solrCoreName = facetParams[data.type].solrCoreName;
-			 		oInfos.mode = facetParams[data.type].solrCoreName + 'Grid';
-			 		oInfos.dataTablePath = dataTablePath;
-			 		
-			 		var dTable = $.fn.fetchEmptyTable(facetParams[data.type].tableHeader, facetParams[data.type].cols, oInfos.mode);
-			 	   			 	    	
-			 	   	var title = $.fn.upperCaseFirstLetter(facetParams[data.type].type);	    	
-			 	   	var gridTitle = $('<div></div>').attr({'class':'gridTitle'}).html(title);
-			 	 
-			 	   	$('div#mpi2-search').html('');		 	  
-			 	   	$('div#mpi2-search').append(gridTitle, dTable);		 	    	   	
-			 	   	
-			 	   	$.fn.invokeDataTable(oInfos); 	
-			 	   
-				}			   	
-			},
-			loadSideBar: function(event, data){
-				// calls left side bar widget				
+			loadSideBar: function(event, data){					
+				//console.log('loadSideBar: q: '+ data.q + ' core: ' + data.core + ' fq: ' + data.fq);
+				// calls left side bar widget	
+						
 				window.jQuery('div#leftSideBar').mpi2LeftSideBar({  
 					data: data, // key q: query string
 			        geneGridElem: 'div#mpi2-search'			                                      
@@ -85,19 +46,20 @@ $(document).ready(function(){
 			},
 			redirectedSearch: function(event, data){				
 				// Make a hidden form on your page, and submit it here.
-			    var form = "<form id='hiddenSrch' action='" + pathname + "' method='post'>"
-			    		 + "<input type='text' name='type' value='" + data.type + "'>"
-			             + "<input type='text' name='geneFound' value=" + data.geneFound + ">"			                                
-			             + "<input type='text' name='queryString' value='" + data.q + "'>"
+					
+			    var form = "<form id='hiddenSrch' action='" + pathname + "' method='get'>"				
+			    		 + "<input type='text' name='core' value='" + data.core + "'>"
+			             + "<input type='text' name='fq' value=" + encodeURI(data.fq) + ">"			                                
+			             + "<input type='text' name='q' value='" + data.q + "'>"
 			             + "</form>";                     
 
 			    window.jQuery('div#bannerSearch').append(form);
-			    window.jQuery('form#hiddenSrch').hide().submit();                    
-			    } 
+			    window.jQuery('form#hiddenSrch').hide().submit();			    
+		   } 
 		}).click(function(){
 				window.jQuery(this).val('');
-				$('span.facetCount').text(''); 
-	}); 	
+				//$('span.facetCount').text('');			
+	}); 
 	
 	// dynamically readjusted position of autosuggest dropdown list due to elastic design of page
 	window.jQuery(window).resize(function(){
