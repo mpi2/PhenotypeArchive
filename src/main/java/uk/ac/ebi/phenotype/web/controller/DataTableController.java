5e5ebed69958dddb790e680c53c31427dd38410d
Gautier Koscielny <gkos.ebi@gmail.com>
2013-02-07 15:40:48 +0000

    apply format and cleanup

--- src/main/java/uk/ac/ebi/phenotype/web/controller/DataTableController.java
+++ src/main/java/uk/ac/ebi/phenotype/web/controller/DataTableController.java
@@ -35,96 +35,85 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import uk.ac.ebi.generic.util.SolrIndex;
 
-
 @Controller
 public class DataTableController implements BeanFactoryAware {
 
 	private Logger log = Logger.getLogger(this.getClass().getCanonicalName());
 	private BeanFactory bf;
 
-	//@Autowired
-	/*public FileExportController(PhenotypeCallSummaryDAO phenotypeCallSummaryDAO) { 
-		this.phenotypeCallSummaryDAO = phenotypeCallSummaryDAO;	
-	}*/
+	// @Autowired
+	/*
+	 * public FileExportController(PhenotypeCallSummaryDAO
+	 * phenotypeCallSummaryDAO) { this.phenotypeCallSummaryDAO =
+	 * phenotypeCallSummaryDAO; }
+	 */
 
 	/**
-	 * <p>Return jQuery dataTable from server-side for lazy-loading.</p>
-	 * @param  
-		
-	bRegex=false
-	bRegex_0=false
-	bRegex_1=false
-	bRegex_2=false
-	
-	bSearchable_0=true
-	bSearchable_1=true
-	bSearchable_2=true
-	
-	bSortable_0=true
-	bSortable_1=true
-	bSortable_2=true
-	
-	iColumns=3
-	
-	* for paging: 
-		iDisplayLength=10
-		iDisplayStart=0
-		
-	* for sorting:	
-		iSortCol_0=0
-		iSortingCols=1
-		
-	* for filtering:
-		sSearch=
-		sSearch_0=
-		sSearch_1=
-		sSearch_2=
-			
-	mDataProp_0=0
-	mDataProp_1=1
-	mDataProp_2=2
-	
-	sColumns=
-	sEcho=1
-			
-	sSortDir_0=asc
-	 * @return 
-	* @return
-	*/
-	
-	@RequestMapping(value="/dataTable", method=RequestMethod.GET)	
-	public @ResponseBody String dataTableJson(		
-			@RequestParam(value="iDisplayStart", required=true) int iDisplayStart,
-			@RequestParam(value="iDisplayLength", required=true) int iDisplayLength,	
-			@RequestParam(value="solrParams", required=true) String solrParams,			
-			HttpServletRequest request, 
-			Model model){
-		
-			log.debug("CHK: " + solrParams);
-			
-			JSONObject jParams = (JSONObject) JSONSerializer.toJSON(solrParams);
-			log.debug("solr: " + jParams.getString("params"));
-			
-			String contextPath = (String) request.getAttribute("baseUrl");						
-			String solrCoreName = jParams.getString("solrCoreName");
-			String query = "";				
-			String mode = jParams.getString("mode");
-			String solrParamStr = jParams.getString("params");
+	 * <p>
+	 * Return jQuery dataTable from server-side for lazy-loading.
+	 * </p>
+	 * 
+	 * @param bRegex
+	 *            =false bRegex_0=false bRegex_1=false bRegex_2=false
+	 * 
+	 *            bSearchable_0=true bSearchable_1=true bSearchable_2=true
+	 * 
+	 *            bSortable_0=true bSortable_1=true bSortable_2=true
+	 * 
+	 *            iColumns=3
+	 * 
+	 *            for paging: iDisplayLength=10 iDisplayStart=0
+	 * 
+	 *            for sorting: iSortCol_0=0 iSortingCols=1
+	 * 
+	 *            for filtering: sSearch= sSearch_0= sSearch_1= sSearch_2=
+	 * 
+	 *            mDataProp_0=0 mDataProp_1=1 mDataProp_2=2
+	 * 
+	 *            sColumns= sEcho=1
+	 * 
+	 *            sSortDir_0=asc
+	 * @return
+	 * @return
+	 */
 
-			boolean showImgView = false;
-			if(jParams.containsKey("showImgView")) {
-				showImgView = jParams.getBoolean("showImgView");
-			}
-			
-			Map config = (Map) bf.getBean("globalConfiguration");
-			SolrIndex solrIndex = new SolrIndex(query, solrCoreName, solrParamStr, mode, iDisplayStart, iDisplayLength, showImgView, request, config);			
-			String jsonStr = solrIndex.fetchDataTableJson(contextPath);
-			
-			return jsonStr; 
-	}		
+	@RequestMapping(value = "/dataTable", method = RequestMethod.GET)
+	public @ResponseBody
+	String dataTableJson(
+			@RequestParam(value = "iDisplayStart", required = false) int iDisplayStart,
+			@RequestParam(value = "iDisplayLength", required = false) int iDisplayLength,
+			@RequestParam(value = "solrParams", required = false) String solrParams,
+			HttpServletRequest request, Model model) {
+
+		log.debug("CHK: " + solrParams);
+
+		JSONObject jParams = (JSONObject) JSONSerializer.toJSON(solrParams);
+		log.debug("solr: " + jParams.getString("params"));
+
+		String contextPath = (String) request.getAttribute("baseUrl");
+
+		String solrCoreName = jParams.getString("solrCoreName");
+		String query = "";
+		String mode = jParams.getString("mode");
+		String solrParamStr = jParams.getString("params");
+
+		boolean showImgView = false;
+		if (jParams.containsKey("showImgView")) {
+			showImgView = jParams.getBoolean("showImgView");
+		}
+
+		Map config = (Map) bf.getBean("globalConfiguration");
+		SolrIndex solrIndex = new SolrIndex(query, solrCoreName, solrParamStr,
+				mode, iDisplayStart, iDisplayLength, showImgView, request,
+				config);
+		String jsonStr = solrIndex.fetchDataTableJson(contextPath);
+
+		return jsonStr;
+	}
+
 	@Override
 	public void setBeanFactory(BeanFactory arg0) throws BeansException {
-		this.bf=arg0;
-		
+		this.bf = arg0;
+
 	}
 }
