4bf6f710d36b9781c82954d8a37e213a64d4361b
Gautier Koscielny <gkos.ebi@gmail.com>
2013-02-07 15:40:31 +0000

    Type of annotations provided

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/PhenotypeAnnotationType.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+/**
+ * 
+ * Type of annotation represented in the database.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since January 2013
+ * @see PhenotypeAnnotation
+ */
+
+public enum PhenotypeAnnotationType {
+	
+	abnormal, abnormal_specific, increased, decreased, inferred, trait;
+
+	public static final PhenotypeAnnotationType find(String type) {
+		String lcType = type.toLowerCase();
+		if (type.equals("abnormal")) {
+			return PhenotypeAnnotationType.abnormal;
+		} else if (type.equals("abnormal_specific")) {
+			return PhenotypeAnnotationType.abnormal_specific;
+		} else if (type.equals("increased")) {
+			return PhenotypeAnnotationType.increased;
+		} else if (type.equals("decreased")) {
+			return PhenotypeAnnotationType.decreased;
+		} else if (type.equals("inferred")) {
+			return PhenotypeAnnotationType.inferred;
+		} else if (type.equals("trait")) {
+			return PhenotypeAnnotationType.trait;
+		}
+		return PhenotypeAnnotationType.abnormal;
+	}
+}
