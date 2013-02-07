515b84771fa86d509d491776096ba3cfae6d2362
Gautier Koscielny <gkos.ebi@gmail.com>
2013-02-07 15:40:50 +0000

    added the type in the redirect model view (gene)

--- src/main/java/uk/ac/ebi/phenotype/web/controller/GenesController.java
+++ src/main/java/uk/ac/ebi/phenotype/web/controller/GenesController.java
@@ -17,7 +17,10 @@ package uk.ac.ebi.phenotype.web.controller;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,12 +30,12 @@ import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.FacetField.Count;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
@@ -41,16 +44,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
+import uk.ac.ebi.generic.util.RegisterInterestDrupalSolr;
 import uk.ac.ebi.generic.util.SolrIndex;
+import uk.ac.ebi.phenotype.dao.GeneDao;
 import uk.ac.ebi.phenotype.dao.GenomicFeatureDAO;
 import uk.ac.ebi.phenotype.dao.PhenotypeCallSummaryDAO;
+import uk.ac.ebi.phenotype.error.GenomicFeatureNotFoundException;
 import uk.ac.ebi.phenotype.imaging.springrest.images.dao.ImagesSolrDao;
 import uk.ac.ebi.phenotype.pojo.GenomicFeature;
 import uk.ac.ebi.phenotype.pojo.PhenotypeCallSummary;
 import uk.ac.ebi.phenotype.web.pojo.PhenotypeRow;
-import uk.ac.ebi.phenotype.web.util.DrupalHttpProxy;
+import uk.ac.ebi.phenotype.web.util.HttpProxy;
 
 
 @Controller
@@ -59,6 +69,11 @@ public class GenesController implements BeanFactoryAware {
 	private final Logger log = LoggerFactory.getLogger(GenesController.class);
 	private BeanFactory bf;
 
+	RegisterInterestDrupalSolr registerInterest;
+	
+	@Autowired
+	private GeneDao geneBiomart;
+	
 	@Autowired
 	private GenomicFeatureDAO genesDao;
 
@@ -82,11 +97,34 @@ public class GenesController implements BeanFactoryAware {
 			@PathVariable String acc, 
 			Model model,
 			HttpServletRequest request,
-			RedirectAttributes attributes) {
+			RedirectAttributes attributes) throws KeyManagementException, NoSuchAlgorithmException, URISyntaxException, GenomicFeatureNotFoundException {
 
 		// Get the global application configuration
 		@SuppressWarnings("unchecked")
 		Map<String,String> config = (Map<String,String>) bf.getBean("globalConfiguration");
+		
+		// see if the gene exists first:
+		GenomicFeature gene = genesDao.getGenomicFeatureByAccession(acc);
+		if (gene == null) {
+			throw new GenomicFeatureNotFoundException("Gene " + acc + " can't be found.", acc);
+		}
+		
+		//get status from biomart- do we want a gene object to hold info such as status? at the moment we are using GenomicFeature..?
+		String geneStatus = null;
+		
+		try {
+			geneStatus = geneBiomart.getGeneStatus(acc);
+		} catch (IndexOutOfBoundsException exception) {
+			throw new GenomicFeatureNotFoundException("Gene " + acc + " can't be found.", acc);
+		}
+		
+		model.addAttribute("geneStatus", geneStatus);
+		System.out.println("geneStatus="+geneStatus);
+		
+		//code for assessing if the person is logged in and if so have they registered interest in this gene or not?
+		registerInterest=new RegisterInterestDrupalSolr( config, request);
+		//boolean  alreadyInterested=registerInterest.alreadyInterested(acc);
+		this.registerInterestState(acc, model);
 
 		getExperimentalImages(acc, model);
 		getExpressionImages(acc, model);
@@ -120,41 +158,40 @@ public class GenesController implements BeanFactoryAware {
 		}
 		model.addAttribute("phenotypes", new ArrayList<PhenotypeRow>(phenotypes.keySet()));
 
-		GenomicFeature gene = genesDao.getGenomicFeatureByAccession(acc);
 		model.addAttribute("gene", gene);
 		model.addAttribute("request", request);
 		model.addAttribute("acc", acc);
 
 		// crappy code to shoehorn the QC panel
-		DrupalHttpProxy proxy = new DrupalHttpProxy(request);
+		HttpProxy proxy = new HttpProxy();
 		// uncomment if you want to test locally
 		//proxy.setDebugSession("SSESSfbfcc940c73e911682a51bb9f1c59a76=y1tfAywDbKYLw_ROFZX6ysb0H9V3yKk7M9E2rfigei0");
 		URL url;
 		try {
 			String drupalBaseUrl = (String) request.getAttribute("drupalBaseUrl");
 			url = new URL(drupalBaseUrl + "/phenotypes/" + acc);
-			String drupalContent = proxy.getContent(url);
-			model.addAttribute("bPreQC", new Boolean(drupalContent.contains("qc?")));
-			if (drupalContent.contains("qc?")) {
-				int beginIndex = drupalContent.indexOf("/qc?");
-				String sub1 = drupalContent.substring(beginIndex);
+			String content = proxy.getContent(url);
+			model.addAttribute("bPreQC", new Boolean(content.contains("qc?")));
+			if (content.contains("qc?")) {
+				int beginIndex = content.indexOf("/qc?");
+				String sub1 = content.substring(beginIndex);
 				int endIndex = sub1.indexOf("'");
 				String sub2 = sub1.substring(0, endIndex);
 				model.addAttribute("qcLink", sub2);
 			}
-			model.addAttribute("bSangerLegacy", new Boolean(drupalContent.contains("sanger")));
-			if (drupalContent.contains("sanger")) {
-				int beginIndex = drupalContent.indexOf("http://www.sanger");
-				String sub1 = drupalContent.substring(beginIndex);
+			model.addAttribute("bSangerLegacy", new Boolean(content.contains("sanger")));
+			if (content.contains("sanger")) {
+				int beginIndex = content.indexOf("http://www.sanger");
+				String sub1 = content.substring(beginIndex);
 				int endIndex = sub1.indexOf("'");
 				String sub2 = sub1.substring(0, endIndex);
 				model.addAttribute("sangerLegacyLink", sub2);
 			}
-			model.addAttribute("bEurophenomeLegacy", new Boolean(drupalContent.contains("europhenome")));
-			if (drupalContent.contains("europhenome")) {
+			model.addAttribute("bEurophenomeLegacy", new Boolean(content.contains("europhenome")));
+			if (content.contains("europhenome")) {
 				// http://www.europhenome.org/databrowser/viewer.jsp?set=true&m=true&l=10035
-				int beginIndex = drupalContent.indexOf("http://www.europhenome.org");
-				String sub1 = drupalContent.substring(beginIndex);
+				int beginIndex = content.indexOf("http://www.europhenome.org");
+				String sub1 = content.substring(beginIndex);
 				int endIndex = sub1.indexOf("'");
 				String sub2 = sub1.substring(0, endIndex);
 				model.addAttribute("europhenomeLegacyLink", sub2);
@@ -186,6 +223,10 @@ public class GenesController implements BeanFactoryAware {
 	private void getExpressionImages(String acc, Model model) {
 		log.info("hi there: 1");
 		QueryResponse solrExpressionR = imagesSolrDao.getExpressionFacetForGeneAccession(acc);
+		if(solrExpressionR==null){
+			System.err.println("no response from solr data source for acc="+acc);
+			return;
+		}
 		log.info("hi there: 2");
 		log.info("solrEpressionR: "+solrExpressionR);
 		List<FacetField> expressionfacets = solrExpressionR.getFacetFields();
@@ -210,7 +251,10 @@ public class GenesController implements BeanFactoryAware {
 
 	private void getExperimentalImages(String acc, Model model) {
 		QueryResponse solrR = imagesSolrDao.getExperimentalFacetForGeneAccession(acc);
-		
+		if(solrR==null){
+			System.err.println("no response from solr data source for acc="+acc);
+			return;
+		}
 		List<FacetField> facets = solrR.getFacetFields();
 		Map<String, SolrDocumentList> facetToDocs = new HashMap<String, SolrDocumentList>();
 		List<Count> filteredCounts=new ArrayList<Count>();
@@ -250,4 +294,75 @@ public class GenesController implements BeanFactoryAware {
 	public void setBeanFactory(BeanFactory arg0) throws BeansException {
 		this.bf=arg0;
 	}
+	
+	
+	private void registerInterestState(String acc, Model model){
+		String registerInterestButtonString="";
+		String registerButtonAnchor="";
+		String id=acc;
+		if (registerInterest.loggedIn()) {
+			if (registerInterest.alreadyInterested(acc)) {
+				//System.out.println("<a id='"+acc+"' href='' class='btn primary interest'>Unregister interest</a>");
+				registerInterestButtonString= "Unregister interest";
+				id=acc;
+			} else {
+				//System.out.println("<a id='"+acc+"' href='' class='btn primary interest'>Register interest</a>");
+				registerInterestButtonString= "Register interest";
+				id=acc;
+			}
+		} else {
+			//System.out.println("<a href='/user/register' class='btn primary'>Login to register interest</a>");
+			registerInterestButtonString= "Login to register interest";
+			registerButtonAnchor="/user/register";
+			
+		}
+		
+		
+		model.addAttribute("registerInterestButtonString", registerInterestButtonString);
+		
+		model.addAttribute("registerButtonAnchor", registerButtonAnchor);
+		
+	
+			model.addAttribute("registerButtonId",id);
+	
+		
+
+	}
+	
+	@ExceptionHandler(GenomicFeatureNotFoundException.class)
+	public ModelAndView handleGenomicFeatureNotFoundException(GenomicFeatureNotFoundException exception) {
+        ModelAndView mv = new ModelAndView("identifierError");
+        mv.addObject("errorMessage",exception.getMessage());
+        mv.addObject("acc",exception.getAcc());
+        mv.addObject("type","MGI gene");
+        mv.addObject("exampleURI", "/genes/MGI:104874");
+        return mv;
+    } 
+	
+	@RequestMapping("/identifierError")
+	public String identifierError(
+			@PathVariable String acc, 
+			Model model,
+			HttpServletRequest request,
+			RedirectAttributes attributes) {
+		return "identifierError";
+	}
+	
+/*	@ExceptionHandler(GenomicFeatureNotFoundException.class)
+	public RedirectView handleGenomicFeatureNotFoundException(GenomicFeatureNotFoundException ex, HttpServletRequest request) throws IOException
+	{
+		RedirectView redirectView = new RedirectView("identifierError");
+		redirectView.addStaticAttribute("errorMessage", ex.getMessage());
+		return redirectView;
+	}
+
+	@RequestMapping(value = "/identifierError")
+	public String errorRedirectPage(HttpServletRequest request, Model model, @RequestParam("errorMessage") String errorMessage)
+	{
+		model.addAttribute("errorMessage", errorMessage);
+		return "identifierError";
+	}
+*/
+	
+	
 }
