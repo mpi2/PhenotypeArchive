705cc861ce3ee3faff922b7ad6eebb4f7052f1e2
Gautier Koscielny <gkos.ebi@gmail.com>
2013-03-12 18:29:10 +0000

    time series graph related objects added

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/TimeSeriesStatisticsDAO.java
@@ -0,0 +1,41 @@
+package uk.ac.ebi.phenotype.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+
+import uk.ac.ebi.phenotype.pojo.BiologicalModel;
+import uk.ac.ebi.phenotype.pojo.CategoricalResult;
+import uk.ac.ebi.phenotype.pojo.Organisation;
+import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.SexType;
+import uk.ac.ebi.phenotype.pojo.TimeSeriesControlView;
+import uk.ac.ebi.phenotype.pojo.ZygosityType;
+
+public interface TimeSeriesStatisticsDAO {
+
+	public List<DiscreteTimePoint> countControl(SexType sex, Parameter parameter, Integer populationId);
+	public List<DiscreteTimePoint> countMutant(SexType sex, ZygosityType zygosity, Parameter parameter, Integer populationId);
+	
+	public BiologicalModel getControlBiologicalModelByPopulation(Integer populationId);
+	public BiologicalModel getMutantBiologicalModelByPopulation(Integer populationId);
+
+	public List<BiologicalModel> getBiologicalModelsByParameter(Parameter parameter);
+	public List<BiologicalModel> getBiologicalModelsByParameterAndGene(Parameter parameter, String accessionId);
+
+	public Double getpValueByParameterAndBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	public Double getMaxEffectSizeByParameterAndBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	
+	public Organisation getOrganisationByPopulation(Integer populationId);
+	public List<Integer> getPopulationIdsByParameter(Parameter parameter);
+	public List<Integer> getPopulationIdsByParameterAndMutantBiologicalModel(Parameter parameter, BiologicalModel biologicalModel);
+
+	public void saveCategoricalResult(CategoricalResult result);
+
+	public SexType getSexByPopulation(Integer populationId);
+	public List<ZygosityType> getZygositiesByPopulation(Integer populationId);
+
+	public void deleteCategoricalResultByParameter(Parameter parameter) throws HibernateException, SQLException;
+	
+}
