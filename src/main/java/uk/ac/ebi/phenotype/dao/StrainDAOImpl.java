d52e5c7ea0250be8fd19d5164e2d2887c21782ef
Gautier Koscielny <gkos.ebi@gmail.com>
2013-04-02 16:29:15 +0100

    beta release 0.1.7 Change copyright in Apache 2 license to 2013 adjust the warning suppression as appropriate to get rid of warnings; Remove unused imports Data access manager interface and implementation. Move PhenotypeArchive to trunk All DAO changes. Contains mapping to genomic features

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/StrainDAOImpl.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ *
+ * Strain data access manager implementation.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import uk.ac.ebi.phenotype.pojo.Strain;
+
+
+
+public class StrainDAOImpl extends HibernateDAOImpl implements
+StrainDAO {
+
+	public StrainDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<Strain> getAllStrains() {
+		return getCurrentSession().createQuery("from Strain").list();
+	}
+
+	@Transactional(readOnly = true)
+	public Strain getStrainByName(String name) {
+		return (Strain) getCurrentSession().createQuery("from Strain as s where s.name= ?").setString(0, name).uniqueResult();
+	}
+
+	@Transactional(readOnly = true)
+	public Strain getStrainBySynonym(String name) {
+		// select mother from Cat as mother, Cat as kit
+		//where kit in elements(foo.kittens)
+		//return (Strain) getCurrentSession().createQuery("select strain from Strain as strain, Synonym as synonym where synonym in elements(strain.synonyms) and synonym.symbol = ?").setString(0, name).uniqueResult();
+		return (Strain) getCurrentSession().createQuery("from Strain as strain left join strain.synonyms as synonym where synonym.symbol = ?").setString(0, name).uniqueResult();
+	}
+
+	@Transactional(readOnly = false)
+	public void saveStrain(Strain strain) {
+		getCurrentSession().saveOrUpdate(strain);
+
+	}
+
+}
