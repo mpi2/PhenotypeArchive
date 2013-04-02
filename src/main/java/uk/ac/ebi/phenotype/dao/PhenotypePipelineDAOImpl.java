3d70f7bc2b06625c7b23eb09b3c8cb298275ec45
Gautier Koscielny <gkos.ebi@gmail.com>
2013-04-02 16:29:09 +0100

    beta release 0.1.7 deleteAllPipelinesByDatasource added to delete phenotype protocols using a native query. DAO updated for phenotype protocols Change copyright in Apache 2 license to 2013 adjust the warning suppression as appropriate to get rid of warnings; Remove unused imports Data access manager interface and implementation. Move PhenotypeArchive to trunk All DAO changes.

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/PhenotypePipelineDAOImpl.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ * 
+ * Phenotype pipeline data access manager implementation.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since May 2012
+ */
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import uk.ac.ebi.phenotype.pojo.Datasource;
+import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.ParameterIncrement;
+import uk.ac.ebi.phenotype.pojo.ParameterOntologyAnnotation;
+import uk.ac.ebi.phenotype.pojo.ParameterOption;
+import uk.ac.ebi.phenotype.pojo.Pipeline;
+import uk.ac.ebi.phenotype.pojo.Procedure;
+
+
+
+public class PhenotypePipelineDAOImpl extends HibernateDAOImpl implements PhenotypePipelineDAO {
+
+	/**
+	 * Creates a new Hibernate pipeline data access manager.
+	 * @param sessionFactory the Hibernate session factory
+	 */
+	public PhenotypePipelineDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Pipeline> getAllPhenotypePipelines() {
+		List<Pipeline> pipelines = getCurrentSession().createQuery("from Pipeline").list();
+		return pipelines;
+	}
+
+	@Transactional(readOnly = true)
+	public Pipeline getPhenotypePipelineByStableId(String stableId) {
+		return (Pipeline) getCurrentSession().createQuery("from Pipeline as p where p.stableId = ?").setString(0, stableId).uniqueResult();
+	}
+
+	@Transactional(readOnly = true)
+	public Pipeline getPhenotypePipelineByStableIdAndVersion(String stableId, int majorVersion, int minorVersion) {
+		return (Pipeline) getCurrentSession().createQuery("from Pipeline as p where p.stableId = ? and p.majorVersion = ? and p.minorVersion = ?")
+				.setString(0, stableId)
+				.setInteger(1, majorVersion)
+				.setInteger(2, minorVersion)
+				.uniqueResult();
+	}
+	
+	
+	@Transactional(readOnly = true)
+	public Procedure getProcedureByStableIdAndVersion(String stableId, int majorVersion, int minorVersion) {
+		return (Procedure) getCurrentSession().createQuery("from Procedure as p where p.stableId = ? and p.majorVersion = ? and p.minorVersion = ?")
+				.setString(0, stableId)
+				.setInteger(1, majorVersion)
+				.setInteger(2, minorVersion)
+				.uniqueResult();
+	}
+	
+	@Transactional(readOnly = true)
+	public Parameter getParameterByStableIdAndVersion(String stableId, int majorVersion, int minorVersion) {
+		return (Parameter) getCurrentSession().createQuery("from Parameter as p where p.stableId = ? and p.majorVersion = ? and p.minorVersion = ?")
+				.setString(0, stableId)
+				.setInteger(1, majorVersion)
+				.setInteger(2, minorVersion)
+				.uniqueResult();
+	}
+	
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Parameter> getProcedureMetaDataParametersByStableIdAndVersion(String stableId, int majorVersion, int minorVersion) {
+		List<Parameter> parameters =   getCurrentSession().createQuery("select param from Parameter as param inner join param.procedure as proc where proc.stableId = ? and param.majorVersion = ? and param.minorVersion = ? and param.metaDataFlag = true")
+				.setString(0, stableId)
+				.setInteger(1, majorVersion)
+				.setInteger(2, minorVersion)
+				.list();
+		return parameters;
+	}
+	
+	@Transactional(readOnly = false)
+	public void savePipeline(Pipeline pipeline) {
+		getCurrentSession().saveOrUpdate(pipeline);
+		
+	}
+	
+	@Transactional(readOnly = false)
+	public void saveProcedure(Procedure procedure) {
+		getCurrentSession().saveOrUpdate(procedure);
+	}
+	
+	@Transactional(readOnly = false)
+	public void saveParameter(Parameter parameter) {
+		getCurrentSession().saveOrUpdate(parameter);
+	}
+
+	@Transactional(readOnly = false)
+	public void saveParameterOption(ParameterOption parameterOption) {
+		getCurrentSession().saveOrUpdate(parameterOption);
+		
+	}
+
+	@Transactional(readOnly = false)
+	public void saveParameterIncrement(ParameterIncrement parameterIncrement) {
+		getCurrentSession().saveOrUpdate(parameterIncrement);
+		
+	}
+
+	@Transactional(readOnly = false)
+	public void saveParameterOntologyAnnotation(
+			ParameterOntologyAnnotation parameterOntologyAnnotation) {
+		getCurrentSession().saveOrUpdate(parameterOntologyAnnotation);
+		
+	}
+	
+	@Transactional(readOnly = false)
+	public void deleteAllPipelinesByDatasource(Datasource datasource) {
+		
+		Query query = getCurrentSession().getNamedQuery("deleteAllPipelinesByDatasource")
+				.setInteger("dbID", datasource.getId());
+		query.executeUpdate();
+	}
+}
