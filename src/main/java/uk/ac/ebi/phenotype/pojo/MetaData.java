77d7c46c6a4097bbc3e07bec53a88025c3d4a883
Gautier Koscielny <gkos.ebi@gmail.com>
2013-04-02 16:27:41 +0100

    beta release 0.1.7 2013 licence added EMBL EBI Apache 2 license added Change copyright in Apache 2 license to 2013 Change copyright in Apache 2 license to 2013 Author and comments added. if statements should have curly braces add override for hashcode and equals Move PhenotypeArchive to trunk Copyright notice changed Observation classes added Biological sample and model added. Meta data information from phenotype pipeline

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/MetaData.java
@@ -0,0 +1,128 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+/**
+ * 
+ * A concrete representation of phenotype meta data within a procedure.
+ * At the procedure level, the meta data information are about aging.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class MetaData {
+
+	private String name;
+	private String value;
+
+	/**
+	 * 
+	 */
+	public MetaData() {
+		super();
+	}
+
+	/**
+	 * @param name
+	 * @param value
+	 */
+	public MetaData(String name, String value) {
+		super();
+		this.name = name;
+		this.value = value;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value
+	 *            the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	// @Override
+	// public boolean equals(Object arg0) {
+	// if(arg0 == null) return false;
+	// if(!(arg0 instanceof MetaData)) return false;
+	// MetaData arg1 = (MetaData) arg0;
+	// return (this.name.equals(arg1.getName())) &&
+	// (this.value.equals(arg1.getValue()));
+	//
+	// }
+
+	public String toString() {
+		return name + " => " + value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) { return true; }
+		if (obj == null) { return false; }
+		if (getClass() != obj.getClass()) { return false; }
+
+		MetaData other = (MetaData) obj;
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		} else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
+	}
+}
