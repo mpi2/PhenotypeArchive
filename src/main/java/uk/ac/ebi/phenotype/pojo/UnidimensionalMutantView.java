6e491ea6b0df5b07eef79507292d99e037d2a59c
Gautier Koscielny <gkos.ebi@gmail.com>
2013-04-02 16:28:19 +0100

    beta release 0.1.7 add type method stats has new unidimensional stats dao

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/UnidimensionalMutantView.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+
+/**
+ * 
+ * A representation of a mutant mouse for a unidimensional value.
+ * 
+ */
+@Entity
+@Table(name = "stats_mv_experimental_unidimensional_values")
+public class UnidimensionalMutantView extends UnidimensionalView implements Serializable {
+
+	private static final long serialVersionUID = 4505038269190999331L;
+	private static final String type = "mutant";
+	public static String getType() {
+		return type;
+	}
+
+}
+
