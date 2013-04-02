1f3c6db7fa9b19477721fe428aa5ed6d0f8cc621
Gautier Koscielny <gkos.ebi@gmail.com>
2013-04-02 16:29:18 +0100

    beta release 0.1.7 Tidied up TimeSeries and Unidimensional DAOs so no remnants of CategoricalDAO methods stats has new unidimensional stats dao

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/UnidimensionalStatisticsDAO.java
@@ -0,0 +1,39 @@
+package uk.ac.ebi.phenotype.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+
+import uk.ac.ebi.phenotype.pojo.BiologicalModel;
+import uk.ac.ebi.phenotype.pojo.CategoricalResult;
+import uk.ac.ebi.phenotype.pojo.Organisation;
+import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.SexType;
+import uk.ac.ebi.phenotype.pojo.ZygosityType;
+
+public interface UnidimensionalStatisticsDAO {
+
+	public List<Float> countControl(SexType sex, Parameter parameter, Integer populationId);
+	public List<Float> countMutant(SexType sex, ZygosityType zygosity, Parameter parameter, Integer populationId);
+	
+	public BiologicalModel getControlBiologicalModelByPopulation(Integer populationId);
+	public BiologicalModel getMutantBiologicalModelByPopulation(Integer populationId);
+
+	public List<BiologicalModel> getBiologicalModelsByParameter(Parameter parameter);
+	public List<BiologicalModel> getBiologicalModelsByParameterAndGene(Parameter parameter, String accessionId);
+
+	public Double getpValueByParameterAndBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	public Double getMaxEffectSizeByParameterAndBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	
+	public Organisation getOrganisationByPopulation(Integer populationId);
+	public List<Integer> getPopulationIdsByParameter(Parameter parameter);
+	public List<Integer> getPopulationIdsByParameterAndMutantBiologicalModel(Parameter parameter, BiologicalModel biologicalModel);
+
+	//public void saveUnidimensionalResult(UnidimensionalResult result);
+
+	public SexType getSexByPopulation(Integer populationId);
+	public List<ZygosityType> getZygositiesByPopulation(Integer populationId);
+
+	
+}
