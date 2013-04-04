a611eb3b90e621a80f0ffe9df1b8978be3044002
Gautier Koscielny <gkos.ebi@gmail.com>
2013-04-04 14:43:14 +0100

    add methods for getting all records bu parameter,organisation and colony

--- src/main/java/uk/ac/ebi/phenotype/dao/UnidimensionalStatisticsDAO.java
+++ src/main/java/uk/ac/ebi/phenotype/dao/UnidimensionalStatisticsDAO.java
@@ -2,14 +2,16 @@ package uk.ac.ebi.phenotype.dao;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Set;
 
 import org.hibernate.HibernateException;
 
 import uk.ac.ebi.phenotype.pojo.BiologicalModel;
-import uk.ac.ebi.phenotype.pojo.CategoricalResult;
 import uk.ac.ebi.phenotype.pojo.Organisation;
 import uk.ac.ebi.phenotype.pojo.Parameter;
 import uk.ac.ebi.phenotype.pojo.SexType;
+import uk.ac.ebi.phenotype.pojo.UnidimensionalResult;
+import uk.ac.ebi.phenotype.pojo.UnidimensionalView;
 import uk.ac.ebi.phenotype.pojo.ZygosityType;
 
 public interface UnidimensionalStatisticsDAO {
@@ -17,24 +19,19 @@ public interface UnidimensionalStatisticsDAO {
 	public List<Float> countControl(SexType sex, Parameter parameter, Integer populationId);
 	public List<Float> countMutant(SexType sex, ZygosityType zygosity, Parameter parameter, Integer populationId);
 	
+	public List<BiologicalModel> getBiologicalModelsByParameterAndGene(Parameter parameter, String accessionId);
 	public BiologicalModel getControlBiologicalModelByPopulation(Integer populationId);
 	public BiologicalModel getMutantBiologicalModelByPopulation(Integer populationId);
 
-	public List<BiologicalModel> getBiologicalModelsByParameter(Parameter parameter);
-	public List<BiologicalModel> getBiologicalModelsByParameterAndGene(Parameter parameter, String accessionId);
-
-	public Double getpValueByParameterAndBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
-	public Double getMaxEffectSizeByParameterAndBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	public Double getpValueByParameterAndBiologicalModel(Parameter parameter, BiologicalModel biologicalModel);
 	
-	public Organisation getOrganisationByPopulation(Integer populationId);
-	public List<Integer> getPopulationIdsByParameter(Parameter parameter);
 	public List<Integer> getPopulationIdsByParameterAndMutantBiologicalModel(Parameter parameter, BiologicalModel biologicalModel);
-
-	public void saveCategoricalResult(CategoricalResult result);
 
 	public SexType getSexByPopulation(Integer populationId);
 	public List<ZygosityType> getZygositiesByPopulation(Integer populationId);
 
-	public void deleteCategoricalResultByParameter(Parameter parameter) throws HibernateException, SQLException;
+	public void deleteUnidimensionalResultByParameter(Parameter parameter) throws HibernateException, SQLException;
+	public void saveUnidimensionalResult(UnidimensionalResult result);
+	public Set<UnidimensionalView> getUnidimensionalData(Parameter parameter, Organisation organization, String colonyId);
 	
 }
