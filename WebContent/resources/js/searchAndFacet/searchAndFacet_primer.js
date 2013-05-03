ce3bd17067cd2dd9f184147f9f690a5c824a6767
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:22:07 +0100

    Beta release 0.1.9 added changes for new imits_phenotype* facet changes for search without autosuggest dropdown and on-demand facet loading new code for search without autosuggest dropdown list and on-demand facet loading modified code to work with new gene/allele core created using httpdatasource commented out hiding facet count dealt with null object for IE fixed bugs in search hash due to lack of encodeURI and added jumping straight to gene/mp page code when select from list use globals to point to solr server fixed search? bug not picking up some fq= in url modified to work with hash in url Search and Facet comments added. Update URL structure from geneDetails?gene_id=xxx to genes/xxx; Update URL structure from /searchAndFacet to /search default logging to not log debug messages for springframework.web catch IE 7 and earlier users fixed spinner bug fixed bug in images index to capture case insensitive search and synonym, eg, xay -> x-ray update gene_details link to geneDetails fixed gene name result sorting bug: do not use qf=auto_suggest, as at sanger, the gene has diff. config fixed generic search bugs for displaying top level categories of image facet added code for image facet Added code for image facet fixed problem of automatic fire of use search due to keyup events that are not 'enter' - added preliminary custom 403/404/500 pages - updated web.xml for custom 403/404/500 pages - added MpController to check for solr query result on the server side and direct users to the custom 404 page if 0 doc is found   Ie, this is no longer controlled from the client side fixed MP/MA facet count not displayed issue when search was coming from redirected page - changed coding style to jquey-widget factory way. - resolved problem of MP text/image sections loading asynchronously Use global baseUrl variable to define relative paths for javascript link generation correct path for JS gene detail page rewrites Update path for search results added search/facet and mp pages

---
+++ WebContent/resources/js/searchAndFacet/searchAndFacet_primer.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * searchAndFacet_primer: callbacks for the autocomplete and sidebar widgets
+ * see: autocompleteWidget.js and sideBarFacetWidget.js
+ * 
+ */
+$(document).ready(function(){
+	'use strict';	
+	
+	$('span.facetCount').text(''); // default when page loads
+
+	//var pathname = MPI2.searchAndFacetConfig.pathname;
+	
+	$('input#userInput').val('');  // clears input when pages loads
+	
+	// default search when search page loads
+	if ( /search$/.exec(location.href) ){
+		// do default gene search by * when search page loads
+		$.fn.fetchSolrFacetCount('*:*');
+	}
+	else if ( location.href.indexOf('/search#') != -1 ){		
+		// load page based on url hash parameters		
+		var hashParams = $.fn.parseHashString(window.location.hash.substring(1));	
+		$.fn.fetchSolrFacetCount(hashParams.q);
+	}
+	
+	// search via ENTER
+	$('input#userInput').keyup(function (e) {		
+	    if (e.keyCode == 13) { // user hits enter
+	    	var input = $('input#userInput').val();	      
+	    	input = input == '' ? '*:*' : input;	    	    	
+	    	document.location.href = baseUrl + '/search#q=' + input;
+	    }
+	}).click(function(){
+		$(this).val(''); // clears input 
+	});
+	
+	// search via button click
+	$('button#acSearch').click(function(){
+		var input = $('input#userInput').val();
+		input = input == '' ? '*:*' : input;			
+		document.location.href = baseUrl + '/search#q=' + input;
+	});
+			
+	// auto-complete is always created
+	// load total facetCount of all data types by default when searchAndFacet page loads
+	/*window.jQuery('input#userInput').mpi2AutoComplete({
+		
+			solrBaseURL_bytemark: MPI2.searchAndFacetConfig.solrBaseURL_bytemark,			
+			solrBaseURL_ebi: MPI2.searchAndFacetConfig.solrBaseURL_ebi,
+			search_pathname: pathname,			        
+			
+			loadSideBar: function(event, data){					
+				//console.log('loadSideBar: q: '+ data.q + ' core: ' + data.core + ' fq: ' + data.fq);
+				// calls left side bar widget	
+						
+				window.jQuery('div#leftSideBar').mpi2LeftSideBar({  
+					data: data, // key q: query string
+			        geneGridElem: 'div#mpi2-search'			                                      
+				});				
+			},
+			redirectedSearch: function(event, data){				
+				// Make a hidden form on your page, and submit it here.
+					
+			    var form = "<form id='hiddenSrch' action='" + pathname + "' method='get'>"				
+			    		 + "<input type='text' name='core' value='" + data.core + "'>"
+			             + "<input type='text' name='fq' value=" + encodeURI(data.fq) + ">"			                                
+			             + "<input type='text' name='q' value='" + data.q + "'>"
+			             + "</form>";                     
+
+			    window.jQuery('div#bannerSearch').append(form);
+			    window.jQuery('form#hiddenSrch').hide().submit();			    
+		   } 
+		}).click(function(){
+				window.jQuery(this).val('');
+				//$('span.facetCount').text('');			
+	}); */
+	
+	// dynamically readjusted position of autosuggest dropdown list due to elastic design of page
+	window.jQuery(window).resize(function(){
+		var pos = window.jQuery('input#userInput').offset();     
+		window.jQuery('ul.ui-autocomplete').css({'position':'absolute', 'top': pos.top + 26, 'left': pos.left}); 
+	});
+	
+});
