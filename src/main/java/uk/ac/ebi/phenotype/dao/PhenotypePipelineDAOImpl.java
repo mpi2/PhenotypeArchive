bc3fe0134d33048ce2582415ac0a872c5f23d2d2
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:23:46 +0100

    Beta release 0.1.9 speed up get all parameters queries (don't use the view, just join together the minimum number of tables required) update to use java 7 try semantics for querying data directly from database; organize imports throw exceptions instead of handling them at this level; update tests to match (forgot to commit with last batch) add unimplemented method deleteAllPipelinesByDatasource added to delete phenotype protocols using a native query. DAO updated for phenotype protocols Change copyright in Apache 2 license to 2013 adjust the warning suppression as appropriate to get rid of warnings; Remove unused imports Data access manager interface and implementation. Move PhenotypeArchive to trunk All DAO changes.

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/PhenotypePipelineDAOImpl.java
@@ -0,0 +1,225 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ * 
+ * Phenotype pipeline data access manager implementation.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since May 2012
+ */
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import uk.ac.ebi.phenotype.pojo.Datasource;
+import uk.ac.ebi.phenotype.pojo.OntologyTerm;
+import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.ParameterIncrement;
+import uk.ac.ebi.phenotype.pojo.ParameterOntologyAnnotation;
+import uk.ac.ebi.phenotype.pojo.ParameterOption;
+import uk.ac.ebi.phenotype.pojo.Pipeline;
+import uk.ac.ebi.phenotype.pojo.Procedure;
+
+
+
+public class PhenotypePipelineDAOImpl extends HibernateDAOImpl implements PhenotypePipelineDAO {
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * Creates a new Hibernate pipeline data access manager.
+	 * @param sessionFactory the Hibernate session factory
+	 */
+	public PhenotypePipelineDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Pipeline> getAllPhenotypePipelines() {
+		List<Pipeline> pipelines = getCurrentSession().createQuery("from Pipeline").list();
+		return pipelines;
+	}
+
+	@Transactional(readOnly = true)
+	public Pipeline getPhenotypePipelineByStableId(String stableId) {
+		return (Pipeline) getCurrentSession().createQuery("from Pipeline as p where p.stableId = ?").setString(0, stableId).uniqueResult();
+	}
+
+	@Transactional(readOnly = true)
+	public Pipeline getPhenotypePipelineByStableIdAndVersion(String stableId, int majorVersion, int minorVersion) {
+		return (Pipeline) getCurrentSession().createQuery("from Pipeline as p where p.stableId = ? and p.majorVersion = ? and p.minorVersion = ?")
+				.setString(0, stableId)
+				.setInteger(1, majorVersion)
+				.setInteger(2, minorVersion)
+				.uniqueResult();
+	}
+	
+	
+	@Transactional(readOnly = true)
+	public Procedure getProcedureByStableIdAndVersion(String stableId, int majorVersion, int minorVersion) {
+		return (Procedure) getCurrentSession().createQuery("from Procedure as p where p.stableId = ? and p.majorVersion = ? and p.minorVersion = ?")
+				.setString(0, stableId)
+				.setInteger(1, majorVersion)
+				.setInteger(2, minorVersion)
+				.uniqueResult();
+	}
+	
+	@Transactional(readOnly = true)
+	public Parameter getParameterByStableIdAndVersion(String stableId, int majorVersion, int minorVersion) {
+		return (Parameter) getCurrentSession().createQuery("from Parameter as p where p.stableId = ? and p.majorVersion = ? and p.minorVersion = ?")
+				.setString(0, stableId)
+				.setInteger(1, majorVersion)
+				.setInteger(2, minorVersion)
+				.uniqueResult();
+	}
+	
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Parameter> getProcedureMetaDataParametersByStableIdAndVersion(String stableId, int majorVersion, int minorVersion) {
+		List<Parameter> parameters =   getCurrentSession().createQuery("select param from Parameter as param inner join param.procedure as proc where proc.stableId = ? and param.majorVersion = ? and param.minorVersion = ? and param.metaDataFlag = true")
+				.setString(0, stableId)
+				.setInteger(1, majorVersion)
+				.setInteger(2, minorVersion)
+				.list();
+		return parameters;
+	}
+	
+	@Transactional(readOnly = false)
+	public void savePipeline(Pipeline pipeline) {
+		getCurrentSession().saveOrUpdate(pipeline);
+		
+	}
+	
+	@Transactional(readOnly = false)
+	public void saveProcedure(Procedure procedure) {
+		getCurrentSession().saveOrUpdate(procedure);
+	}
+	
+	@Transactional(readOnly = false)
+	public void saveParameter(Parameter parameter) {
+		getCurrentSession().saveOrUpdate(parameter);
+	}
+
+	@Transactional(readOnly = false)
+	public void saveParameterOption(ParameterOption parameterOption) {
+		getCurrentSession().saveOrUpdate(parameterOption);
+		
+	}
+
+	@Transactional(readOnly = false)
+	public void saveParameterIncrement(ParameterIncrement parameterIncrement) {
+		getCurrentSession().saveOrUpdate(parameterIncrement);
+		
+	}
+
+	@Transactional(readOnly = false)
+	public void saveParameterOntologyAnnotation(
+			ParameterOntologyAnnotation parameterOntologyAnnotation) {
+		getCurrentSession().saveOrUpdate(parameterOntologyAnnotation);
+		
+	}
+	
+	@Transactional(readOnly = false)
+	public void deleteAllPipelinesByDatasource(Datasource datasource) {
+		
+		Query query = getCurrentSession().getNamedQuery("deleteAllPipelinesByDatasource")
+				.setInteger("dbID", datasource.getId());
+		query.executeUpdate();
+	}
+	
+	/**
+	 * Helper method to fetch the actual parameter pojo when provided a 
+	 * database id.
+	 */
+	@Transactional(readOnly = true)
+	public Parameter getParameterById(Integer parameterId) {
+		return (Parameter) getCurrentSession()
+				.createQuery("SELECT p FROM Parameter p WHERE p.id=?")
+				.setInteger(0, parameterId)
+				.uniqueResult();
+	}
+
+	/**
+	 * getProcedureByOntologyTerm returns the sorted set of procedures
+	 * associated to a passed in ontology term 
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	public Set<Procedure> getProceduresByOntologyTerm(OntologyTerm term) {
+		if (term == null) return null;
+		return (Set<Procedure>) new HashSet<Procedure>(getCurrentSession()
+				.createQuery("SELECT proc FROM Procedure proc INNER JOIN proc.parameters as param INNER JOIN param.annotations as annotations WHERE annotations.ontologyTerm.id.databaseId=? AND annotations.ontologyTerm.id.accession=?")
+				.setInteger(0, term.getId().getDatabaseId())
+				.setString(1, term.getId().getAccession())
+				.list());
+	}
+
+	/**
+	 * Return all categorical parameters for which we have data loaded
+	 * 
+	 * @exception SQLException When a database error occurrs
+	 */
+	@Transactional(readOnly = true)
+	public Set<Parameter> getAllCategoricalParametersForProcessing() throws SQLException {
+		Set<Parameter> parameters = new HashSet<Parameter>();
+
+		String query = "SELECT DISTINCT o.parameter_id FROM observation o JOIN biological_sample bs ON o.biological_sample_id = bs.id WHERE o.observation_type = 'categorical' AND bs.sample_group = 'control'";
+
+		try (PreparedStatement statement = getConnection().prepareStatement(query)) {
+		    ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				parameters.add(getParameterById(resultSet.getInt("parameter_id")));
+			}
+		}
+
+		return parameters;
+	}
+
+	/**
+	 * Return all unidimensional parameters for which we have data loaded
+	 * 
+	 * @exception SQLException When a database error occurrs
+	 */
+	@Transactional(readOnly = true)
+	public Set<Parameter> getAllUnidimensionalParametersForProcessing() throws SQLException {
+		Set<Parameter> parameters = new HashSet<Parameter>();
+
+		String query = "SELECT DISTINCT o.parameter_id FROM observation o JOIN biological_sample bs ON o.biological_sample_id = bs.id WHERE o.observation_type = 'unidimensional' AND bs.sample_group = 'control'";
+
+		try (PreparedStatement statement = getConnection().prepareStatement(query)) {
+		    ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				parameters.add(getParameterById(resultSet.getInt("parameter_id")));
+			}
+		}
+
+		return parameters;
+	}
+
+}
