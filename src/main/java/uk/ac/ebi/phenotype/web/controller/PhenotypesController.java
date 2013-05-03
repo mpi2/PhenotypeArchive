99b095592fdb980fe5dbf0af41be064fd4543092
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:57:52 +0100

    change liveSite to a boolean

--- src/main/java/uk/ac/ebi/phenotype/web/controller/PhenotypesController.java
+++ src/main/java/uk/ac/ebi/phenotype/web/controller/PhenotypesController.java
@@ -15,14 +15,28 @@
  */
 package uk.ac.ebi.phenotype.web.controller;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.hibernate.HibernateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,24 +49,38 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.ebi.generic.util.SolrIndex;
 import uk.ac.ebi.phenotype.dao.OntologyTermDAO;
-import uk.ac.ebi.phenotype.error.GenomicFeatureNotFoundException;
+import uk.ac.ebi.phenotype.dao.PhenotypeCallSummaryDAO;
+import uk.ac.ebi.phenotype.dao.PhenotypePipelineDAO;
 import uk.ac.ebi.phenotype.error.OntologyTermNotFoundException;
 import uk.ac.ebi.phenotype.imaging.springrest.images.dao.ImagesSolrDao;
-import uk.ac.ebi.phenotype.pojo.GenomicFeature;
+import uk.ac.ebi.phenotype.pojo.Datasource;
 import uk.ac.ebi.phenotype.pojo.OntologyTerm;
+import uk.ac.ebi.phenotype.pojo.PhenotypeCallSummary;
+import uk.ac.ebi.phenotype.pojo.Procedure;
+import uk.ac.ebi.phenotype.web.pojo.PhenotypeRow;
 
 @Controller
-public class PhenotypesController implements BeanFactoryAware {
+public class PhenotypesController {
 
 	private final Logger log = LoggerFactory.getLogger(PhenotypesController.class);
 
-	private BeanFactory bf;
 
 	@Autowired
 	private OntologyTermDAO ontoTermDao;
 	
 	@Autowired
+	private PhenotypeCallSummaryDAO phenoDAO;
+
+	
+	@Autowired
+	private PhenotypePipelineDAO pipelineDao;
+	
+	@Autowired
 	private ImagesSolrDao imagesSolrDao;
+
+	@Resource(name="globalConfiguration")
+	private Map<String, String> config;
+
 
 	/**
 	 * Phenotype controller loads information required for displaying 
@@ -61,66 +89,171 @@ public class PhenotypesController implements BeanFactoryAware {
 	 * 
 	 * @param phenotype_id the Mammalian phenotype id of the phenotype to display
 	 * @return the name of the view to render, or redirect to search page on error
+	 * @throws URISyntaxException 
+	 * @throws IOException 
+	 * @throws SolrServerException 
 	 * 
 	 */
-	
 	@RequestMapping(value="/phenotypes/{phenotype_id}", method=RequestMethod.GET)
 	public String loadMpPage(
 			@PathVariable String phenotype_id, 
 			Model model,
-			RedirectAttributes attributes) throws OntologyTermNotFoundException {
+			HttpServletRequest request,
+			RedirectAttributes attributes) throws OntologyTermNotFoundException, IOException, URISyntaxException, SolrServerException {
 		
-		// Get the global application configuration
-		@SuppressWarnings("unchecked")
-		Map<String,String> config = (Map<String,String>) bf.getBean("globalConfiguration");
-
-		OntologyTerm phenotypeTerm = ontoTermDao.getOntologyTermByAccessionAndDatabaseId(phenotype_id, 5);
-		if (phenotypeTerm == null) {
+		OntologyTerm phenotype = ontoTermDao.getOntologyTermByAccessionAndDatabaseId(phenotype_id, 5);
+		if (phenotype == null) {
 			throw new OntologyTermNotFoundException("", phenotype_id);
 		}
 		
+		TreeSet<Procedure> procedures = new TreeSet<Procedure>(pipelineDao.getProceduresByOntologyTerm(phenotype));
+
+		model.addAttribute("phenotype", phenotype);
+		model.addAttribute("procedures", procedures);
+
+		Set<OntologyTerm> anatomyTerms = new HashSet<OntologyTerm>();
+		Set<OntologyTerm> mpSiblings = new HashSet<OntologyTerm>();
+		Set<OntologyTerm> goTerms = new HashSet<OntologyTerm>();
+
+		try {
+
+			JSONObject mpData = new SolrIndex(config)
+				.getMpData(phenotype_id)
+				.getJSONObject("response")
+				.getJSONArray("docs")
+				.getJSONObject(0);
+
+			JSONArray terms;
+			
+			if (mpData.containsKey("ma_id")) {
+				terms = mpData.getJSONArray("ma_id");
+				for (Object maObj : terms) {
+					String id = (String) maObj;
+					anatomyTerms.add(ontoTermDao.getOntologyTermByAccessionAndDatabaseId(id, 8));
+				}
+			}
+
+			if (mpData.containsKey("sibling_mp_id")) {
+				terms = mpData.getJSONArray("sibling_mp_id");
+				for (Object obj : terms) {
+					String id = (String) obj;
+					if( ! id.equals(phenotype_id)) {
+						mpSiblings.add(ontoTermDao.getOntologyTermByAccessionAndDatabaseId(id, 5));
+					}
+				}
+			}
+
+			if (mpData.containsKey("go_id")) {
+				terms = mpData.getJSONArray("go_id");
+				for (Object obj : terms) {
+					String id = (String) obj;
+					goTerms.add(ontoTermDao.getOntologyTermByAccessionAndDatabaseId(id, 11));
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			anatomyTerms = new HashSet<OntologyTerm>();
+			mpSiblings = new HashSet<OntologyTerm>();
+		}
 		
-		model.addAttribute("phenotypeTerm", phenotypeTerm);
-		model.addAttribute("phenotype_id", phenotype_id);
+		model.addAttribute("anatomy", anatomyTerms);
+		model.addAttribute("go", goTerms);
+		model.addAttribute("siblings", mpSiblings);
 
 		// Query the images for this phenotype
 		QueryResponse response=imagesSolrDao.getDocsForMpTerm(phenotype_id, 0, 6);
+		System.out.println(response);
 		model.addAttribute("numberFound", response.getResults().getNumFound());
 		model.addAttribute("images", response.getResults());
 
-		// Check to see if the phenotype term is found in the index
-		// if not, redirect to the 
-		String solrCoreName = "mp";
-		String mode = "mpPage";
-		SolrIndex solrIndex = new SolrIndex(phenotype_id, solrCoreName, mode, config);
-		
-		log.info("CHECK numFound : " + solrIndex.fetchNumFound());
-
-		if ( solrIndex.fetchNumFound() == 0 ){
-			throw new OntologyTermNotFoundException("Phenotype <b>" + phenotype_id + "</b> was not found", phenotype_id);
-			//attributes.addFlashAttribute();
-			//return "redirect:/search";
+		// This block collapses phenotype rows
+		// phenotype term, allele, zygosity, and sex
+		// sex is collapsed into a single column
+		List<PhenotypeCallSummary> phenotypeList = new ArrayList<PhenotypeCallSummary>();
+		try {
+			phenotypeList = phenoDAO.getPhenotypeCallByMPAccession(phenotype_id, 5);
+		} catch (HibernateException e) {
+			log.error("ERROR GETTING PHENOTYPE LIST");
+			e.printStackTrace();
+			phenotypeList = new ArrayList<PhenotypeCallSummary>();
 		}
+
+		// This is a map because we need to support lookups
+		Map<PhenotypeRow,PhenotypeRow> phenotypes = new HashMap<PhenotypeRow,PhenotypeRow>(); 
+		
+		for (PhenotypeCallSummary pcs : phenotypeList) {
+
+			// Use a tree set to maintain an alphabetical order (Female, Male)
+			List<String> sex = new ArrayList<String>();
+			sex.add(pcs.getSex().toString());
+
+			PhenotypeRow pr = new PhenotypeRow();
+			pr.setGene(pcs.getGene());
+			pr.setAllele(pcs.getAllele());
+			pr.setSexes(sex);
+			pr.setPhenotypeTerm(pcs.getPhenotypeTerm());
+
+			// zygosity representation depends on source of information
+			// we need to know what the data source is so we can generate appropriate link on the page
+			Datasource ds = pcs.getDatasource();
+
+			// Defend in case the datasource is not loaded
+			String dataSourceName = (ds != null) ? dataSourceName = ds.getName() : "";
+			pr.setDataSourceName(dataSourceName);
+
+			// this should be the fix but EuroPhenome is buggy
+			String rawZygosity = (dataSourceName.equals("EuroPhenome")) ? 
+					//Utilities.getZygosity(pcs.getZygosity()) : pcs.getZygosity().toString();
+					"All" : pcs.getZygosity().toString();
+			pr.setRawZygosity(rawZygosity);
+			pr.setZygosity(pcs.getZygosity());
+			pr.setProjectId(pcs.getExternalId());
+			pr.setProcedure(pcs.getProcedure());
+			pr.setParameter(pcs.getParameter());
+
+			if(phenotypes.containsKey(pr)) {
+				pr = phenotypes.get(pr);
+				TreeSet<String> sexes = new TreeSet<String>();
+				for (String s : pr.getSexes()) { sexes.add(s); }
+				sexes.add(pcs.getSex().toString());
+				pr.setSexes(new ArrayList<String>(sexes));
+			}
+			
+			if(pr.getParameter() != null && pr.getProcedure()!= null) {				
+				phenotypes.put(pr, pr);
+			}
+		}
+		model.addAttribute("phenotypes", new ArrayList<PhenotypeRow>(phenotypes.keySet()));
+
+		/**
+		 * 
+		 */
+
+		model.addAttribute("isLive", new Boolean((String) request.getAttribute("liveSite")));
 
 		return "phenotypes";
 	}	
 
+
 	
-	/**
-	 * required for implementing BeanFactoryAware
-	 * 
-	 * @see org.springframework.beans.factory.BeanFactoryAware#setBeanFactory(org.springframework.beans.factory.BeanFactory)
-	 */
-	@Override
-	public void setBeanFactory(BeanFactory arg0) throws BeansException {
-		this.bf=arg0;
-	}
 	
 	@ExceptionHandler(OntologyTermNotFoundException.class)
 	public ModelAndView handleGenomicFeatureNotFoundException(OntologyTermNotFoundException exception) {
         ModelAndView mv = new ModelAndView("identifierError");
         mv.addObject("errorMessage",exception.getMessage());
         mv.addObject("acc",exception.getAcc());
+        mv.addObject("type","mouse phenotype");
+        mv.addObject("exampleURI", "/phenotypes/MP:0000585");
+        return mv;
+    } 
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleGenericException(Exception exception) {
+		exception.printStackTrace();
+        ModelAndView mv = new ModelAndView("identifierError");
+        mv.addObject("errorMessage",exception.getMessage());
+        mv.addObject("acc","");
         mv.addObject("type","mouse phenotype");
         mv.addObject("exampleURI", "/phenotypes/MP:0000585");
         return mv;
