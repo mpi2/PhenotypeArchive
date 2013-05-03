1d512639e36dd91a67a3033cdf7f86b2a6149de2
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:57:53 +0100

    update phenotype row collapsing based on gender/zygosity

--- src/main/java/uk/ac/ebi/phenotype/web/pojo/PhenotypeRow.java
+++ src/main/java/uk/ac/ebi/phenotype/web/pojo/PhenotypeRow.java
@@ -15,10 +15,18 @@
  */
 package uk.ac.ebi.phenotype.web.pojo;
 
-import java.util.Set;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang.WordUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import uk.ac.ebi.phenotype.pojo.Allele;
+import uk.ac.ebi.phenotype.pojo.GenomicFeature;
 import uk.ac.ebi.phenotype.pojo.OntologyTerm;
+import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.Procedure;
 import uk.ac.ebi.phenotype.pojo.ZygosityType;
 
 /**
@@ -28,23 +36,61 @@ import uk.ac.ebi.phenotype.pojo.ZygosityType;
  */
 public class PhenotypeRow {
 	
+	private final Logger log = LoggerFactory.getLogger(PhenotypeRow.class);
+
 	private OntologyTerm phenotypeTerm;
+	private GenomicFeature gene;
 	private Allele allele;
-	private Set<String> sexes;
+	private List<String> sexes;
 	private ZygosityType zygosity;
 	private String rawZygosity;
 	private int projectId;
-	private String procedureId;
-	private String parameterId;
+	private Procedure procedure;
+	private Parameter parameter;
 	private String dataSourceName;//to hold the name of the origin of the data e.g. Europhenome or WTSI Mouse Genetics Project
-	private String linkToOriginalDataProvider;
-	
-	public String getLinkToOriginalDataProvider() {
-		return linkToOriginalDataProvider;
+	 
+	/**
+	 * Returns a PhenotypeRow object with the original data provider link
+	 * populated.
+	 * 
+	 * @param pr The original phenotype row object
+	 * @return
+	 */
+	public String getPhenotypeLink() {
+
+		String linkUrl="";
+
+		if(getDataSourceName().equals("EuroPhenome")){
+			String sex="";
+			if(getSexes().size()==2){
+				sex="Both-Split";
+			}else{
+				Iterator<String> iter = getSexes().iterator();
+				String first = (String) iter.next();
+				sex=WordUtils.capitalize(first);
+			}
+			try {
+				linkUrl="http://www.europhenome.org/databrowser/viewer.jsp?set=true&m=true&l="+getProjectId()+"&zygosity="+getRawZygosity()+"&x="+sex+"&p="+getProcedure().getStableId()+"&pid_"+getParameter().getStableId()+"=on&compareLines=View+Data";
+			} catch (NullPointerException e) {
+				log.error("Project: "+getProjectId());
+				log.error("Zygosity: "+getRawZygosity());
+				log.error("Procedure: "+getProcedure());
+				log.error("Parameter: "+getParameter());
+				linkUrl = "";
+			}
+		}
+
+		if(getDataSourceName().equals("WTSI Mouse Genetics Project")){
+			
+			if(getAllele()!=null  && getAllele().getGene()!=null){
+				linkUrl="http://www.sanger.ac.uk/mouseportal/search?query="+getAllele().getGene().getSymbol();
+			}
+
+		}
+
+		return linkUrl;
 	}
-	public void setLinkToOriginalDataProvider(String linkToOriginalDataProvider) {
-		this.linkToOriginalDataProvider = linkToOriginalDataProvider;
-	}
+
 	public String getDataSourceName() {
 		return dataSourceName;
 	}
@@ -63,11 +109,11 @@ public class PhenotypeRow {
 	public void setAllele(Allele allele) {
 		this.allele = allele;
 	}
-	public Set<String> getSexes() {
+	public List<String> getSexes() {
 		return sexes;
 	}
-	public void setSexes(Set<String> sexes) {
-		this.sexes = sexes;
+	public void setSexes(List<String> sex) {
+		this.sexes = sex;
 	}
 	public ZygosityType getZygosity() {
 		return zygosity;
@@ -102,65 +148,106 @@ public class PhenotypeRow {
 	public void setProjectId(int projectId) {
 		this.projectId = projectId;
 	}
-	/**
-	 * @return the procedureId
-	 */
-	public String getProcedureId() {
-		return procedureId;
+
+
+	public GenomicFeature getGene() {
+		return gene;
 	}
-	/**
-	 * @param procedureId the procedureId to set
-	 */
-	public void setProcedureId(String procedureId) {
-		this.procedureId = procedureId;
+	public void setGene(GenomicFeature gene) {
+		this.gene = gene;
 	}
-	/**
-	 * @return the parameterId
-	 */
-	public String getParameterId() {
-		return parameterId;
+	public Procedure getProcedure() {
+		return procedure;
 	}
-	/**
-	 * @param parameterId the parameterId to set
-	 */
-	public void setParameterId(String parameterId) {
-		this.parameterId = parameterId;
+	public void setProcedure(Procedure procedure) {
+		this.procedure = procedure;
+	}
+	public Parameter getParameter() {
+		return parameter;
+	}
+	public void setParameter(Parameter parameter) {
+		this.parameter = parameter;
 	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((allele == null) ? 0 : allele.hashCode());
+		result = prime * result + ((dataSourceName == null) ? 0 : dataSourceName.hashCode());
 		result = prime * result + ((phenotypeTerm == null) ? 0 : phenotypeTerm.hashCode());
-		result = prime * result
-				+ ((zygosity == null) ? 0 : zygosity.hashCode());
+		result = prime * result + ((zygosity == null) ? 0 : zygosity.hashCode());
+		if (gene != null) {
+			result = prime * result + ((parameter == null) ? 0 : parameter.hashCode());
+			result = prime * result + ((procedure == null) ? 0 : procedure.hashCode());
+		}
 		return result;
 	}
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
 		PhenotypeRow other = (PhenotypeRow) obj;
 		if (allele == null) {
-			if (other.allele != null)
+			if (other.allele != null) {
 				return false;
-		} else if (!allele.equals(other.allele))
+			}
+		} else if (!allele.equals(other.allele)) {
 			return false;
+		}
+		if (dataSourceName == null) {
+			if (other.dataSourceName != null) {
+				return false;
+			}
+		} else if (!dataSourceName.equals(other.dataSourceName)) {
+			return false;
+		}
+		if (gene != null) {
+			if (parameter == null) {
+				if (other.parameter != null) {
+					return false;
+				}
+			} else if (!parameter.equals(other.parameter)) {
+				return false;
+			}
+			if (procedure == null) {
+				if (other.procedure != null) {
+					return false;
+				}
+			} else if (!procedure.equals(other.procedure)) {
+				return false;
+			}	
+		}
 		if (phenotypeTerm == null) {
-			if (other.phenotypeTerm != null)
+			if (other.phenotypeTerm != null) {
 				return false;
-		} else if (!phenotypeTerm.equals(other.phenotypeTerm))
+			}
+		} else if (!phenotypeTerm.equals(other.phenotypeTerm)) {
 			return false;
-		if (zygosity == null) {
-			if (other.zygosity != null)
-				return false;
-		} else if (!zygosity.equals(other.zygosity))
+		}
+		if (zygosity != other.zygosity) {
 			return false;
+		}
 		return true;
 	}
+
+
+	@Override
+	public String toString() {
+		return "PhenotypeRow [phenotypeTerm=" + phenotypeTerm
+				+ ", gene=" + gene + ", allele=" + allele + ", sexes=" + sexes
+				+ ", zygosity=" + zygosity + ", rawZygosity=" + rawZygosity
+				+ ", projectId=" + projectId + ", procedure=" + procedure
+				+ ", parameter=" + parameter + ", dataSourceName="
+				+ dataSourceName + "]";
+	}
+
+	
 
 }
