16a8d926188d5c25968ae17d5f315af34d72739a
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:22:51 +0100

    Beta release 0.1.9 Organize imports and add hashcode and equals for objects required for stats processing add toString methods Annotations added 2013 licence added EMBL EBI Apache 2 license added Change copyright in Apache 2 license to 2013 Author and comments added. Move PhenotypeArchive to trunk Copyright notice changed Observation classes added Biological sample and model added.

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/ParameterOption.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+/**
+ * 
+ * A concrete representation of phenotype parameter option.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @version $Revision: 2227 $
+ *  @since February 2012
+ */
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "phenotype_parameter_option")
+public class ParameterOption {
+	
+	/**
+	 * MySQL auto increment
+	 * GenerationType.AUTO won't work
+	 */
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "description")	
+	private String description;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * 
+	 */
+	public ParameterOption() {
+		super();
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "ParameterOption [id=" + id + ", name=" + name
+				+ ", description=" + description + "]";
+	}
+	
+	
+}
