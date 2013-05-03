8f13eb9f8d93e2c75ec91ec346e0db2a40504093
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 15:59:54 +0100

    Release 0.1.9 2013 licence added EMBL EBI Apache 2 license added Change copyright in Apache 2 license to 2013 Author and comments added. Move PhenotypeArchive to trunk Copyright notice changed Observation classes added Biological sample and model added. Phenotype Archive Project (Java/Maven/Spring/Hibernate/MySQL)

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/Project.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+/**
+ * 
+ * Representation of a phenotyping / mutant mice project.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "project")
+public class Project {
+
+	@Id
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "fullname")
+	private String fullname;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@ManyToMany
+	@JoinTable(name = "participant", 
+	 joinColumns = { @JoinColumn(name = "project_id")},
+	 inverseJoinColumns={@JoinColumn(name="organisation_id")}   
+	  )
+	private Set<Organisation> participants = new HashSet<Organisation>();
+	
+	/**
+	 * @return the participants
+	 */
+	public Set<Organisation> getParticipants() {
+		return participants;
+	}
+
+	protected Project() {
+		super();
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the fullname
+	 */
+	public String getFullname() {
+		return fullname;
+	}
+
+	/**
+	 * @param fullname the fullname to set
+	 */
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
