903fe64d76361d06c94bff33863a8a17a7da331d
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:23:30 +0100

    Beta release 0.1.9 changes to some stats method names and removal of getHtml from biological model replaced by jm formatAllele tag DiscreteTimePoint updated to include confidence data/error bar data time series graph related objects added

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/DiscreteTimePoint.java
@@ -0,0 +1,74 @@
+package uk.ac.ebi.phenotype.dao;
+
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.xml.bind.api.impl.NameConverter.Standard;
+
+
+public class DiscreteTimePoint implements Comparable{
+	
+	private List<Float> errorPair;//if this is the mean of a set we can store the error bars data and other data such as Standard Deviation etc?
+	
+	public List<Float> getErrorPair() {
+		return this.errorPair;
+	}
+	public void setErrorPair(List<Float> errorPair) {
+		this.errorPair = errorPair;
+	}
+	public DiscreteTimePoint(Float discreteTime, Float discreteDataPoint){
+		this.discreteTime=discreteTime;
+		this.data=discreteDataPoint;
+	}
+	public DiscreteTimePoint(Float discreteTime, Float discreteDataPoint, Float standardDeviation){
+		this(discreteTime, discreteDataPoint);
+		this.stdDeviation=standardDeviation;
+		errorPair=new ArrayList<Float>();
+		errorPair.add(this.data-this.stdDeviation);
+		errorPair.add(this.data+this.stdDeviation);
+	}
+	
+
+private Float discreteTime;
+ public Float getDiscreteTime() {
+	return discreteTime;
+}
+public void setDiscreteTime(Float discretePoint) {
+	this.discreteTime = discretePoint;
+}
+
+public Float getData() {
+	return data;
+}
+public void setData(Float data) {
+	this.data = data;
+}
+private Float data;
+private Float stdDeviation;
+ 
+public Float getStdDeviation() {
+	return stdDeviation;
+}
+
+public String toString(){
+	//String myString = DateFormat.getDateInstance(DateFormat.FULL).format(date);
+	String stdDev="No std dev specified";
+	if(stdDeviation!=null)stdDev=stdDeviation.toString();
+	return "["+discreteTime+" ,"+ data+stdDev+"]";
+	
+}
+
+@Override
+public int compareTo(Object arg0) {
+	DiscreteTimePoint p = (DiscreteTimePoint) arg0; 
+	if(p.getDiscreteTime()>this.getDiscreteTime()){
+		return -1;
+	}else{
+		return 1;
+	}
+
+}
+
+}
