dabd86f6587c393b895594cf89662d5dcfa8b106
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:57:19 +0100

    Organize imports and add hashcode and equals for objects required for stats processing

--- src/main/java/uk/ac/ebi/phenotype/pojo/Datasource.java
+++ src/main/java/uk/ac/ebi/phenotype/pojo/Datasource.java
@@ -22,7 +22,7 @@ package uk.ac.ebi.phenotype.pojo;
  * Not exhaustive list: ontologies, MGI markers, EuroPhenome legacy data, etc.
  * 
  * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
- * @version $Revision: 1291 $
+ * @version $Revision: 2227 $
  *  @since February 2012
  */
 
@@ -131,4 +131,54 @@ public class Datasource {
 	public String toString() {
 		return "Id: " + id + "; Name: " + name + "; version: " + version + "; release date: " + releaseDate;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((releaseDate == null) ? 0 : releaseDate.hashCode());
+		result = prime * result
+				+ ((shortName == null) ? 0 : shortName.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Datasource other = (Datasource) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (releaseDate == null) {
+			if (other.releaseDate != null)
+				return false;
+		} else if (!releaseDate.equals(other.releaseDate))
+			return false;
+		if (shortName == null) {
+			if (other.shortName != null)
+				return false;
+		} else if (!shortName.equals(other.shortName))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
+	
+	
 }
