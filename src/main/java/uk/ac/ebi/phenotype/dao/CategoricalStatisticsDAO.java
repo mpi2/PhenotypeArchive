9ecfd44becb156fcf5ce07aee251321db6d84760
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 16:00:07 +0100

    Release 0.1.9 factor out common save method and introduce DAO inheritance for statistics DAOs stats graphs boxing bugs fixed and stats test entry points added with start and length parameters to get different rows from db added getListOfUniqueParametersAndGenes method to stats daos and a page that uses these so we can get long lists of links to try out for stats graphs added getListOfUniqueParametersAndGenes method to stats daos and a page that uses these so we can get long lists of links to try out for stats graphs cat dao merged add methods to support caching the required data on a parameter level new methods for batch categorical data processing cat dao has list int returned use SexType and Zygosity type instead of String add methods for getting population ID from model and parameter and for getting a list of all models for a parameter and gene add methods to get gender and zygosity from population id updates required for the new categorical database schema clean up app config a little update control (don't use zygosity) make DAO methods more general; use biological model instead of genomic feature to query mutants add more general methods for passing in the correct values rather than a method for every combination add classes and test for pulling out categorical data by parameter

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/CategoricalStatisticsDAO.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.hibernate.HibernateException;
+
+import uk.ac.ebi.phenotype.pojo.BiologicalModel;
+import uk.ac.ebi.phenotype.pojo.CategoricalGroupKey;
+import uk.ac.ebi.phenotype.pojo.Organisation;
+import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.SexType;
+import uk.ac.ebi.phenotype.pojo.ZygosityType;
+
+public interface CategoricalStatisticsDAO extends StatisticsDAO {
+
+	public List<String> getCategories(Parameter parameter);
+
+	public Long countControl(SexType sex, Parameter parameter, String category, Integer populationId) throws SQLException;
+	public Long countMutant(SexType sex, ZygosityType zygosity, Parameter parameter, String category, Integer populationId) throws SQLException;
+	
+	public BiologicalModel getControlBiologicalModelByPopulation(Integer populationId);
+	public BiologicalModel getMutantBiologicalModelByPopulation(Integer populationId);
+
+	public List<BiologicalModel> getMutantBiologicalModelsByParameterAndGene(Parameter parameter, String accessionId);
+	
+	public List<BiologicalModel> getBiologicalModelsByParameter(Parameter parameter) ;
+
+	public Double getpValueByParameterAndControlBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	
+	public Double getpValueByParameterAndMutantBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	
+	public Double getMaxEffectSizeByParameterAndMutantBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	
+	public Organisation getOrganisationByPopulation(Integer populationId);
+	public List<Integer> getPopulationIdsByParameter(Parameter parameter);
+	public List<Integer> getPopulationIdsByParameterAndMutantBiologicalModel(Parameter parameter, BiologicalModel biologicalModel);
+
+	public SexType getSexByPopulation(Integer populationId);
+	public List<ZygosityType> getZygositiesByPopulation(Integer populationId);
+
+	public void deleteCategoricalResultByParameter(Parameter parameter) throws HibernateException, SQLException;
+
+	public Integer getPopulationIdByColonySexParameter(String colonyId, SexType sex, Parameter parameter);
+
+	public List<CategoricalGroupKey> getControlCategoricalDataByParameter(Parameter parameter);
+	public List<CategoricalGroupKey> getMutantCategoricalDataByParameter(Parameter parameter);
+
+	public Map<Integer, Integer> getOrganisationsByParameter(Parameter parameter);
+	
+	public  List<Map<String,String>>  getListOfUniqueParametersAndGenes(int start, int length) throws SQLException;
+	
+
+}
