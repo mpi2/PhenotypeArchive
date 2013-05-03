14df3b6643c5e6d531d9cd6059f1051b229f9e41
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:57:21 +0100

    Organize imports and add hashcode and equals for objects required for stats processing

--- src/main/java/uk/ac/ebi/phenotype/pojo/Organisation.java
+++ src/main/java/uk/ac/ebi/phenotype/pojo/Organisation.java
@@ -125,5 +125,63 @@ public class Organisation {
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result
+				+ ((fullname == null) ? 0 : fullname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((projects == null) ? 0 : projects.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Organisation other = (Organisation) obj;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (fullname == null) {
+			if (other.fullname != null)
+				return false;
+		} else if (!fullname.equals(other.fullname))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (projects == null) {
+			if (other.projects != null)
+				return false;
+		} else if (!projects.equals(other.projects))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Organisation [id=" + id + ", name=" + name + ", fullname="
+				+ fullname + ", country=" + country + ", projects=" + projects
+				+ "]";
+	}
 	
 }
