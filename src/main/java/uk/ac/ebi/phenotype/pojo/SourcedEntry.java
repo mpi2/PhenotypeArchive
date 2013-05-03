8f43abfd2e283fa492a228826e38e99e4569982a
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:57:25 +0100

    Organize imports and add hashcode and equals for objects required for stats processing

--- src/main/java/uk/ac/ebi/phenotype/pojo/SourcedEntry.java
+++ src/main/java/uk/ac/ebi/phenotype/pojo/SourcedEntry.java
@@ -73,6 +73,38 @@ public class SourcedEntry {
 	public void setDatasource(Datasource datasource) {
 		this.datasource = datasource;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((datasource == null) ? 0 : datasource.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SourcedEntry other = (SourcedEntry) obj;
+		if (datasource == null) {
+			if (other.datasource != null)
+				return false;
+		} else if (!datasource.equals(other.datasource))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 	
 }
