a51d3b23d2cc256f97b6b2e6a09685820445c56f
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:23:55 +0100

    Beta release 0.1.9 factor out common save method and introduce DAO inheritance for statistics DAOs stats graphs boxing bugs fixed and stats test entry points added with start and length parameters to get different rows from db added getListOfUniqueParametersAndGenes method to stats daos and a page that uses these so we can get long lists of links to try out for stats graphs changes to some stats method names and removal of getHtml from biological model replaced by jm formatAllele tag Tidied up TimeSeries and Unidimensional DAOs so no remnants of CategoricalDAO methods time series graph related objects added

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/TimeSeriesStatisticsDAO.java
@@ -0,0 +1,35 @@
+package uk.ac.ebi.phenotype.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import uk.ac.ebi.phenotype.pojo.BiologicalModel;
+import uk.ac.ebi.phenotype.pojo.Organisation;
+import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.SexType;
+import uk.ac.ebi.phenotype.pojo.ZygosityType;
+
+public interface TimeSeriesStatisticsDAO extends StatisticsDAO {
+
+	public List<DiscreteTimePoint> getControlStats(SexType sex, Parameter parameter, Integer populationId);
+	public List<DiscreteTimePoint> getMutantStats(SexType sex, ZygosityType zygosity, Parameter parameter, Integer populationId);
+	
+	public BiologicalModel getControlBiologicalModelByPopulation(Integer populationId);
+	public BiologicalModel getMutantBiologicalModelByPopulation(Integer populationId);
+
+	public List<BiologicalModel> getBiologicalModelsByParameter(Parameter parameter);
+	public List<BiologicalModel> getBiologicalModelsByParameterAndGene(Parameter parameter, String accessionId);
+
+	public Double getpValueByParameterAndBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	public Double getMaxEffectSizeByParameterAndBiologicalModelAndSexAndZygosity(Parameter parameter, BiologicalModel biologicalModel, SexType sex, ZygosityType zygosity);
+	
+	public Organisation getOrganisationByPopulation(Integer populationId);
+	public List<Integer> getPopulationIdsByParameter(Parameter parameter);
+	public List<Integer> getPopulationIdsByParameterAndMutantBiologicalModel(Parameter parameter, BiologicalModel biologicalModel);
+
+	public SexType getSexByPopulation(Integer populationId);
+	public List<ZygosityType> getZygositiesByPopulation(Integer populationId);
+	
+	public  List<Map<String,String>>  getListOfUniqueParametersAndGenes(int start, int length) throws SQLException;
+}
