7b07aede494e6dbb68120e5785f3903f638190b0
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 15:59:31 +0100

    Release 0.1.9 2013 licence added EMBL EBI Apache 2 license added Change copyright in Apache 2 license to 2013 Move PhenotypeArchive to trunk Refactored to use generics. Use generics. lightweight object representation for HTML / JSON / XML export. List container. It is used for HTML rendering with jstl.

---
+++ src/main/java/uk/ac/ebi/phenotype/bean/ListContainer.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.bean;
+
+/**
+ * 
+ * This is a list container for jstl.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ *  @since February 2012
+ */
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="listContainer")
+public class ListContainer<X> {
+
+	private int count;
+	private List<X> results;
+
+	public ListContainer() {}
+	
+	public ListContainer(List<X> results) {
+		this.results = results;
+		this.count = results.size();
+	}
+
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	@XmlElement(name="results")
+	public List<X> getResults() {
+		return results;
+	}
+	public void setResults(List<X> results) {
+		this.results = results;
+	}
+}
