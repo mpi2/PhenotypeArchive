cf445183cf2557467c5f1a6e228f60253db310e9
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:22:32 +0100

    Beta release 0.1.9 Organize imports and add hashcode and equals for objects required for stats processing 2013 licence added EMBL EBI Apache 2 license added Change copyright in Apache 2 license to 2013 Author and comments added. Move PhenotypeArchive to trunk Copyright notice changed Observation classes added Biological sample and model added. A datasource object mapped to external_db.

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/Datasource.java
@@ -0,0 +1,184 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+/**
+ *
+ * Representation of a datasource in the database.
+ * A datasource is an external data representation from different providers.
+ * Not exhaustive list: ontologies, MGI markers, EuroPhenome legacy data, etc.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @version $Revision: 2227 $
+ *  @since February 2012
+ */
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "external_db")
+public class Datasource {
+
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "short_name")
+	private String shortName;
+	
+	@Column(name = "version")
+	private String version;
+	
+	@Column(name = "version_date")
+	private Date releaseDate;
+	
+	protected Datasource() {
+		super();
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return the shortName
+	 */
+	public String getShortName() {
+		return shortName;
+	}
+
+	/**
+	 * @param shortName the shortName to set
+	 */
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the version
+	 */
+	public String getVersion() {
+		return version;
+	}
+
+	/**
+	 * @param version the version to set
+	 */
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	/**
+	 * @return the releaseDate
+	 */
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	/**
+	 * @param releaseDate the releaseDate to set
+	 */
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	public String toString() {
+		return "Id: " + id + "; Name: " + name + "; version: " + version + "; release date: " + releaseDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((releaseDate == null) ? 0 : releaseDate.hashCode());
+		result = prime * result
+				+ ((shortName == null) ? 0 : shortName.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Datasource other = (Datasource) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (releaseDate == null) {
+			if (other.releaseDate != null)
+				return false;
+		} else if (!releaseDate.equals(other.releaseDate))
+			return false;
+		if (shortName == null) {
+			if (other.shortName != null)
+				return false;
+		} else if (!shortName.equals(other.shortName))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
+	
+	
+}
