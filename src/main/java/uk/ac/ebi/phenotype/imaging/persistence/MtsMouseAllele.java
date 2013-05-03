34c32f5f758f5403f8bf269f2e6c653b9d85e149
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:24:10 +0100

    Beta release 0.1.9 EMBL EBI Apache 2 license added annotations and classes added to get more information from the sanger and komp2 genomic features for the images gene index extra mgis for allele and wt included - some classes for genotype dict but not yet wired up

---
+++ src/main/java/uk/ac/ebi/phenotype/imaging/persistence/MtsMouseAllele.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.imaging.persistence;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name="MTS_MOUSE_ALLELE")
+public class MtsMouseAllele {
+	private int id;
+	private int mouseId;
+	//private int alleleId;
+	
+	private MtsGenotypeDict mtsGenotypeDict;
+
+	@ManyToOne
+	@JoinColumn(name="GENOTYPE_DICT_ID", referencedColumnName="ID")
+	public MtsGenotypeDict getMtsGenotypeDict() {
+		return mtsGenotypeDict;
+	}
+
+	public void setMtsGenotypeDict(MtsGenotypeDict mtsGenotypeDict) {
+		this.mtsGenotypeDict = mtsGenotypeDict;
+	}
+
+	@Id
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Column(name="MOUSE_ID")
+	public int getMouseId() {
+		return mouseId;
+	}
+
+	public void setMouseId(int mouseId) {
+		this.mouseId = mouseId;
+	}
+	
+}
