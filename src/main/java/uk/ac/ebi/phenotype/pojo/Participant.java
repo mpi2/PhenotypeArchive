750cc873c416a2ef64b5d4d11629172a0cd34432
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:57:23 +0100

    correctly map column names; implement accessor methods; implement hashcode/equals methods

--- src/main/java/uk/ac/ebi/phenotype/pojo/Participant.java
+++ src/main/java/uk/ac/ebi/phenotype/pojo/Participant.java
@@ -26,19 +26,99 @@ package uk.ac.ebi.phenotype.pojo;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "PARTICIPANT")
+@Table(name = "participant")
 public class Participant {
 
 	@Id
-	@Column(name = "ID")
-	private Integer entityId;
+	@OneToOne
+	@JoinColumn(name = "project_id")
+	private Project projectId;
 
-	@Column(name = "ROLE")
+	@OneToOne
+	@JoinColumn(name = "organisation_id")
+	private Organisation organisationId;
+
+	@Column(name = "role")
 	private String role;
 
 	protected Participant() {
 	}
+
+	public Project getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(Project projectId) {
+		this.projectId = projectId;
+	}
+
+	public Organisation getOrganisationId() {
+		return organisationId;
+	}
+
+	public void setOrganisationId(Organisation organisationId) {
+		this.organisationId = organisationId;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((organisationId == null) ? 0 : organisationId.hashCode());
+		result = prime * result
+				+ ((projectId == null) ? 0 : projectId.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Participant other = (Participant) obj;
+		if (organisationId == null) {
+			if (other.organisationId != null) {
+				return false;
+			}
+		} else if (!organisationId.equals(other.organisationId)) {
+			return false;
+		}
+		if (projectId == null) {
+			if (other.projectId != null) {
+				return false;
+			}
+		} else if (!projectId.equals(other.projectId)) {
+			return false;
+		}
+		if (role == null) {
+			if (other.role != null) {
+				return false;
+			}
+		} else if (!role.equals(other.role)) {
+			return false;
+		}
+		return true;
+	}
+	
 }
