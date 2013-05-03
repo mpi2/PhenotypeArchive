0858c640946fe8928e267a2f0943f0eee351c9a9
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:24:08 +0100

    Beta release 0.1.9 EMBL EBI Apache 2 license added imaging java classes added

---
+++ src/main/java/uk/ac/ebi/phenotype/imaging/persistence/ImaSubcontext.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.imaging.persistence;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import java.util.List;
+
+
+/**
+ * The persistent class for the IMA_SUBCONTEXT database table.
+ * 
+ */
+@Entity
+@Table(name="IMA_SUBCONTEXT")
+public class ImaSubcontext implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private ImaImportContext imaImportContext;
+	private byte isDefault;
+	//private List<ImaImageRecord> imaImageRecords;
+	private ImaExperimentDict imaExperimentDict;
+
+    public ImaSubcontext() {
+    }
+
+    @JsonIgnore
+	@Id
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	//@Column(name="IMPORT_CONTEXT_ID")
+	@ManyToOne(optional=false)
+    @JoinColumn(name="IMPORT_CONTEXT_ID",referencedColumnName="ID")
+	public ImaImportContext getImaImportContext() {
+		return this.imaImportContext;
+	}
+
+	public void setImaImportContext(ImaImportContext imaImportContext) {
+		this.imaImportContext = imaImportContext;
+	}
+
+
+	@Column(name="IS_DEFAULT")
+	public byte getIsDefault() {
+		return this.isDefault;
+	}
+
+	public void setIsDefault(byte isDefault) {
+		this.isDefault = isDefault;
+	}
+
+
+	//bi-directional many-to-one association to ImaImageRecord
+//	@OneToMany(fetch=FetchType.EAGER, mappedBy="imaSubcontext")
+//	public List<ImaImageRecord> getImaImageRecords() {
+//		return this.imaImageRecords;
+//	}
+//
+//	public void setImaImageRecords(List<ImaImageRecord> imaImageRecords) {
+//		this.imaImageRecords = imaImageRecords;
+//	}
+	
+
+	//bi-directional many-to-one association to ImaExperimentDict
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="EXPERIMENT_DICT_ID")
+	public ImaExperimentDict getImaExperimentDict() {
+		return this.imaExperimentDict;
+	}
+
+	public void setImaExperimentDict(ImaExperimentDict imaExperimentDict) {
+		this.imaExperimentDict = imaExperimentDict;
+	}
+	
+}
