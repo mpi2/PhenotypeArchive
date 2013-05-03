77f6c1248af3653ea67487dafc44d67ffee0bb90
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 15:59:48 +0100

    Release 0.1.9 Organize imports and add hashcode and equals for objects required for stats processing Annotations added 2013 licence added EMBL EBI Apache 2 license added Change copyright in Apache 2 license to 2013 Author and comments added. Move PhenotypeArchive to trunk Copyright notice changed Observation classes added Biological sample and model added.

---
+++ src/main/java/uk/ac/ebi/phenotype/pojo/ParameterIncrement.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.pojo;
+
+/**
+ * 
+ * A concrete representation of phenotype parameter increment.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "phenotype_parameter_increment")
+public class ParameterIncrement {
+
+	/**
+	 * MySQL auto increment
+	 * GenerationType.AUTO won't work
+	 */
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "increment_value")
+	private String value;
+	
+	@Column(name = "increment_datatype")	
+	private String dataType;
+	
+	@Column(name = "increment_unit")	
+	private String unit;
+	
+	@Column(name = "increment_minimum")
+	private String minimum;
+	
+	/**
+	 * 
+	 */
+	public ParameterIncrement() {
+		super();
+	}
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+	/**
+	 * @return the dataType
+	 */
+	public String getDataType() {
+		return dataType;
+	}
+	/**
+	 * @param dataType the dataType to set
+	 */
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+	/**
+	 * @return the unit
+	 */
+	public String getUnit() {
+		return unit;
+	}
+	/**
+	 * @param unit the unit to set
+	 */
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	/**
+	 * @return the minimum
+	 */
+	public String getMinimum() {
+		return minimum;
+	}
+	/**
+	 * @param minimum the minimum to set
+	 */
+	public void setMinimum(String minimum) {
+		this.minimum = minimum;
+	}
+	
+	public String toString() {
+		return "Value: " + value + "; dataType: " + dataType + "; unit: " + unit + "; minimum: " + minimum;
+	}
+	
+}
