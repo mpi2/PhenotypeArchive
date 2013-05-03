5a330efab6ffc5a6ba8d006b1272e674ec29c08d
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:23:40 +0100

    Beta release 0.1.9 Change copyright in Apache 2 license to 2013 Data access manager interface and implementation. Move PhenotypeArchive to trunk All DAO changes. New methods for ontology browsing. Contains mapping to genomic features

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/OntologyTermDAO.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ * 
+ * Ontology term and controlled vocabulary data access manager interface.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since February 2012
+ */
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+import uk.ac.ebi.phenotype.pojo.OntologyTerm;
+
+
+public interface OntologyTermDAO {
+
+	public List<OntologyTerm> getAllOntologyTerms();
+	
+	public List<OntologyTerm> getAllOntologyTermsByDatabaseId(int databaseId);
+	
+	public OntologyTerm getOntologyTermByName(String name);
+	
+	public OntologyTerm getOntologyTermByNameAndDatabaseId(String name, int databaseId);
+	
+	public OntologyTerm getOntologyTermByAccessionAndDatabaseId(String accession, int databaseId);
+	
+	public HashMap<String, OntologyTerm> getAllTerms(int databaseId);
+	
+	public int deleteAllTerms(String datasourceName);
+	
+	public int batchInsertion(Collection<OntologyTerm> ontologyTerms);
+	
+}
