2f11d500c09fc0a13a31597fd8e7e25fb5821d31
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 16:01:17 +0100

    Release 0.1.9 change liveSite to a boolean check if it's live update phenotype row collapsing based on gender/zygosity make phenotype link an attribute of the phenotype row object wrong dependency MP page to use the database and hibernate; remove mppage JS. throw exceptions instead of handling them at this level; update tests to match Error handling added to genes.jsp to handle when images, other cores and biomart service is not responding added an ExceptionHandler to redirect to the identifierError view pass the mp term to the view EMBL EBI Apache 2 license added Update URL structure from mp?mp_id=xxx to phenotypes/xxx; added code for datatable export: on current pagination and all data in a table Add bean factory to get the configuration and pass into solrIndex image results works from mp, genedetails and search??? merged new backend images jsps for geneDetails and mp pages - added preliminary custom 403/404/500 pages - updated web.xml for custom 403/404/500 pages - added MpController to check for solr query result on the server side and direct users to the custom 404 page if 0 doc is found   Ie, this is no longer controlled from the client side

---
+++ src/main/java/uk/ac/ebi/phenotype/web/controller/PhenotypesController.java
@@ -0,0 +1,261 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.web.controller;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.hibernate.HibernateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import uk.ac.ebi.generic.util.SolrIndex;
+import uk.ac.ebi.phenotype.dao.OntologyTermDAO;
+import uk.ac.ebi.phenotype.dao.PhenotypeCallSummaryDAO;
+import uk.ac.ebi.phenotype.dao.PhenotypePipelineDAO;
+import uk.ac.ebi.phenotype.error.OntologyTermNotFoundException;
+import uk.ac.ebi.phenotype.imaging.springrest.images.dao.ImagesSolrDao;
+import uk.ac.ebi.phenotype.pojo.Datasource;
+import uk.ac.ebi.phenotype.pojo.OntologyTerm;
+import uk.ac.ebi.phenotype.pojo.PhenotypeCallSummary;
+import uk.ac.ebi.phenotype.pojo.Procedure;
+import uk.ac.ebi.phenotype.web.pojo.PhenotypeRow;
+
+@Controller
+public class PhenotypesController {
+
+	private final Logger log = LoggerFactory.getLogger(PhenotypesController.class);
+
+
+	@Autowired
+	private OntologyTermDAO ontoTermDao;
+	
+	@Autowired
+	private PhenotypeCallSummaryDAO phenoDAO;
+
+	
+	@Autowired
+	private PhenotypePipelineDAO pipelineDao;
+	
+	@Autowired
+	private ImagesSolrDao imagesSolrDao;
+
+	@Resource(name="globalConfiguration")
+	private Map<String, String> config;
+
+
+	/**
+	 * Phenotype controller loads information required for displaying 
+	 * the phenotype page or, in the case of an error, redirects to the 
+	 * error page
+	 * 
+	 * @param phenotype_id the Mammalian phenotype id of the phenotype to display
+	 * @return the name of the view to render, or redirect to search page on error
+	 * @throws URISyntaxException 
+	 * @throws IOException 
+	 * @throws SolrServerException 
+	 * 
+	 */
+	@RequestMapping(value="/phenotypes/{phenotype_id}", method=RequestMethod.GET)
+	public String loadMpPage(
+			@PathVariable String phenotype_id, 
+			Model model,
+			HttpServletRequest request,
+			RedirectAttributes attributes) throws OntologyTermNotFoundException, IOException, URISyntaxException, SolrServerException {
+		
+		OntologyTerm phenotype = ontoTermDao.getOntologyTermByAccessionAndDatabaseId(phenotype_id, 5);
+		if (phenotype == null) {
+			throw new OntologyTermNotFoundException("", phenotype_id);
+		}
+		
+		TreeSet<Procedure> procedures = new TreeSet<Procedure>(pipelineDao.getProceduresByOntologyTerm(phenotype));
+
+		model.addAttribute("phenotype", phenotype);
+		model.addAttribute("procedures", procedures);
+
+		Set<OntologyTerm> anatomyTerms = new HashSet<OntologyTerm>();
+		Set<OntologyTerm> mpSiblings = new HashSet<OntologyTerm>();
+		Set<OntologyTerm> goTerms = new HashSet<OntologyTerm>();
+
+		try {
+
+			JSONObject mpData = new SolrIndex(config)
+				.getMpData(phenotype_id)
+				.getJSONObject("response")
+				.getJSONArray("docs")
+				.getJSONObject(0);
+
+			JSONArray terms;
+			
+			if (mpData.containsKey("ma_id")) {
+				terms = mpData.getJSONArray("ma_id");
+				for (Object maObj : terms) {
+					String id = (String) maObj;
+					anatomyTerms.add(ontoTermDao.getOntologyTermByAccessionAndDatabaseId(id, 8));
+				}
+			}
+
+			if (mpData.containsKey("sibling_mp_id")) {
+				terms = mpData.getJSONArray("sibling_mp_id");
+				for (Object obj : terms) {
+					String id = (String) obj;
+					if( ! id.equals(phenotype_id)) {
+						mpSiblings.add(ontoTermDao.getOntologyTermByAccessionAndDatabaseId(id, 5));
+					}
+				}
+			}
+
+			if (mpData.containsKey("go_id")) {
+				terms = mpData.getJSONArray("go_id");
+				for (Object obj : terms) {
+					String id = (String) obj;
+					goTerms.add(ontoTermDao.getOntologyTermByAccessionAndDatabaseId(id, 11));
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			anatomyTerms = new HashSet<OntologyTerm>();
+			mpSiblings = new HashSet<OntologyTerm>();
+		}
+		
+		model.addAttribute("anatomy", anatomyTerms);
+		model.addAttribute("go", goTerms);
+		model.addAttribute("siblings", mpSiblings);
+
+		// Query the images for this phenotype
+		QueryResponse response=imagesSolrDao.getDocsForMpTerm(phenotype_id, 0, 6);
+		System.out.println(response);
+		model.addAttribute("numberFound", response.getResults().getNumFound());
+		model.addAttribute("images", response.getResults());
+
+		// This block collapses phenotype rows
+		// phenotype term, allele, zygosity, and sex
+		// sex is collapsed into a single column
+		List<PhenotypeCallSummary> phenotypeList = new ArrayList<PhenotypeCallSummary>();
+		try {
+			phenotypeList = phenoDAO.getPhenotypeCallByMPAccession(phenotype_id, 5);
+		} catch (HibernateException e) {
+			log.error("ERROR GETTING PHENOTYPE LIST");
+			e.printStackTrace();
+			phenotypeList = new ArrayList<PhenotypeCallSummary>();
+		}
+
+		// This is a map because we need to support lookups
+		Map<PhenotypeRow,PhenotypeRow> phenotypes = new HashMap<PhenotypeRow,PhenotypeRow>(); 
+		
+		for (PhenotypeCallSummary pcs : phenotypeList) {
+
+			// Use a tree set to maintain an alphabetical order (Female, Male)
+			List<String> sex = new ArrayList<String>();
+			sex.add(pcs.getSex().toString());
+
+			PhenotypeRow pr = new PhenotypeRow();
+			pr.setGene(pcs.getGene());
+			pr.setAllele(pcs.getAllele());
+			pr.setSexes(sex);
+			pr.setPhenotypeTerm(pcs.getPhenotypeTerm());
+
+			// zygosity representation depends on source of information
+			// we need to know what the data source is so we can generate appropriate link on the page
+			Datasource ds = pcs.getDatasource();
+
+			// Defend in case the datasource is not loaded
+			String dataSourceName = (ds != null) ? dataSourceName = ds.getName() : "";
+			pr.setDataSourceName(dataSourceName);
+
+			// this should be the fix but EuroPhenome is buggy
+			String rawZygosity = (dataSourceName.equals("EuroPhenome")) ? 
+					//Utilities.getZygosity(pcs.getZygosity()) : pcs.getZygosity().toString();
+					"All" : pcs.getZygosity().toString();
+			pr.setRawZygosity(rawZygosity);
+			pr.setZygosity(pcs.getZygosity());
+			pr.setProjectId(pcs.getExternalId());
+			pr.setProcedure(pcs.getProcedure());
+			pr.setParameter(pcs.getParameter());
+
+			if(phenotypes.containsKey(pr)) {
+				pr = phenotypes.get(pr);
+				TreeSet<String> sexes = new TreeSet<String>();
+				for (String s : pr.getSexes()) { sexes.add(s); }
+				sexes.add(pcs.getSex().toString());
+				pr.setSexes(new ArrayList<String>(sexes));
+			}
+			
+			if(pr.getParameter() != null && pr.getProcedure()!= null) {				
+				phenotypes.put(pr, pr);
+			}
+		}
+		model.addAttribute("phenotypes", new ArrayList<PhenotypeRow>(phenotypes.keySet()));
+
+		/**
+		 * 
+		 */
+
+		model.addAttribute("isLive", new Boolean((String) request.getAttribute("liveSite")));
+
+		return "phenotypes";
+	}	
+
+
+	
+	
+	@ExceptionHandler(OntologyTermNotFoundException.class)
+	public ModelAndView handleGenomicFeatureNotFoundException(OntologyTermNotFoundException exception) {
+        ModelAndView mv = new ModelAndView("identifierError");
+        mv.addObject("errorMessage",exception.getMessage());
+        mv.addObject("acc",exception.getAcc());
+        mv.addObject("type","mouse phenotype");
+        mv.addObject("exampleURI", "/phenotypes/MP:0000585");
+        return mv;
+    } 
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleGenericException(Exception exception) {
+		exception.printStackTrace();
+        ModelAndView mv = new ModelAndView("identifierError");
+        mv.addObject("errorMessage",exception.getMessage());
+        mv.addObject("acc","");
+        mv.addObject("type","mouse phenotype");
+        mv.addObject("exampleURI", "/phenotypes/MP:0000585");
+        return mv;
+    } 
+}
