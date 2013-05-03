6e123117aec869ea746a1273e362021759ac7b51
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:57:15 +0100

    use class variable for interesting genes

--- src/main/java/uk/ac/ebi/generic/util/RegisterInterestDrupalSolr.java
+++ src/main/java/uk/ac/ebi/generic/util/RegisterInterestDrupalSolr.java
@@ -7,24 +7,30 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import uk.ac.ebi.phenotype.web.util.DrupalHttpProxy;
-
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.ebi.phenotype.web.util.DrupalHttpProxy;
+
 public class RegisterInterestDrupalSolr {
 
-	private List<String> interestingGenes = null;
-	private String drupalBaseUrl;
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 	private DrupalHttpProxy drupalProxy;
-	private String loggedIn=null;
-	
-	public RegisterInterestDrupalSolr(Map<String, String> config, HttpServletRequest request){
+	private String drupalBaseUrl;
+
+	private Boolean loggedIn = null;
+	private List<String> interestingGenes = null;
+
+	public RegisterInterestDrupalSolr(Map<String,String> config, HttpServletRequest request) {
 		this.drupalBaseUrl = config.get("drupalBaseUrl");
-		this.drupalProxy=new DrupalHttpProxy(request);
-		
+		this.drupalProxy = new DrupalHttpProxy(request);
 	}
+
 	/**
 	 * is this user interested in the passed in gene
 	 * 
@@ -38,11 +44,13 @@ public class RegisterInterestDrupalSolr {
 	 * @return true if the user has expressed interest in the gene, false
 	 * if not
 	 */
-	public  boolean alreadyInterested(String geneid) {
-		if (this.interestingGenes == null) {
+	public boolean alreadyInterested(String geneid) {
+		
+		if ( ! loggedIn()) { return false; }
 
-			this.interestingGenes = new ArrayList<String>();
-
+		if (interestingGenes == null) {
+			interestingGenes = new ArrayList<String>();
+	
 			try {
 				URL url = new URL(drupalBaseUrl + "/genesofinterest");
 				String content = drupalProxy.getContent(url);
@@ -66,30 +74,33 @@ public class RegisterInterestDrupalSolr {
 	 * is this user logged in
 	 * 
 	 * This checks an external URL on the drupal instance to see if the
-	 * user is logged in.  It abuses the toggleflagfromjs exposed URL
-	 * and relies on the different error messages being returned when the
-	 * user is not logged in.
-	 * 
-	 * TODO: not optimal. should use a single sign on (not drupal)
+	 * user is logged in.  It uses the "/roles" URL.
 	 * 
 	 * @return true if the user is logged in, false if not
 	 */
 	public  boolean loggedIn() {
-		if (this.loggedIn == null) {
+
+		if (drupalProxy.getDrupalSessionCookieString().isEmpty()) {
+			log.debug("No drupal session cookie means not logged in");
+			return false;
+		}
+
+		if (loggedIn == null) {
+			loggedIn = false;
+
 			try {
 				URL url = new URL(drupalBaseUrl + "/roles");
 				String content = drupalProxy.getContent(url);
 				if (content.contains("authenticated user")) {
-					this.loggedIn = "true";
-				} else {
-					this.loggedIn = "false";
+					log.debug("User is logged in to drupal");
+					loggedIn = true;
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
-				this.loggedIn = "false";
+				loggedIn = false;
 			}
 		}
 
-		return this.loggedIn == "true" ? true : false;
+		return loggedIn;
 	}
 }
