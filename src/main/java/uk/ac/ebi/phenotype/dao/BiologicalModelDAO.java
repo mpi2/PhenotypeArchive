6294fdc5f0c7ff91248cfabbca872a8623ef5a43
Gautier Koscielny <gkos.ebi@gmail.com>
2013-05-03 10:23:23 +0100

    Beta release 0.1.9 add code to retrieve phenotypeStatus add getBiologicalSampleById method Method added to BiologicalModel and Observation DAOs to delete models, samples and observation using the datasource id (but not the organisation id). extends HibernateDAO make DAO methods more general; use biological model instead of genomic feature to query mutants 2013 licence added EMBL EBI Apache 2 license added Change copyright in Apache 2 license to 2013 Data access manager interface and implementation.

---
+++ src/main/java/uk/ac/ebi/phenotype/dao/BiologicalModelDAO.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.phenotype.dao;
+
+/**
+ * 
+ * Biological model manager interface.
+ * 
+ * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
+ * @since May 2012
+ */
+
+import java.util.List;
+
+import uk.ac.ebi.phenotype.pojo.BiologicalModel;
+import uk.ac.ebi.phenotype.pojo.BiologicalSample;
+import uk.ac.ebi.phenotype.pojo.Datasource;
+import uk.ac.ebi.phenotype.pojo.LiveSample;
+import uk.ac.ebi.phenotype.pojo.Organisation;
+
+public interface BiologicalModelDAO extends HibernateDAO {
+
+	public void saveBiologicalSample(BiologicalSample sample);
+	public void saveLiveSample(LiveSample sample);
+	public int deleteAllLiveSamplesByDatasource(Datasource datasource);
+	public int  deleteAllLiveSamplesWithoutModelsByDatasource(Datasource datasource);
+	public int deleteAllBiologicalSamplesByDatasource(Datasource datasource);
+	
+	public List<BiologicalModel> getAllBiologicalModelsByDatasourceId(int databaseId);
+	public List<BiologicalModel> getAllBiologicalModelsByAccession(String accession);
+	
+	public List<LiveSample> getAllLiveSamplesByDatasourceId(int databaseId);
+	
+	
+	public BiologicalModel getBiologicalModelById(int modelId);
+	public BiologicalSample getBiologicalSampleById(int sampleId);
+	
+	public void saveBiologicalModel(BiologicalModel model);
+	
+	public int deleteAllBiologicalModelsByDatasource(Datasource datasource);
+	
+	public List<LiveSample> getAllLiveSampleByOrganisationAndDatasource(Organisation organisation, Datasource datasource);
+}
