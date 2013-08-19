76dcc2b6c6642af46b4bf3cd22671959f0eb8d7f
jwgwarren <warren.jonathan@gmail.com>
2013-08-19 16:41:09 +0100

    Adding new file to Git

--- src/main/java/uk/ac/ebi/phenotype/pojo/Project.java
+++ src/main/java/uk/ac/ebi/phenotype/pojo/Project.java
@@ -23,6 +23,7 @@ package uk.ac.ebi.phenotype.pojo;
  * @since February 2012
  */
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -36,7 +37,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "project")
-public class Project {
+public class Project implements Serializable {
+	private static final long serialVersionUID = 3861408046906205232L;
 
 	@Id
 	@Column(name = "id")
@@ -123,6 +125,62 @@ public class Project {
 	 */
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result
+				+ ((fullname == null) ? 0 : fullname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Project other = (Project) obj;
+		if (description == null) {
+			if (other.description != null) {
+				return false;
+			}
+		} else if (!description.equals(other.description)) {
+			return false;
+		}
+		if (fullname == null) {
+			if (other.fullname != null) {
+				return false;
+			}
+		} else if (!fullname.equals(other.fullname)) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
 	}
 	
 	
