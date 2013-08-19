aeb313b5c992926efb76a1f410920278f44bbab2
jwgwarren <warren.jonathan@gmail.com>
2013-08-19 16:41:17 +0100

    add method to get genes by parameter/organisation/strain/zygosity combinations

--- src/main/java/uk/ac/ebi/phenotype/dao/ObservationDAOImpl.java
+++ src/main/java/uk/ac/ebi/phenotype/dao/ObservationDAOImpl.java
@@ -22,13 +22,15 @@ package uk.ac.ebi.phenotype.dao;
  * @since May 2012
  */
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.ebi.phenotype.pojo.BiologicalSample;
@@ -40,15 +42,282 @@ import uk.ac.ebi.phenotype.pojo.Observation;
 import uk.ac.ebi.phenotype.pojo.ObservationType;
 import uk.ac.ebi.phenotype.pojo.Organisation;
 import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.SexType;
+import uk.ac.ebi.phenotype.pojo.TextObservation;
 import uk.ac.ebi.phenotype.pojo.TimeSeriesObservation;
 import uk.ac.ebi.phenotype.pojo.UnidimensionalObservation;
+import uk.ac.ebi.phenotype.pojo.ZygosityType;
 
-@Service
 public class ObservationDAOImpl extends HibernateDAOImpl implements ObservationDAO {
 	
-	@Autowired
-	SessionFactory sessionFactory;
+	/**
+	 * Creates a new Hibernate project data access manager.
+	 * @param sessionFactory the Hibernate session factory
+	 */
+	public ObservationDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Organisation> getAllOrganisationsWithObservations() {
+		return getCurrentSession().createQuery("select distinct org from Observation as o inner join o.sample as s inner join s.organisation org").list();
+	}
+
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Parameter> getAllParametersWithObservations() {
+		return getCurrentSession().createQuery("select distinct param from Observation as o inner join o.parameter as param").list();
+	}
 	
+	@Transactional(readOnly = true)
+	public List<Integer> getAllParameterIdsWithObservationsByOrganisation(Organisation organisation) throws SQLException {
+	    List<Integer> parameterIds = new ArrayList<Integer>();
+
+		String query = "SELECT DISTINCT p.id"
+		+ " FROM phenotype_parameter p"
+		+ " INNER JOIN observation o ON o.parameter_id=p.id"
+		+ " INNER JOIN biological_sample bs ON bs.id=o.biological_sample_id"
+		+ " WHERE bs.organisation_id=?"
+		;
+
+		try (PreparedStatement statement = getConnection().prepareStatement(query)){
+	        statement.setInt(1, organisation.getId());
+		    ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				parameterIds.add(resultSet.getInt("id"));
+			}
+		}
+
+		return parameterIds; 	
+	}
+
+	@Transactional(readOnly = true)
+	public List<Integer> getAllCategoricalParameterIdsWithObservationsByOrganisation(Organisation organisation) throws SQLException {
+	    List<Integer> parameterIds = new ArrayList<Integer>();
+
+		String query = "SELECT DISTINCT p.id"
+		+ " FROM phenotype_parameter p"
+		+ " INNER JOIN observation o ON o.parameter_id=p.id"
+		+ " INNER JOIN biological_sample bs ON bs.id=o.biological_sample_id"
+		+ " WHERE bs.organisation_id=?"
+		+ " AND o.observation_type='categorical'"
+		;
+
+		try (PreparedStatement statement = getConnection().prepareStatement(query)){
+	        statement.setInt(1, organisation.getId());
+		    ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				parameterIds.add(resultSet.getInt("id"));
+			}
+		}
+
+		return parameterIds; 	
+	}
+
+	public List<Integer> getAllUnidimensionalParameterIdsWithObservationsByOrganisation(Organisation organisation) throws SQLException {
+	    List<Integer> parameterIds = new ArrayList<Integer>();
+
+		String query = "SELECT DISTINCT p.id"
+		+ " FROM phenotype_parameter p"
+		+ " INNER JOIN observation o ON o.parameter_id=p.id"
+		+ " INNER JOIN biological_sample bs ON bs.id=o.biological_sample_id"
+		+ " WHERE bs.organisation_id=?"
+		+ " AND o.observation_type='unidimensional'"
+		;
+
+		try (PreparedStatement statement = getConnection().prepareStatement(query)){
+	        statement.setInt(1, organisation.getId());
+		    ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				parameterIds.add(resultSet.getInt("id"));
+			}
+		}
+
+		return parameterIds; 			
+	}
+
+	
+	@Transactional(readOnly = true)
+	public Observation getObservationById(Integer obsId) {
+		return (Observation) getCurrentSession().createQuery("select distinct o from Observation as o where o.id=?")
+			.setInteger(0, obsId)
+			.uniqueResult();
+	}
+
+
+	public List<String> getAllStrainsByParameterOrganistion(Parameter parameter, Organisation organisation) throws SQLException {
+		List<String> strains = new ArrayList<String>();
+
+		String query = "SELECT DISTINCT strain_acc" 
+		+ " FROM observation obs"
+		+ " INNER JOIN biological_sample bs ON obs.biological_sample_id=bs.id"
+		+ " INNER JOIN biological_model_sample bms ON bms.biological_sample_id=bs.id"
+		+ " INNER JOIN biological_model bm ON bms.biological_model_id=bm.id"
+		+ " INNER JOIN biological_model_strain strain ON strain.biological_model_id=bm.id"
+		+ " WHERE parameter_id=?"
+		+ " AND bs.organisation_id=?";
+
+		try (PreparedStatement statement = getConnection().prepareStatement(query)){
+	        statement.setInt(1, parameter.getId());
+	        statement.setInt(2, organisation.getId());
+		    ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				strains.add(resultSet.getString("strain_acc"));
+			}
+		}
+
+		return strains;
+	}
+
+	public List<Integer> getAllObservationIdsByParameterGeneAccZygosityOrganisationStrainSex(Parameter parameter, String geneAcc, ZygosityType zygosity, Organisation organisation, String strain, SexType sex) throws SQLException {
+	    List<Integer> ids = new ArrayList<Integer>();
+
+		// Get all the experimental observation IDs
+		String query = "SELECT DISTINCT o.id"
+		+ " FROM observation o"
+		+ " INNER JOIN biological_sample bs ON o.biological_sample_id=bs.id"
+		+ " INNER JOIN live_sample ls ON ls.id=bs.id"
+		+ " INNER JOIN biological_model_sample bms ON bms.biological_sample_id=bs.id"
+		+ " INNER JOIN biological_model bm ON bms.biological_model_id=bm.id"
+		+ " INNER JOIN biological_model_genomic_feature bmgf ON bm.id=bmgf.biological_model_id"
+		+ " INNER JOIN biological_model_strain strain on strain.biological_model_id=bm.id"
+		+ " WHERE o.parameter_id=?"
+		+ " AND o.missing!=1"
+		+ " AND bmgf.gf_acc=?"
+		+ " AND bs.organisation_id=?"
+		+ " AND ls.zygosity=?"
+		+ " AND ls.sex=?"
+		+ " AND strain.strain_acc=?"
+		+ " AND bs.sample_group='experimental'"
+		;
+
+		try (PreparedStatement statement = getConnection().prepareStatement(query)){
+	        statement.setInt(1, parameter.getId());
+	        statement.setString(2, geneAcc);
+	        statement.setInt(3, organisation.getId());
+	        statement.setString(4, zygosity.name());
+	        statement.setString(5, sex.name());
+	        statement.setString(6, strain);
+		    ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				ids.add(resultSet.getInt("id"));
+			}
+		}
+
+		// Add in the control observation IDs (do not take into account the zygosity of the control samples) 
+		query = "SELECT DISTINCT o.id"
+		+ " FROM observation o"
+		+ " INNER JOIN biological_sample bs ON o.biological_sample_id=bs.id"
+		+ " INNER JOIN live_sample ls ON ls.id=bs.id"
+		+ " INNER JOIN biological_model_sample bms ON bms.biological_sample_id=bs.id"
+		+ " INNER JOIN biological_model_strain strain on strain.biological_model_id=bms.biological_model_id"
+		+ " INNER JOIN experiment_observation eo ON eo.observation_id=o.id"
+		+ " INNER JOIN experiment e ON e.id=eo.experiment_id"
+		+ " WHERE o.parameter_id=?"
+		+ " AND o.missing!=1"
+		+ " AND e.organisation_id=?"
+		+ " AND ls.sex=?"
+		+ " AND strain.strain_acc=?"
+		+ " AND bs.sample_group='control'"
+		;
+		
+		try (PreparedStatement statement = getConnection().prepareStatement(query)){
+	        statement.setInt(1, parameter.getId());
+	        statement.setInt(2, organisation.getId());
+	        statement.setString(3, sex.name());
+	        statement.setString(4, strain);
+		    ResultSet resultSet = statement.executeQuery();
+		    int count = 0;
+			while (resultSet.next()) {
+				ids.add(resultSet.getInt("id"));
+				count++;
+			}
+			// If there is no control data, don't save this as a population
+			if(count==0) ids.clear();
+		}
+
+		return ids; 	
+	}
+	
+	@Transactional(readOnly = true)
+	public List<String> getAllGeneAccessionIdsByParameterOrganisationStrainZygositySex(Parameter parameter, Organisation organisation, String strain, ZygosityType zygosity, SexType sex) throws SQLException {
+	    List<String> genes = new ArrayList<String>();
+
+		String query = "SELECT DISTINCT bmgf.gf_acc"
+		+ " FROM observation o"
+		+ " INNER JOIN biological_sample bs ON o.biological_sample_id=bs.id"
+		+ " INNER JOIN live_sample ls ON ls.id=bs.id"
+		+ " INNER JOIN biological_model_sample bms ON bms.biological_sample_id=bs.id"
+		+ " INNER JOIN biological_model bm ON bms.biological_model_id=bm.id"
+		+ " INNER JOIN biological_model_genomic_feature bmgf ON bm.id=bmgf.biological_model_id"
+		+ " INNER JOIN biological_model_strain strain on strain.biological_model_id=bm.id"
+		+ " WHERE o.parameter_id=?"
+		+ " AND bs.organisation_id=?"
+		+ " AND strain.strain_acc=?"
+		+ " AND ls.zygosity=?"
+		+ " AND ls.sex=?"
+		;
+
+		try (PreparedStatement statement = getConnection().prepareStatement(query)){
+	        statement.setInt(1, parameter.getId());
+	        statement.setInt(2, organisation.getId());
+	        statement.setString(3, strain);
+	        statement.setString(4, zygosity.name());
+	        statement.setString(5, sex.name());
+		    ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				genes.add(resultSet.getString("gf_acc"));
+			}
+		}
+
+		return genes; 	
+	}
+
+	@Transactional(readOnly = true)
+	public List<String> getAllGeneAccessionIdsByParameterOrganisationStrainZygosity(Parameter parameter, Organisation organisation, String strain, ZygosityType zygosity) throws SQLException {
+	    List<String> genes = new ArrayList<String>();
+
+		String query = "SELECT DISTINCT bmgf.gf_acc"
+		+ " FROM observation o"
+		+ " INNER JOIN biological_sample bs ON o.biological_sample_id=bs.id"
+		+ " INNER JOIN live_sample ls ON ls.id=bs.id"
+		+ " INNER JOIN biological_model_sample bms ON bms.biological_sample_id=bs.id"
+		+ " INNER JOIN biological_model bm ON bms.biological_model_id=bm.id"
+		+ " INNER JOIN biological_model_genomic_feature bmgf ON bm.id=bmgf.biological_model_id"
+		+ " INNER JOIN biological_model_strain strain on strain.biological_model_id=bm.id"
+		+ " WHERE o.parameter_id=?"
+		+ " AND bs.organisation_id=?"
+		+ " AND strain.strain_acc=?"
+		+ " AND ls.zygosity=?"
+		;
+
+		try (PreparedStatement statement = getConnection().prepareStatement(query)){
+	        statement.setInt(1, parameter.getId());
+	        statement.setInt(2, organisation.getId());
+	        statement.setString(3, strain);
+	        statement.setString(4, zygosity.name());
+		    ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				genes.add(resultSet.getString("gf_acc"));
+			}
+		}
+
+		return genes; 	
+
+	}
+
+	
+	
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Observation> getAllObservationsByParameter(Parameter parameter) {
+		return getCurrentSession().createQuery("select distinct o from Observation as o where o.parameterId=?")
+			.setInteger(1, parameter.getId())
+			.list();
+	}
+
+
 	@Transactional(readOnly = true)
 	@SuppressWarnings("unchecked")
 	public List<Observation> getObservationsBySampleIdAndParameterStableId(
@@ -233,7 +502,6 @@ public class ObservationDAOImpl extends HibernateDAOImpl implements ObservationD
 			
 			logger.debug("Metadata: " + firstDimensionValue);
 			MetaDataObservation metaDataObservation = new MetaDataObservation();
-			metaDataObservation.setKey(secondDimensionValue);
 			metaDataObservation.setValue(firstDimensionValue);
 			metaDataObservation.setDatasource(datasource);
 			metaDataObservation.setExperiment(experiment);
@@ -289,9 +557,25 @@ public class ObservationDAOImpl extends HibernateDAOImpl implements ObservationD
 			unidimensionalObservation.setType(observationType);
 
 			obs = unidimensionalObservation;
+		} else if (observationType == ObservationType.text) {
+
+			 /* Unidimensional information */								 
+
+			logger.debug("Text :" + firstDimensionValue);
+			TextObservation textObservation = new TextObservation();
+		
+			textObservation.setText(firstDimensionValue);
+			textObservation.setDatasource(datasource);
+			textObservation.setExperiment(experiment);
+			textObservation.setParameter(parameter);
+			textObservation.setSample(sample);
+			textObservation.setType(observationType);
+
+			obs = textObservation;
 		}
 		
 		obs.setPopulationId(populationId);
+		obs.setParameterStableId(parameter.getStableId());
 		
 		return obs;
 	}
