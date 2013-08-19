833849fff8d8509e65c621935bf05fa9271c2cab
jwgwarren <warren.jonathan@gmail.com>
2013-08-19 16:41:14 +0100

    Adding new file to Git

--- src/main/java/uk/ac/ebi/phenotype/dao/DatasourceDAOImpl.java
+++ src/main/java/uk/ac/ebi/phenotype/dao/DatasourceDAOImpl.java
@@ -26,17 +26,19 @@ package uk.ac.ebi.phenotype.dao;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.ebi.phenotype.pojo.Datasource;
 
-@Service
 public class DatasourceDAOImpl extends HibernateDAOImpl implements DatasourceDAO {
 
-	@Autowired
-	SessionFactory sessionFactory;
+	/**
+	 * Creates a new Hibernate coordinate system data access manager.
+	 * @param sessionFactory the Hibernate session factory
+	 */
+	public DatasourceDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 	
 	@Transactional(readOnly = true)
 	@SuppressWarnings("unchecked")
