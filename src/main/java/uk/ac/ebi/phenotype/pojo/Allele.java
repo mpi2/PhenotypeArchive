354089965d937b5f3e9c5522d311d79454284bba
jwgwarren <warren.jonathan@gmail.com>
2013-08-19 16:41:03 +0100

    Phenotype page contains phenoypes panel created using solr allele call

--- src/main/java/uk/ac/ebi/phenotype/pojo/Allele.java
+++ src/main/java/uk/ac/ebi/phenotype/pojo/Allele.java
@@ -119,6 +119,55 @@ public class Allele {
 	public void setSymbol(String symbol) {
 		this.symbol = symbol;
 	}
+
+	@Override
+	public String toString() {
+		return "Allele [id=" + id + ", gene=" + gene + ", name=" + name
+				+ ", symbol=" + symbol + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((gene == null) ? 0 : gene.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((symbol == null) ? 0 : symbol.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Allele other = (Allele) obj;
+		if (gene == null) {
+			if (other.gene != null)
+				return false;
+		} else if (!gene.equals(other.gene))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (symbol == null) {
+			if (other.symbol != null)
+				return false;
+		} else if (!symbol.equals(other.symbol))
+			return false;
+		return true;
+	}
 	
 	
 }
