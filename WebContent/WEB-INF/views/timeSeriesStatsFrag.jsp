64519fb6bfb6f18132946303e731914b8937b427
jwgwarren <warren.jonathan@gmail.com>
2013-09-19 16:39:59 +0100

    stats page split into fragments for each statistical graph type

--- WebContent/WEB-INF/views/timeSeriesStatsFrag.jsp
+++ WebContent/WEB-INF/views/timeSeriesStatsFrag.jsp
@@ -2,44 +2,6 @@
 <%@taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
 <%@taglib prefix="t" tagdir="/WEB-INF/tags"%>
 
-<t:genericpage>
-	<jsp:attribute name="title">Stats for ${gene.name}</jsp:attribute>
-
-	<jsp:attribute name="breadcrumb">&nbsp;&raquo; <a
-			href="${baseUrl}/search#sort=marker_symbol asc&q=*:*&core=gene">Genes</a> <c:if
-			test="${not empty gene.subtype.name }">&raquo; <a
-				href='${baseUrl}/search#fq=marker_type:"${gene.subtype.name}"&q=*:*&core=gene'>${gene.subtype.name}</a>
-		</c:if> &raquo; ${gene.symbol}</jsp:attribute>
-
-	<jsp:attribute name="header">
-
-	<script type="text/javascript">
-		var gene_id = '${acc}';
-	</script>
-
-	<!--    extra header stuff goes here such as extra page specific javascript -->
-	<!-- highcharts -->
-<script type='text/javascript' src='${baseUrl}/js/charts/highcharts.js'></script>
- <script type='text/javascript' src='${baseUrl}/js/charts/highcharts-more.js'></script> 
-<script src="${baseUrl}/js/charts/exporting.js"></script>
-	
-
-	
-
-<style>
-</style>
-
-    </jsp:attribute>
-
-	<jsp:body>
-        
-		<div class='topic'>Gene: ${gene.symbol}</div>
-		<c:if test="${statsError}">
-					<div class="alert alert-error">
-							  				<strong>Error:</strong> A stats error occured - results on this page maybe incorrect.
-					</div>
-		</c:if>
-	
 
 <!-- time series charts here-->
 
@@ -70,19 +32,3 @@
 		 </div>
 </c:if>
 </c:forEach>
-
-
-
-<jsp:include page="categoricalStatsFrag.jsp"/>
-
-<jsp:include page="unidimensionalStatsFrag.jsp"/>
-
-
-		
- 
-
-
-
-
-    </jsp:body>
-</t:genericpage>
---
+++ src/main/java/uk/ac/ebi/phenotype/stats/timeseries/TimeSeriesStats.java
@@ -0,0 +1,65 @@
+package uk.ac.ebi.phenotype.stats.timeseries;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.apache.commons.collections.map.LinkedMap;
+import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
+
+import uk.ac.ebi.phenotype.dao.DiscreteTimePoint;
+
+public class TimeSeriesStats {
+
+	
+	
+
+	public List<DiscreteTimePoint> getMeanDataPoints(List<DiscreteTimePoint> timeSeriesDataForLine) {
+		List<DiscreteTimePoint> means=new ArrayList<>();
+		
+
+			// Add the data from the array
+		 SortedMap<Float, List<Float>> timeMap=new TreeMap<Float, List<Float>>();
+		 for(DiscreteTimePoint timePoint: timeSeriesDataForLine) {
+			 if(timeMap.containsKey(timePoint.getDiscreteTime())) {
+				 timeMap.get(timePoint.getDiscreteTime()).add(timePoint.getData());
+			 }else {
+				 List<Float> dataPointsFloats=new ArrayList<Float>();
+				 dataPointsFloats.add(timePoint.getData());
+				 timeMap.put(timePoint.getDiscreteTime(), dataPointsFloats);
+			 }
+			 
+		 }
+		 System.out.println("time map size="+timeMap.keySet().size());
+		 
+			for(Float time:  timeMap.keySet()) {
+				 DescriptiveStatistics stats = new DescriptiveStatistics();
+				System.out.println("time="+time+" number of points="+ timeMap.get(time).size());
+				for(Float data: timeMap.get(time)) {
+			        stats.addValue(data);
+				}
+				
+				// Compute some statistics
+				double mean = stats.getMean();
+				double std = stats.getStandardDeviation();
+				double median = stats.getPercentile(50);
+				DiscreteTimePoint meanDataTimePoint=new DiscreteTimePoint(time, new Float(mean), new Float(stats.getStandardDeviation()));
+				List<Float> errorPair=new ArrayList<>();
+				
+				Float lower=new Float(mean-std);
+				Float higher=new Float(mean+std);
+				errorPair.add(lower);
+				errorPair.add(higher);
+				System.out.println("stddev="+std+" lower="+lower+" higher="+higher);
+				meanDataTimePoint.setErrorPair(errorPair);
+				//meanDataTimePoint.s
+				means.add(meanDataTimePoint);
+				
+			}
+
+			
+			return means;
+	}
+}
