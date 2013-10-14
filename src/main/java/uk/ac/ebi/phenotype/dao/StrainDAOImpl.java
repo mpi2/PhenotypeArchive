03fdac42e594caad987081de06ec1ee4dddb2810
Jeremy Mason <jmason@ebi.ac.uk>
2013-10-14 16:49:27 +0100

    add method for save or update strain

--- src/main/java/uk/ac/ebi/phenotype/dao/StrainDAOImpl.java
+++ src/main/java/uk/ac/ebi/phenotype/dao/StrainDAOImpl.java
@@ -29,53 +29,61 @@ import org.hibernate.SessionFactory;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.ebi.phenotype.pojo.Allele;
+import uk.ac.ebi.phenotype.pojo.BiologicalModel;
 import uk.ac.ebi.phenotype.pojo.Strain;
 
 
 public class StrainDAOImpl extends HibernateDAOImpl implements StrainDAO {
 
-	/**
-	 * Creates a new Hibernate sequence region data access manager.
-	 * @param sessionFactory the Hibernate session factory
-	 */
-	public StrainDAOImpl(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
+        /**
+         * Creates a new Hibernate sequence region data access manager.
+         * @param sessionFactory the Hibernate session factory
+         */
+        public StrainDAOImpl(SessionFactory sessionFactory) {
+                this.sessionFactory = sessionFactory;
+        }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public List<Strain> getAllStrains() {
-		return getCurrentSession().createQuery("from Strain").list();
-	}
+        @Override
+        @SuppressWarnings("unchecked")
+        public List<Strain> getAllStrains() {
+                return getCurrentSession().createQuery("from Strain").list();
+        }
 
-	@Transactional(readOnly = true)
-	public Strain getStrainByName(String name) {
-		return (Strain) getCurrentSession().createQuery("from Strain as s where s.name= ?").setString(0, name).uniqueResult();
-	}
-	
-	@Transactional(readOnly = true)
-	public Strain getStrainByAcc(String acc) {
-		return (Strain) getCurrentSession().createQuery("from Strain as s where s.id.accession= ?").setString(0, acc).uniqueResult();
-	}
+        @Transactional(readOnly = true)
+        public Strain getStrainByName(String name) {
+                return (Strain) getCurrentSession().createQuery("from Strain as s where s.name= ?").setString(0, name).uniqueResult();
+        }
+        
+        @Transactional(readOnly = true)
+        public Strain getStrainByAcc(String acc) {
+                return (Strain) getCurrentSession().createQuery("from Strain as s where s.id.accession= ?").setString(0, acc).uniqueResult();
+        }
 
 
-	@Transactional(readOnly = true)
-	public Strain getStrainBySynonym(String name) {
-		// select mother from Cat as mother, Cat as kit
-		//where kit in elements(foo.kittens)
-		//return (Strain) getCurrentSession().createQuery("select strain from Strain as strain, Synonym as synonym where synonym in elements(strain.synonyms) and synonym.symbol = ?").setString(0, name).uniqueResult();
-		return (Strain) getCurrentSession().createQuery("from Strain as strain left join strain.synonyms as synonym where synonym.symbol = ?").setString(0, name).uniqueResult();
-	}
+        @Transactional(readOnly = true)
+        public Strain getStrainBySynonym(String name) {
+                // select mother from Cat as mother, Cat as kit
+                //where kit in elements(foo.kittens)
+                //return (Strain) getCurrentSession().createQuery("select strain from Strain as strain, Synonym as synonym where synonym in elements(strain.synonyms) and synonym.symbol = ?").setString(0, name).uniqueResult();
+                return (Strain) getCurrentSession().createQuery("from Strain as strain left join strain.synonyms as synonym where synonym.symbol = ?").setString(0, name).uniqueResult();
+        }
 
-	@Transactional(readOnly = false)
-	public void saveStrain(Strain strain) {
-		try {
-			getCurrentSession().merge(strain);
-		} catch (Exception e) {
-			e.printStackTrace();
-			getCurrentSession().saveOrUpdate(strain);
-		}
+        @Transactional(readOnly = false)
+        public void saveStrain(Strain strain) {
+                try {
+                        getCurrentSession().merge(strain);
+                } catch (Exception e) {
+                        e.printStackTrace();
+                        getCurrentSession().saveOrUpdate(strain);
+                }
 
-	}
+        }
+        
+        @Transactional(readOnly = false)
+        public void saveOrUpdateStrain(Strain strain) {
+                getCurrentSession().saveOrUpdate(strain);
+                getCurrentSession().flush();
+        }
 
 }
+
