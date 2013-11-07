001905d76ae6909fcee126d892c1ad637aeef693
Ilinca Tudose <ilinca.tudose@gmail.com>
2013-11-07 14:56:13 +0000

    Changed getControls method to do less solr calls (and eventually work faster)

--- src/main/java/uk/ac/ebi/phenotype/stats/ObservationService.java
+++ src/main/java/uk/ac/ebi/phenotype/stats/ObservationService.java
@@ -109,7 +109,78 @@ public class ObservationService {
 			e.printStackTrace();
 		} // Jan 1, 2000 UTC
 		
-		int resSize = 0;
+		SolrQuery queryb = new SolrQuery()
+		.setQuery("*:*")
+		.addFilterQuery("biologicalSampleGroup:control")
+		.addFilterQuery("dateOfExperiment:["+df.format(minDate)+"Z TO "+df.format(max)+"Z]")
+		.addFilterQuery("parameterId:"+parameterId)
+		.addFilterQuery("organisationId:"+organisationId)
+		.addFilterQuery("strain:"+strain.replace(":", "\\:"))
+		.addFilterQuery("gender:"+sex)
+		.setSortField("dateOfExperiment", ORDER.desc) // good for dates before the date of the experiment
+		;
+		queryb.setStart(0).setRows(resultsMaxSize);
+		
+		SolrQuery querya = new SolrQuery()
+		.setQuery("*:*")
+		.addFilterQuery("biologicalSampleGroup:control")
+		.addFilterQuery("dateOfExperiment:["+df.format(DateUtils.addDays(max, -1))+"Z TO "+df.format(today)+"Z]")
+		.addFilterQuery("parameterId:"+parameterId)
+		.addFilterQuery("organisationId:"+organisationId)
+		.addFilterQuery("strain:"+strain.replace(":", "\\:"))
+		.addFilterQuery("gender:"+sex)
+		.setSortField("dateOfExperiment", ORDER.asc) // good for dates after the date of the experiment
+		;
+		querya.setStart(0).setRows(resultsMaxSize);
+
+		responseb = solr.query(queryb);
+		responsea = solr.query(querya);
+		
+		List<ObservationDTO> resA = responsea.getBeans(ObservationDTO.class);
+		List<ObservationDTO> resB = responseb.getBeans(ObservationDTO.class);
+		ArrayList<ObservationDTO> closest = new ArrayList<ObservationDTO>();
+		long datInMs = max.getTime();
+		
+		while (results.size() < resultsMaxSize && resA.size() > 0 && resB.size() > 0){
+			closest = getClosest(datInMs, resA.get(0), resB.get(0));
+			// (b, a)
+			if (closest.get(0) != null){
+				results.add(closest.get(0));
+				resA.remove(0);
+			}
+			if (closest.get(1) != null){
+				results.add(closest.get(1));
+				resA.remove(0);
+			}
+		}
+		
+//		results.addAll(responsea.getBeans(ObservationDTO.class).subList(0, neededAfter));
+//		results.addAll(responseb.getBeans(ObservationDTO.class).subList(0, neededBefore));
+
+//		System.out.println(solr.getBaseURL() + querya);
+//		System.out.println(solr.getBaseURL() + queryb);
+		
+//		System.out.println("------Got after : " + sizeA );
+//		System.out.println("------Got before : " + sizeB );
+//		System.out.println("------Return total " + results.size());
+		
+	/*/
+	 * 
+http://ves-ebi-d0.ebi.ac.uk:8090/mi/impc/dev/solr/experimentq=*%3A*&fq=biologicalSampleGroup%3Acontrol&fq=dateOfExperiment%3A%5B2009-12-14T00%3A00%3A00Z+TO+2013
+-11-07T00%3A00%3A00Z%5D&fq=parameterId%3A1326&fq=organisationId%3A8&fq=strain%3AMGI%5C%3A2164831&fq=gender%3Amale&sort=dateOfExperiment+asc
+
+http://ves-ebi-d0.ebi.ac.uk:8090/mi/impc/dev/solr/experimentq=*%3A*&fq=biologicalSampleGroup%3Acontrol&fq=dateOfExperiment%3A%5B2009-12-14T00%3A00%3A00Z+TO+2010
+-12-15T00%3A00%3A00Z%5D&fq=parameterId%3A1326&fq=organisationId%3A8&fq=strain%3AMGI%5C%3A2164831&fq=gender%3Amale&sort=dateOfExperiment+asc&start=0&rows=500
+------Got after : 10 take 10
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+/*		int resSize = 0;
 		int loop = 1;
 		
 		while (resSize < resultsMaxSize && withinTimeLimits) {
@@ -173,19 +244,44 @@ public class ObservationService {
 	//		System.out.println("resSizeA : " + resSizeA);
 	//		System.out.println("resSizeB : " + resSizeB);
 			
+
+			System.out.println(solr.getBaseURL() + querya);
+			System.out.println(solr.getBaseURL() + querybs);
 			loop++;
 		}
 
 		results.addAll(responsea.getBeans(ObservationDTO.class));
 		results.addAll(responseb.getBeans(ObservationDTO.class));
 		
-		System.out.println("returning : " + results.size() + " for " + sex);
-//		System.out.println("---- I RETURN :" + results.size() + " , " + responsea.getResults().size() + " + " + responseb.getResults().size()) ;
+		
 		
 //		for (ObservationDTO obs : results){
 //				System.out.println(obs);
 //			}
+ //*/
+		System.out.println("returning : " + results.size() + " for " + sex);
+//		System.out.println("---- I RETURN :" + results.size() + " , " + responsea.getResults().size() + " + " + responseb.getResults().size()) ;
 		return results;
+	}
+
+	// list <b,a>
+	private ArrayList<ObservationDTO> getClosest (long datInMs, ObservationDTO aObj, ObservationDTO bObj){
+		ArrayList<ObservationDTO> res = new ArrayList<>();
+		long distanceA = aObj.getDateOfExperiment().getTime() - datInMs;
+		long distanceB = datInMs - bObj.getDateOfExperiment().getTime();
+		if (distanceA == distanceB){
+			res.add(aObj);
+			res.add(bObj);
+		}
+		else if (distanceA < distanceB){
+			res.add(aObj);
+			res.add(null);
+		}
+		else if (distanceB < distanceA){
+			res.add(null);
+			res.add(bObj);
+		}
+		return res;
 	}
 	
 	/**
