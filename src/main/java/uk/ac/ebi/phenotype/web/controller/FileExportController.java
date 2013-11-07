b56760194a8f97d2be4d5e08836b947042c70e8e
Chao-Kung Chen <ckchen@ebi.ac.uk>
2013-11-07 17:03:51 +0000

    temporarily display as MGP instead of IMPC before Harwell data is integrated

--- src/main/java/uk/ac/ebi/phenotype/web/controller/FileExportController.java
+++ src/main/java/uk/ac/ebi/phenotype/web/controller/FileExportController.java
@@ -620,7 +620,7 @@ public class FileExportController {
 		JSONArray docs = json.getJSONObject("response").getJSONArray("docs");	
 		
 		List<String> rowData = new ArrayList<String>();
-		rowData.add("Disease id\tDisease name\tSource\tCurated genes in human\tCurated genes in mouse (MGI)\tCandidate genes by phenotype (IMPC)\tCandidate genes by phenotype (MGI)"); // column names	
+		rowData.add("Disease id\tDisease name\tSource\tCurated genes in human\tCurated genes in mouse (MGI)\tCandidate genes by phenotype (MGP)\tCandidate genes by phenotype (MGI)"); // column names	
 		
 		for (int i=0; i<docs.size(); i++) {			
 			List<String> data = new ArrayList<String>();
