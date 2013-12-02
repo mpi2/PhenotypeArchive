36015a34b168d4208a93a3e129c5935a1d71ed26
Ilinca Tudose <ilinca.tudose@gmail.com>
2013-12-02 13:02:16 +0000

    cleaned imports

--- src/main/java/uk/ac/ebi/phenotype/pojo/PipelineSolrImpl.java
+++ src/main/java/uk/ac/ebi/phenotype/pojo/PipelineSolrImpl.java
@@ -2,15 +2,7 @@ package uk.ac.ebi.phenotype.pojo;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-import uk.ac.ebi.generic.util.JSONRestUtil;
 import uk.ac.ebi.phenotype.stats.GenotypePhenotypeService;
 
 public class PipelineSolrImpl {
