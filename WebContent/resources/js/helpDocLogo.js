1c58da55111c8b8479059509336be313889a8d34
Jeremy Mason <jmason@ebi.ac.uk>
2014-01-09 12:40:27 +0000

    remove combined css file

--- WebContent/resources/js/helpDocLogo.js
+++ WebContent/resources/js/helpDocLogo.js
@@ -1,7 +1,9 @@
+// Helper function to assign the target URL of the logo to the bare domain
+// (whatever it is) while preserving the protocol
 
 $(document).ready(function(){
 	var url = window.location.href;		
 	var arr = url.split("/");
-	var domain = arr[0] + "//" + arr[2]
+	var domain = arr[0] + "//" + arr[2];
 	$('a#logo').attr('href', domain);	
 });
