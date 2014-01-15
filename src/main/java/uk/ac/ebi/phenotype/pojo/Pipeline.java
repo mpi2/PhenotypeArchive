7d81c2b9bf0f55226a0e45b10eb3477749299e26
Jeremy Mason <jmason@ebi.ac.uk>
2014-01-15 15:42:11 +0000

    oops, forgot to push, so I didn't really test the changes

--- src/main/java/uk/ac/ebi/phenotype/pojo/Pipeline.java
+++ src/main/java/uk/ac/ebi/phenotype/pojo/Pipeline.java
@@ -1,29 +1,28 @@
 /**
  * Copyright © 2011-2013 EMBL - European Bioinformatics Institute
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License.  
- * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package uk.ac.ebi.phenotype.pojo;
 
 /**
- * 
+ *
  * Representation of a phenotype pipeline along with its procedures.
- * 
+ *
  * @author Gautier Koscielny (EMBL-EBI) <koscieln@ebi.ac.uk>
  * @since February 2012
  * @see PipelineEntry
  */
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -40,40 +39,34 @@ import javax.persistence.Table;
 @Table(name = "phenotype_pipeline")
 public class Pipeline extends PipelineEntry {
 
-	@ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
-            name="phenotype_pipeline_procedure",
-            joinColumns = @JoinColumn( name="pipeline_id"),
-            inverseJoinColumns = @JoinColumn( name="procedure_id")
+            name = "phenotype_pipeline_procedure",
+            joinColumns = @JoinColumn(name = "pipeline_id"),
+            inverseJoinColumns = @JoinColumn(name = "procedure_id")
     )
-	private Set<Procedure> procedures;
-	
-	public Pipeline() {
-		super();
-	}
+    private Set<Procedure> procedures= new HashSet<>();
 
-	public void addProcedure(Procedure procedure) {
-		
-		if (procedures == null) {
-			procedures = new HashSet<Procedure>();
-		}
-		procedures.add(procedure);
-		
-	}
-	
-	/**
-	 * @return the procedures
-	 */
-	public Set<Procedure> getProcedures() {
-		return procedures;
-	}
+    public Pipeline() {
+        super();
+    }
 
-	/**
-	 * @param procedures the procedures to set
-	 */
-	public void setProcedures(Set<Procedure> procedures) {
-		this.procedures = procedures;
-	}
-	
-	
+    public void addProcedure(Procedure procedure) {
+        procedures.add(procedure);
+    }
+
+    /**
+     * @return the procedures
+     */
+    public Set<Procedure> getProcedures() {
+        return procedures;
+    }
+
+    /**
+     * @param procedures the procedures to set
+     */
+    public void setProcedures(Set<Procedure> procedures) {
+        this.procedures = procedures;
+    }
+
 }
