801ec554be566af0a5153f8257f7d5cde80eb008
Ilinca Tudose <ilinca.tudose@gmail.com>
2014-02-21 12:46:45 +0000

    Changed colours for overview graphs to be in synch with the new design

--- src/main/java/uk/ac/ebi/phenotype/stats/categorical/CategoricalChartAndTableProvider.java
+++ src/main/java/uk/ac/ebi/phenotype/stats/categorical/CategoricalChartAndTableProvider.java
@@ -278,9 +278,11 @@ public class CategoricalChartAndTableProvider {
 		
 		String chartId = "single-chart-div";//replace space in MRC Harwell with underscore so valid javascritp variable
 		String toolTipFunction = "	{ formatter: function() {         return \''+  this.series.name +': '+ this.y +' ('+ (this.y*100/this.total).toFixed(1) +'%)';   }    }";
+		List<String> colors=ChartColors.getFemaleMaleColorsRgba(ChartColors.alphaBox);
+		JSONArray colorArray = new JSONArray(colors);
 		String javascript = "$(document).ready(function() { chart = new Highcharts.Chart({ " 
-//				+" colors:"+colorArray
-				+" chart: { renderTo: '"
+				+" colors:"+colorArray
+				+", chart: { renderTo: '"
 				+ chartId
 				+ "', type: 'column' }, title: { text: '"
 				+ WordUtils.capitalize(title)
@@ -290,6 +292,7 @@ public class CategoricalChartAndTableProvider {
 				+ "}, yAxis: { min: 0, title: { text: 'Percent Occurrance' } ,  labels: {       formatter: function() { return this.value +'%';   }  }},  plotOptions: { column: { stacking: 'percent' } }, series: "
 				+ seriesArray + " });  });";
 
+		
 		chartData.setChart(javascript);
 		chartData.setId(chartId);	
 		return javascript;
@@ -401,7 +404,8 @@ public class CategoricalChartAndTableProvider {
 		// categoricalBarCharts.add(javascript);
 		chartData.setChart(javascript);
 		chartData.setChartIdentifier(chartId);
-		
+
+		System.out.println("\n\n" + javascript);
 		return javascript;
 	}
 
