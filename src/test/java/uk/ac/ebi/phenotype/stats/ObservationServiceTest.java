72cce7ea24d55f30d86e1dab4c63fd2b4d9e2562
Gautier Koscielny <gkos.ebi@gmail.com>
2014-03-25 15:34:42 +0000

    Add method to retrieve data for specific combination of gene, allele and phenotyping center

--- src/test/java/uk/ac/ebi/phenotype/stats/ObservationServiceTest.java
+++ src/test/java/uk/ac/ebi/phenotype/stats/ObservationServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,6 +38,72 @@ public class ObservationServiceTest {
 	@Autowired
 	private OrganisationDAO organisationDAO;
 
+	@Test
+	public void testGetDistinctPipelineAlleleCenterListByGeneAccession() {
+		
+		// http://ves-ebi-d0.ebi.ac.uk:8090/mi/impc/dev/solr/experiment/select?q=gene_accession%3A%22MGI%3A104874%22%20AND%20biological_sample_group:experimental&wt=json&start=0&rows=0&indent=true&facet=true&facet.pivot=pipeline_id,phenotyping_center,allele_accession&facet.limit=-1
+		List<Map<String,String>> dataset = null;
+		String genomicFeatureAcc = "MGI:104874"; // Akt2
+			try {
+				dataset = os.getDistinctPipelineAlleleCenterListByGeneAccession(genomicFeatureAcc);
+				for (Map<String, String> map: dataset) {
+					for (String key: map.keySet()) {
+						System.out.println(key + ":" + map.get(key));
+					}
+					System.out.println();
+				}
+			} catch (SolrServerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		assertTrue(dataset.size()>0);
+	}
+	
+	@Test
+	public void testGetDistinctProcedureListByPipelineAlleleCenter() {
+
+		// http://ves-ebi-d0.ebi.ac.uk:8090/mi/impc/dev/solr/experiment/select?q=gene_accession%3A%22MGI%3A104874%22%20AND%20biological_sample_group:experimental&wt=json&start=0&rows=0&indent=true&facet=true&facet.pivot=pipeline_id,phenotyping_center,allele_accession&facet.limit=-1
+		List<String> dataset = null;
+		String phenotypingCenter = "WTSI"; 
+		String pipelineStableId = "ESLIM_001";
+		String alleleAccession = "EUROALL:19";
+
+		try {
+			dataset = os.getDistinctProcedureListByPipelineAlleleCenter(pipelineStableId, alleleAccession, phenotypingCenter);
+			for (String key: dataset) {
+				System.out.println(key);
+
+			}
+		} catch (SolrServerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue(dataset.size()>0);
+	}
+
+	@Test
+	public void testGetDistinctParameterListByPipelineAlleleCenter() {
+		
+		// http://ves-ebi-d0.ebi.ac.uk:8090/mi/impc/dev/solr/experiment/select?q=gene_accession%3A%22MGI%3A104874%22%20AND%20biological_sample_group:experimental&wt=json&start=0&rows=0&indent=true&facet=true&facet.pivot=pipeline_id,phenotyping_center,allele_accession&facet.limit=-1
+		List<Map<String,String>> dataset = null;
+		String phenotypingCenter = "WTSI"; 
+		String pipelineStableId = "ESLIM_001";
+		String alleleAccession = "EUROALL:19";
+		
+			try {
+				dataset = os.getDistinctParameterListByPipelineAlleleCenter(pipelineStableId, alleleAccession, phenotypingCenter, null);
+				for (Map<String, String> map: dataset) {
+					for (String key: map.keySet()) {
+						System.out.println(key + ":" + map.get(key));
+					}
+					System.out.println();
+				}
+			} catch (SolrServerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		assertTrue(dataset.size()>0);
+	}	
 	
 	@Test
 	public void testGetExperimentKeys() {
