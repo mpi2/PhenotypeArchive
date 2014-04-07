cb6b2739a11f4c20388fb1db69945bc091e4ed5c
Chao-Kung Chen <ckchen@ebi.ac.uk>
2014-04-07 15:43:23 +0100

    some preliminary search page test stuff

--- src/test/web/uk/ac/ebi/phenotype/web/SearchPageTest.java
+++ src/test/web/uk/ac/ebi/phenotype/web/SearchPageTest.java
@@ -21,8 +21,10 @@ import static org.junit.Assert.fail;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import net.sf.json.JSONArray;
@@ -36,9 +38,12 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import uk.ac.ebi.generic.util.JSONRestUtil;
 
@@ -52,7 +57,7 @@ public class SearchPageTest {
 	private WebDriver driver;
 	
 	private String baseUrl;
-	private String geneSolrUrl;
+	private String solrPath;
 	private StringBuffer verificationErrors = new StringBuffer();
 	
 	private static final String SELENIUM_SERVER_URL ="http://mi-selenium-win.windows.ebi.ac.uk:4444/wd/hub";
@@ -61,7 +66,7 @@ public class SearchPageTest {
 	public void setUp() throws Exception {
 		
 		baseUrl = "https://dev.mousephenotype.org";
-		geneSolrUrl = baseUrl + "/mi/impc/dev/solr";	
+		solrPath = baseUrl + "/mi/impc/dev/solr";	
 		
 	}
 	
@@ -98,7 +103,7 @@ public class SearchPageTest {
 		newQueryString+="&start="+startIndex+"&rows="+nbRows;
 		System.out.println("newQueryString=" + newQueryString);
 
-		JSONObject geneResults = JSONRestUtil.getResults(geneSolrUrl + newQueryString);
+		JSONObject geneResults = JSONRestUtil.getResults(solrPath + newQueryString);
 		JSONArray docs = JSONRestUtil.getDocArray(geneResults);
 		if (docs != null) {
 			int size = docs.size();
@@ -111,7 +116,93 @@ public class SearchPageTest {
 			}
 		}
 	}
+	@Test
+	public void testSelectedMgiIds() throws Exception {
+		
+		String newQueryString = "/gene/select?q=mgi_accession_id:*&fl=mgi_accession_id,marker_symbol&wt=json";
+		int startIndex = 0;
+		int nbRows = 1;
+		newQueryString+="&start="+startIndex+"&rows="+nbRows;
+		System.out.println("newQueryString=" + newQueryString);
 
+		JSONObject geneResults = JSONRestUtil.getResults(solrPath + newQueryString);
+		JSONArray docs = JSONRestUtil.getDocArray(geneResults);
+		if (docs != null) {
+			int size = docs.size();
+			for (int i=0; i<size; i++) {
+				String mgiId = docs.getJSONObject(i).getString("mgi_accession_id");
+				String symbol = docs.getJSONObject(i).getString("marker_symbol");				
+				driver.get(baseUrl + "/data/search?q=" + mgiId);
+				//WebElement geneLink = driver.findElement(By.xpath("//a[@href='/data/genes/" + mgiId + "'"));				
+				WebElement geneLink = driver.findElement(By.cssSelector("div.geneCol a").linkText(symbol)); 
+				assert(geneLink != null);	
+				System.out.println("OK: query by MGI accession id: " + mgiId);
+			}
+		}
+	}
+	
+	@Test
+	public void testLeadingWildcard() throws Exception {
+				
+		driver.get(baseUrl + "/data/search?q=*rik");	
+			
+		new WebDriverWait(driver, 25).until(ExpectedConditions.visibilityOfElementLocated(By.id("geneGrid_info"))); 
+		System.out.println("wildcard check found: " + driver.findElement(By.cssSelector("span#resultCount a")).getText());
+					
+		
+	}
+	
+	@Test
+	public void testFacetCounts() throws Exception {
+		
+		String commonParam = "qf=auto_suggest&defType=edismax&wt=json&rows=0&q=*:*";
+		
+		final String geneParams      = "/gene/select?" + commonParam + "&fq=marker_type:* -marker_type:\"heritable phenotypic marker\"&core=gene";
+		final String mpParams        = "/mp/select?" + commonParam + "&fq=ontology_subset:*&core=mp";
+		final String diseaseParams   = "/disease/select?" + commonParam + "&fq=type:disease&core=disease";
+		final String maParams        = "/ma/select?" + commonParam + "&fq=ontology_subset:IMPC_Terms AND selected_top_level_ma_term:*&core=ma";
+		final String pipelineParams  = "/pipeline/select?" + commonParam + "&fq=pipeline_stable_id:*&core=pipeline";
+		final String imagesParams    = "/images/select?" + commonParam + "&fq=annotationTermId:M* OR expName:* OR symbol:* OR annotated_or_inferred_higherLevelMaTermName:* OR annotated_or_inferred_higherLevelMpTermName:*&core=images";
+		
+		List<String> paramList = new ArrayList<String>() {
+			  {
+				add(geneParams);
+				add(mpParams);
+				add(diseaseParams);
+				add(maParams);
+				add(pipelineParams);
+				add(imagesParams);
+			   }
+		};
+		
+		for (String s : paramList ){			
+			JSONObject geneResults = JSONRestUtil.getResults(solrPath + s);	
+			
+			int facetCountFromSolr = geneResults.getJSONObject("response").getInt("numFound");
+			String core = geneResults.getJSONObject("responseHeader").getJSONObject("params").getString("core");
+			String fq = geneResults.getJSONObject("responseHeader").getJSONObject("params").getString("fq");
+			
+			driver.get(baseUrl + "/data/search#fq=" + fq + "&core=" + core);
+			
+			// test facet panel loaded ok
+			System.out.println("current fq=" + fq + "&core=" + core);
+			int facetCountFromPage = Integer.parseInt(driver.findElement(By.cssSelector("div.flist li#" + core + " span.fcount")).getText());
+			System.out.println("facet panel test for " + core + " core: " + facetCountFromSolr + " vs " + facetCountFromPage);
+			assertEquals(facetCountFromSolr, facetCountFromPage);
+			
+			// wait for ajax response before doing the test
+			new WebDriverWait(driver, 25).until(ExpectedConditions.visibilityOfElementLocated(By.id("geneGrid_info")));
+								
+			// test dataTable loaded ok			
+			System.out.println("facet count check found : " + driver.findElement(By.cssSelector("span#resultCount a")).getText());
+			String[] parts = driver.findElement(By.cssSelector("span#resultCount a")).getText().split(" ");	
+			System.out.println("check: " + parts[0]);
+			int dataTalbeFoundCount = Integer.parseInt(parts[0]);			
+			System.out.println("table panel test for " + core + " core: " + facetCountFromSolr + " vs " + dataTalbeFoundCount);
+			assertEquals(facetCountFromSolr, dataTalbeFoundCount);
+		}
+	}
+	
 	@After
 	public void tearDown() throws Exception {
 		driver.quit();
