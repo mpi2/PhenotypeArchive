9b2358c6730316a6fa4029abfb6758a3a845302e
mrelac <mrelac@bottishamlock.windows.ebi.ac.uk>
2014-04-10 11:29:31 +0100

    This change runs the phenotype test with all known phenotypes, gathering successes and failures at the end of the run to make it easy to see which pages failed. Also added filtering mechanism to enable running fewer than the maximum number of iterations/phenotypes. Upgraded to latest version of WebDriver components. Added Chrome and Safari.

--- src/test/java/org/mousephenotype/GetAllPhenotypePagesTest.java
+++ src/test/java/org/mousephenotype/GetAllPhenotypePagesTest.java
@@ -20,7 +20,10 @@
 
 package org.mousephenotype;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -28,11 +31,9 @@ import java.util.concurrent.TimeUnit;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
@@ -59,11 +60,13 @@ public class GetAllPhenotypePagesTest implements ApplicationContextAware  {
 
     private ApplicationContext ac;
 
-    private String host = "http://localhost:8080";
+    private String host;
     private String baseUrl;
     static WebDriver driver;
     public BrowserType browserType;
-    public final int MAX_MGI_LINK_CHECK_COUNT = 5;
+    public final int MAX_MGI_LINK_CHECK_COUNT = 5;                              // -1 means test all links.
+    public final int MAX_PHENOTYPE_TEST_PAGE_COUNT = -1;                        // -1 means test all pages.
+    public DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     
     public static enum BrowserType {
           FIREFOX
@@ -80,13 +83,14 @@ public class GetAllPhenotypePagesTest implements ApplicationContextAware  {
     
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-            ac = applicationContext;
+        ac = applicationContext;
     }
 
     @Before
     public void setup() {
             @SuppressWarnings("unchecked")
             Map<String,String> config = (Map<String, String>) ac.getBean("globalConfiguration");
+            host = "https://dev.mousephenotype.org";
             baseUrl = host + config.get("baseUrl");
             
             switch (browserType) {
@@ -138,18 +142,15 @@ public class GetAllPhenotypePagesTest implements ApplicationContextAware  {
         List<String> exceptionList = new ArrayList();
         String message;
         
-        System.out.println("pageForEveryPhenotypeTest");
+        System.out.println(dateFormat.format(new Date()) + ": pageForEveryPhenotypeTest started.");
         
         try {
             // Loop through all phenotypes, testing each one for valid page load.
-            
-            
             int i = 0;
             for (String phenotypeId : phenotypeIds) {
-                if (i++ > 40) {
-                    return;
+                if ((MAX_PHENOTYPE_TEST_PAGE_COUNT != -1) && (i++ >= MAX_PHENOTYPE_TEST_PAGE_COUNT)) {
+                    break;
                 }
-            
             
                 target = baseUrl + "/phenotypes/" + phenotypeId;
                 driver.get(target);
@@ -179,11 +180,9 @@ public class GetAllPhenotypePagesTest implements ApplicationContextAware  {
             for (String s : exceptionList) {
                 System.out.println("\t" + s);
             }
-            
-            fail(errorList.size() + " MP_TERM_ID records failed.");
         }
             
-        System.out.println(successList.size() + " MP_TERM_ID records processed successfully.");
+        System.out.println(dateFormat.format(new Date()) + ": " + successList.size() + " MP_TERM_ID records processed successfully.\n\n");
         
         if (errorList.size() + exceptionList.size() > 0) {
             fail("ERRORS: " + errorList.size() + ". EXCEPTIONS: " + exceptionList.size());
@@ -199,31 +198,70 @@ public class GetAllPhenotypePagesTest implements ApplicationContextAware  {
     @Test
     public void testMGILinksAreValid() throws SolrServerException {
         Set<String> phenotypeIds = genotypePhenotypeService.getAllPhenotypes();
-        String target;
+        String target = "";
+        List<String> errorList = new ArrayList();
+        List<String> successList = new ArrayList();
+        List<String> exceptionList = new ArrayList();
+        String message;
         
-        System.out.println("testMGILinksAreValid");
+        System.out.println(dateFormat.format(new Date()) + ": testMGILinksAreValid started.");
         
-        // Loop through first MAX_MGI_LINK_CHECK_COUNT phenotype MGI links, testing each one for valid page load.
-        int i = 0;
-        for (String phenotypeId : phenotypeIds) {
-            if (i++ > MAX_MGI_LINK_CHECK_COUNT) {
-                return;
-            }
-                
-            target = baseUrl + "/phenotypes/" + phenotypeId;
-            driver.get(target);
-            List<WebElement> phenotypeLinks = driver.findElements(By.cssSelector("div.inner a").linkText(phenotypeId));
-            if ( ! phenotypeLinks.isEmpty()) {
-                if (phenotypeLinks.size() != 1) {
-                    fail("Expected exactly 1 page for MP_TERM_ID " + phenotypeId + "(" + target + ") but found " + phenotypeLinks.size());
+        try {
+            // Loop through first MAX_MGI_LINK_CHECK_COUNT phenotype MGI links, testing each one for valid page load.
+            int i = 0;
+            for (String phenotypeId : phenotypeIds) {
+                if ((MAX_MGI_LINK_CHECK_COUNT != -1) && (i++ >= MAX_MGI_LINK_CHECK_COUNT)) {
+                    break;
                 }
-                
+
+                target = baseUrl + "/phenotypes/" + phenotypeId;
+                driver.get(target);
+                List<WebElement> phenotypeLinks = driver.findElements(By.cssSelector("div.inner a").linkText(phenotypeId));
+                if (phenotypeLinks.isEmpty()) {
+                    message = "No page found for MP_TERM_ID " + phenotypeId + "(" + target + ")";
+                    errorList.add(message);
+                    continue;
+                }
+                    
+                if (phenotypeLinks.size() != 1) {
+                    message = "Expected exactly 1 page for MP_TERM_ID " + phenotypeId + "(" + target + ") but found " + phenotypeLinks.size();
+                    errorList.add(message);
+                }
+
                 phenotypeLinks.get(0).click();
                 String idString = "[" + phenotypeId + "]";
                 boolean found = driver.findElement(By.cssSelector("div[id='templateBodyInsert']")).getText().contains(idString);
-                
-                assertTrue(found);
+                if ( ! found) {
+                    message = "div id 'templateBodyInsert' not found.";
+                    errorList.add(message);
+                } else {
+                    message = "SUCCESS: MGI link OK for " + phenotypeId + ". Target URL: " + target;
+                    successList.add(message);
+                }
             }
+        } catch (Exception e) {
+            message = "EXCEPTION processing target URL " + target + ": " + e.getLocalizedMessage();
+            exceptionList.add(message);
+        }
+        
+        if ( ! errorList.isEmpty()) {
+            System.out.println(errorList.size() + " MGI links failed:");
+            for (String s : errorList) {
+                System.out.println("\t" + s);
+            }
+        }
+        
+        if ( ! exceptionList.isEmpty()) {
+            System.out.println(errorList.size() + " MGI links caused exceptions to be thrown:");
+            for (String s : exceptionList) {
+                System.out.println("\t" + s);
+            }
+        }
+            
+        System.out.println(dateFormat.format(new Date()) + ": " + successList.size() + " MGI links processed successfully.\n\n");
+        
+        if (errorList.size() + exceptionList.size() > 0) {
+            fail("ERRORS: " + errorList.size() + ". EXCEPTIONS: " + exceptionList.size());
         }
     }
 
