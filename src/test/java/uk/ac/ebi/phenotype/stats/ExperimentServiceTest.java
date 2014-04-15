e3679ed01f039887a8ef299d329a15408b84ca12
Jeremy Mason <jmason@ebi.ac.uk>
2014-04-15 14:25:32 +0100

    update test case to test control selection

--- src/test/java/uk/ac/ebi/phenotype/stats/ExperimentServiceTest.java
+++ src/test/java/uk/ac/ebi/phenotype/stats/ExperimentServiceTest.java
@@ -6,6 +6,8 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -16,10 +18,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import uk.ac.ebi.phenotype.dao.OrganisationDAO;
+import uk.ac.ebi.phenotype.dao.PhenotypePipelineDAO;
+import uk.ac.ebi.phenotype.pojo.Organisation;
+import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.Pipeline;
 import uk.ac.ebi.phenotype.pojo.SexType;
 import uk.ac.ebi.phenotype.pojo.ZygosityType;
-
-import uk.ac.ebi.phenotype.stats.graphs.GraphUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:app-config.xml" })
@@ -29,10 +34,17 @@ public class ExperimentServiceTest {
 
 	@Autowired
 	private ExperimentService es;
+	
+	@Autowired
+	private PhenotypePipelineDAO pDAO;
+
+	@Autowired
+	private OrganisationDAO orgDAO;
 
 	@Ignore
 	@Test
 	public void testGetExperimentDTO() throws SolrServerException, IOException, URISyntaxException {
+		System.out.println("\nexecuting testGetExperimentDTO");
 		List<ExperimentDTO> exp = es.getExperimentDTO("ESLIM_001_001_158", null, "MGI:1920194");
 		System.out.println(exp);
 		assertTrue(exp.size()>0);
@@ -47,7 +59,8 @@ public class ExperimentServiceTest {
 
 	@Test
 	public void testGetExperimentDTO2() throws SolrServerException, IOException, URISyntaxException {
-		List<ExperimentDTO> exp = es.getExperimentDTO("ESLIM_003_001_011", null, "MGI:1922257");
+		System.out.println("\nexecuting testGetExperimentDTO2");
+		List<ExperimentDTO> exp = es.getExperimentDTO("ESLIM_015_001_012", null, "MGI:2444584");
 		System.out.println(exp);
 		assertTrue(exp.size()>0);
 		System.out.println(exp.get(0));
@@ -56,16 +69,21 @@ public class ExperimentServiceTest {
 
 	
 	@Test
-	public void testThisThing() throws SolrServerException, IOException, URISyntaxException {
-		List<String> sexes = new ArrayList<>();
-		sexes.add(SexType.male.name());
-		List<String> zygs = new ArrayList<>();
-		zygs.add(ZygosityType.homozygote.name());
-		Integer pipelineId=0;
-       // List<ExperimentDTO> experimentList = es.getExperimentDTO(1594, pipelineId, "MGI:1922257", sexes, zygs, 8);
-      //  System.out.println("EXP list is: "+experimentList);
-       // System.out.println("Size is: "+experimentList.size());
+	public void testGetExperimentDTOESLIM_015_001_012() throws SolrServerException, IOException, URISyntaxException {
 
+		Logger.getRootLogger().setLevel(Level.INFO);
+
+		System.out.println("\nexecuting testGetExperimentDTOESLIM_015_001_012");
+		Parameter p = pDAO.getParameterByStableId("ESLIM_015_001_012");
+		Pipeline pipe = pDAO.getPhenotypePipelineByStableId("ESLIM_002");
+		Organisation org = orgDAO.getOrganisationByName("MRC Harwell");
+		
+		List<String> zygs = new ArrayList<>();
+		zygs.add(ZygosityType.heterozygote.name());
+		List<ExperimentDTO> experimentList = es.getExperimentDTO(p.getId(), pipe.getId(), "MGI:2444584", null, org.getId(), zygs, "MGI:4830588", "9180596a255e4e612acae74beb22b506", false);
+
+        System.out.println("EXP list is: "+experimentList);
+        System.out.println("Size is: "+experimentList.size());
 
 	}
 
