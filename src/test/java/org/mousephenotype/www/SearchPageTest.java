be6425f5ff40a7fab5759890b843fdd583574419
ckchen-ebi <ckchen@ebi.ac.uk>
2014-06-05 11:28:31 +0100

    restore all tests

--- src/test/java/org/mousephenotype/www/SearchPageTest.java
+++ src/test/java/org/mousephenotype/www/SearchPageTest.java
@@ -227,7 +227,7 @@ public class SearchPageTest {
     
     
     @Test
-    @Ignore
+    ////@Ignore
     public void autosuggestTest() throws Exception {
     	// test that there is a dropdown when at least 3 letters with match are entered into the input box
     	 testCount++;
@@ -259,7 +259,7 @@ public class SearchPageTest {
     
     
     @Test
-    @Ignore
+    //@Ignore
     public void testTickingFacetFilters() throws Exception {
         testCount++;
         System.out.println();
@@ -349,7 +349,7 @@ public class SearchPageTest {
     }
 
     @Test
-    @Ignore
+    //@Ignore
     public void testQueryingRandomGeneSymbols() throws Exception {
         testCount++;
         String testName = "RANDOM GENE SYMBOL QUERY TESTS";
@@ -421,7 +421,7 @@ public class SearchPageTest {
     }
 
     @Test
-    @Ignore
+    //@Ignore
     public void testRandomMgiIds() throws Exception {
         testCount++;
         System.out.println();
@@ -487,31 +487,31 @@ public class SearchPageTest {
     }
 
     @Test
-    @Ignore
+    //@Ignore
     public void testPhrase() throws Exception {
         specialStrQueryTest("PHRASE QUERY TESTS", "grip strength");
     }
 
     @Test
-    @Ignore
+    //@Ignore
     public void testPhraseInQuotes() throws Exception {
         specialStrQueryTest("PHRASE IN QUOTES QUERY TESTS", "\"zinc finger protein\"");
     }
 
     @Test
-    @Ignore
+    //@Ignore
     public void testLeadingWildcard() throws Exception {
         specialStrQueryTest("LEADING WILDCARD QUERY TESTS", "*rik");
     }
 
     @Test
-    @Ignore
+    //@Ignore
     public void testTrailingWildcard() throws Exception {
         specialStrQueryTest("TRAILING WILDCARD QUERY TESTS", "hox*");
     }
 
     @Test
-    @Ignore
+    //@Ignore
     public void testPagination() throws Exception {
         testCount++;
         System.out.println();
@@ -592,7 +592,7 @@ public class SearchPageTest {
     }
 
     @Test
-    @Ignore
+    //@Ignore
     public void testFacetCounts() throws Exception {
         testCount++;
         System.out.println();
