cf8c764e02054c3b3f47f62b1a6120189c68f6af
Jeremy Mason <jmason@ebi.ac.uk>
2014-06-19 11:16:21 +0100

    factor out common method of splitting the parameter status on colon

--- src/main/java/uk/ac/ebi/phenotype/dao/ObservationDAOImpl.java
+++ src/main/java/uk/ac/ebi/phenotype/dao/ObservationDAOImpl.java
@@ -30,7 +30,9 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
@@ -626,18 +628,10 @@ public class ObservationDAOImpl extends HibernateDAOImpl implements ObservationD
         // Add the status code to the observation if there is one
         if(parameterStatus!=null) {
 
-            String code = parameterStatus;
+            Map<String, String> pStatMap = getParameterStatusAndMessage(parameterStatus);
 
-            if(code.contains(":")) {
-    
-                String message = code.substring(code.indexOf(":")+1, code.length()).trim();
-                obs.setParameterStatusMessage(message);
-
-                code = code.substring(0, code.indexOf(":"));
-
-            }
-
-            obs.setParameterStatus(code);
+            obs.setParameterStatus(pStatMap.get("status"));
+            obs.setParameterStatusMessage(pStatMap.get("message"));
             obs.setMissingFlag(true);
 
         }
@@ -688,9 +682,44 @@ public class ObservationDAOImpl extends HibernateDAOImpl implements ObservationD
 		obs.setSample(sample);
 		obs.setType(observationType);
 		obs.setParameterStableId(parameter.getStableId());
-		obs.setParameterStatus(parameterStatus);
-		
+
+		// Add the status code to the observation if there is one
+        if(parameterStatus!=null) {
+            
+            Map<String, String> pStatMap = getParameterStatusAndMessage(parameterStatus);
+
+            obs.setParameterStatus(pStatMap.get("status"));
+            obs.setParameterStatusMessage(pStatMap.get("message"));
+            obs.setMissingFlag(true);
+
+        }
+
 		return obs;
 	}
 
+	public static Map<String, String> getParameterStatusAndMessage(String parameterStatus) {
+
+	    Map<String, String> pStatusMap = new HashMap<>();
+        pStatusMap.put("message", null);
+        pStatusMap.put("status", null);
+	    
+       // Add the status code to the observation if there is one
+        if(parameterStatus != null) {
+
+            String code = parameterStatus;
+
+            if(code.contains(":")) {
+    
+                String message = code.substring(code.indexOf(":")+1, code.length()).trim();
+                pStatusMap.put("message", message);
+
+                code = code.substring(0, code.indexOf(":"));
+                pStatusMap.put("status", code);
+
+            }
+        }
+        
+        return pStatusMap;
+	}
+	
 }
