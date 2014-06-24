be06988ea8143d7263b87a1b4ee457ad5b3aebcc
Ilinca Tudose <ilinca.tudose@gmail.com>
2014-06-24 16:22:08 +0100

    I think the text summary should work now but I need to wait for CK to add the cildren in the mp core before I can push.

--- src/main/java/uk/ac/ebi/phenotype/service/GenotypePhenotypeService.java
+++ src/main/java/uk/ac/ebi/phenotype/service/GenotypePhenotypeService.java
@@ -116,9 +116,11 @@ public class GenotypePhenotypeService {
 						+ ":\"MGI:2164831\")").setRows(10000);
 		q.set("group.field", "" + GenotypePhenotypeField.MARKER_SYMBOL);
 		q.set("group", true);
+		q.set("group.limit", 0);
 		if (sex != null) {
 			q.addFilterQuery(GenotypePhenotypeField.SEX + ":" + sex);
 		}
+		System.out.println("++++++++++  " + solr.getBaseURL() + "/select?"+ q);
 		QueryResponse results = solr.query(q);
 		return results.getGroupResponse().getValues().get(0).getValues();
 	}
--- src/main/java/uk/ac/ebi/phenotype/service/ObservationService.java
+++ src/main/java/uk/ac/ebi/phenotype/service/ObservationService.java
@@ -1374,32 +1374,33 @@ public class ObservationService {
 			List<String> parameters) throws SolrServerException {
 		
 		System.out.println("::::::::::::: Getting tested genes for : "+ parameters.size() + parameters.get(0));
-		List<String> genes = new ArrayList<String>();
-		for (String parameter : parameters) {
-			SolrQuery query = new SolrQuery()
-					.setQuery(
-							ExperimentField.PARAMETER_STABLE_ID + ":"
-									+ parameter)
+		HashSet<String> genes = new HashSet<String>();
+		int i = 0;
+		while (i < parameters.size()) {
+			// Add no more than 10 params at the time so the url doesn't get too long
+			String parameter = parameters.get(i++);
+			String query = "("+ExperimentField.PARAMETER_STABLE_ID + ":" + parameter;
+			while (i%15 != 0 && i < parameters.size()){
+				parameter = parameters.get(i++);
+				query += " OR " + ExperimentField.PARAMETER_STABLE_ID + ":" + parameter;
+			}
+			query += ")";
+			
+			SolrQuery q = new SolrQuery().setQuery(query)
 					.addField(ExperimentField.GENE_ACCESSION)
-					.setFilterQueries(
-							ExperimentField.STRAIN + ":\"MGI:2159965\" OR "
-									+ ExperimentField.STRAIN
-									+ ":\"MGI:2164831\"").setRows(10000);
-			query.set("group.field", ExperimentField.GENE_ACCESSION);
-			query.set("group", true);
+					.setFilterQueries(ExperimentField.STRAIN + ":\"MGI:2159965\" OR " + ExperimentField.STRAIN + ":\"MGI:2164831\"").setRows(10000);
+			q.set("group.field", ExperimentField.GENE_ACCESSION);
+			q.set("group", true);
 			if (sex != null) {
-				query.addFilterQuery(ExperimentField.SEX + ":" + sex);
+				q.addFilterQuery(ExperimentField.SEX + ":" + sex);
 			}
 			// I need to add the genes to a hash in case some come up multiple
 			// times from different parameters
-			System.out.println("=====" + solr.getBaseURL() + query);
-			List<Group> groups = solr.query(query).getGroupResponse()
-					.getValues().get(0).getValues();
+			System.out.println("=====" + solr.getBaseURL() + "/select?"+ q);
+			List<Group> groups = solr.query(q).getGroupResponse().getValues().get(0).getValues();
 			for (Group gr : groups) {
 				// System.out.println(gr.getGroupValue());
-				if (!genes.contains((String) gr.getGroupValue())) {
-					genes.add((String) gr.getGroupValue());
-				}
+				genes.add((String) gr.getGroupValue());
 			}
 		}
 		return genes.size();
