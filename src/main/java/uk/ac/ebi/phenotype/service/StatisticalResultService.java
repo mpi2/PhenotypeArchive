7b4531d5e082d8c7e6b80d9d41b0e6b32c529f21
Jeremy Mason <jmason@ebi.ac.uk>
2014-07-25 14:12:34 +0100

    move field constants to DTO and use field constants for @Field annotations

--- src/main/java/uk/ac/ebi/phenotype/service/StatisticalResultService.java
+++ src/main/java/uk/ac/ebi/phenotype/service/StatisticalResultService.java
@@ -44,7 +44,6 @@ import uk.ac.ebi.phenotype.pojo.StatisticalResult;
 import uk.ac.ebi.phenotype.pojo.UnidimensionalResult;
 import uk.ac.ebi.phenotype.pojo.ZygosityType;
 import uk.ac.ebi.phenotype.service.dto.GenotypePhenotypeDTO;
-import uk.ac.ebi.phenotype.service.dto.ObservationDTO;
 import uk.ac.ebi.phenotype.service.dto.StatisticalResultDTO;
 import uk.ac.ebi.phenotype.web.pojo.GeneRowForHeatMap;
 import uk.ac.ebi.phenotype.web.pojo.HeatMapCell;
@@ -70,109 +69,6 @@ public class StatisticalResultService {
     private HttpSolrServer solr;
 
     private static final Logger LOG = LoggerFactory.getLogger(StatisticalResultService.class);
-
-    // Definition of the solr fields
-    public static final class StatisticalResultField {
-
-        public final static String DOCUMENT_ID = "doc_id";
-        public final static String DB_ID = "db_id";
-        public final static String DATA_TYPE = "data_type";
-
-        public final static String MP_TERM_ID = "mp_term_id"; 
-        public final static String MP_TERM_NAME = "mp_term_name"; 
-        public final static String TOP_LEVEL_MP_TERM_ID = "top_level_mp_term_id";
-        public final static String TOP_LEVEL_MP_TERM_NAME = "top_level_mp_term_name";
-        public final static String INTERMEDIATE_MP_TERM_ID = "intermediate_mp_term_id";
-        public final static String INTERMEDIATE_MP_TERM_NAME = "intermediate_mp_term_name";
-
-        public final static String RESOURCE_NAME = "resource_name"; 
-        public final static String RESOURCE_FULLNAME = "resource_fullname";
-        public final static String RESOURCE_ID = "resource_id"; 
-        public final static String PROJECT_NAME = "project_name";
-        public final static String PHENOTYPING_CENTER = "phenotyping_center"; 
-
-        public final static String PIPELINE_STABLE_ID = "pipeline_stable_id"; 
-        public final static String PIPELINE_STABLE_KEY = "pipeline_stable_key"; 
-        public final static String PIPELINE_NAME = "pipeline_name"; 
-        public final static String PIPELINE_ID = "pipeline_id"; 
-
-        public final static String PROCEDURE_STABLE_ID = "procedure_stable_id";
-        public final static String PROCEDURE_STABLE_KEY = "procedure_stable_key"; 
-        public final static String PROCEDURE_NAME = "procedure_name"; 
-        public final static String PROCEDURE_ID = "procedure_id"; 
-
-        public final static String PARAMETER_STABLE_ID = "parameter_stable_id";
-        public final static String PARAMETER_STABLE_KEY = "parameter_stable_key";
-        public final static String PARAMETER_NAME = "parameter_name";
-        public final static String PARAMETER_ID = "parameter_id";
-
-        public final static String COLONY_ID = "colony_id"; 
-        public final static String MARKER_SYMBOL = "marker_symbol";
-        public final static String MARKER_ACCESSION_ID = "marker_accession_id";
-        public final static String ALLELE_SYMBOL = "allele_symbol";
-        public final static String ALLELE_NAME = "allele_name";
-        public final static String ALLELE_ACCESSION_ID = "allele_accession_id";
-        public final static String STRAIN_NAME = "strain_name"; 
-        public final static String STRAIN_ACCESSION_ID = "strain_accession_id"; 
-        public final static String SEX = "sex"; 
-        public final static String ZYGOSITY = "zygosity"; 
-
-        public final static String CONTROL_SELECTION_METHOD = "control_selection_method";
-        public final static String DEPENDENT_VARIABLE = "dependent_variable";
-        public final static String METADATA_GROUP = "metadata_group";
-
-        public final static String CONTROL_BIOLOGICAL_MODEL_ID = "control_biological_model_id";
-        public final static String MUTANT_BIOLOGICAL_MODEL_ID = "mutant_biological_model_id";
-        public final static String MALE_CONTROL_COUNT = "male_control_count";
-        public final static String MALE_MUTANT_COUNT = "male_mutant_count";
-        public final static String FEMALE_CONTROL_COUNT = "female_control_count";
-        public final static String FEMALE_MUTANT_COUNT = "female_mutant_count";
-
-        public final static String STATISTICAL_METHOD = "statistical_method";
-        public final static String STATUS = "status";
-        public final static String ADDITIONAL_INFORMATION = "additional_information";
-        public final static String RAW_OUTPUT = "raw_output";
-        public final static String P_VALUE = "p_value";
-        public final static String EFFECT_SIZE = "effect_size";
-
-        public final static String CATEGORIES = "categories";
-        public final static String CATEGORICAL_P_VALUE = "categorical_p_value";
-        public final static String CATEGORICAL_EFFECT_SIZE = "categorical_effect_size";
-        
-        public final static String BATCH_SIGNIFICANT = "batch_significant";
-        public final static String VARIANCE_SIGNIFICANT = "variance_significant";
-        public final static String NULL_TEST_P_VALUE = "null_test_p_value";
-        public final static String GENOTYPE_EFFECT_P_VALUE = "genotype_effect_p_value";
-        public final static String GENOTYPE_EFFECT_STDERR_ESTIMATE = "genotype_effect_stderr_estimate";
-        public final static String GENOTYPE_EFFECT_PARAMETER_ESTIMATE = "genotype_effect_parameter_estimate";
-
-        public final static String SEX_EFFECT_P_VALUE = "sex_effect_p_value";
-        public final static String SEX_EFFECT_STDERR_ESTIMATE = "sex_effect_stderr_estimate";
-        public final static String SEX_EFFECT_PARAMETER_ESTIMATE = "sex_effect_parameter_estimate";
-        public final static String WEIGHT_EFFECT_P_VALUE = "weight_effect_p_value";
-        public final static String WEIGHT_EFFECT_STDERR_ESTIMATE = "weight_effect_stderr_estimate";
-        public final static String WEIGHT_EFFECT_PARAMETER_ESTIMATE = "weight_effect_parameter_estimate";
-
-        public final static String group_1_genotype = "group_1_genotype";
-        public final static String group_1_residuals_normality_test = "group_1_residuals_normality_test";
-        public final static String group_2_genotype = "group_2_genotype";
-        public final static String group_2_residuals_normality_test = "group_2_residuals_normality_test";
-        public final static String blups_test = "blups_test";
-        public final static String rotated_residuals_test = "rotated_residuals_test";
-
-        public final static String intercept_estimate = "intercept_estimate";
-        public final static String intercept_estimate_stderr_estimate = "intercept_estimate_stderr_estimate";
-        public final static String interaction_significant = "interaction_significant";
-        public final static String interaction_effect_p_value = "interaction_effect_p_value";
-        public final static String female_ko_effect_p_value = "female_ko_effect_p_value";
-        public final static String female_ko_effect_stderr_estimate = "female_ko_effect_stderr_estimate";
-        public final static String female_ko_parameter_estimate = "female_ko_parameter_estimate";
-        public final static String male_ko_effect_p_value = "male_ko_effect_p_value";
-        public final static String male_ko_effect_stderr_estimate = "male_ko_effect_stderr_estimate";
-        public final static String male_ko_parameter_estimate = "male_ko_parameter_estimate";
-        public final static String classification_tag = "classification_tag";
-
-    }
 
     public StatisticalResultService(String solrUrl) {
         solr = new HttpSolrServer(solrUrl);
@@ -201,29 +97,29 @@ public class StatisticalResultService {
     
         SolrQuery query = new SolrQuery()
             .setQuery("*:*")
-            .addFilterQuery(StatisticalResultField.ALLELE_ACCESSION_ID + ":\"" + alleleAccession + "\"")
-            .addFilterQuery(StatisticalResultField.PHENOTYPING_CENTER + ":\"" + phenotypingCenter + "\"")
-            .addFilterQuery(StatisticalResultField.PIPELINE_STABLE_ID + ":" + pipelineStableId)
-            .addFilterQuery(StatisticalResultField.PARAMETER_STABLE_ID + ":" + parameterStableId)
-            .addFilterQuery(StatisticalResultField.ZYGOSITY + ":" + zygosity.name())
+            .addFilterQuery(StatisticalResultDTO.ALLELE_ACCESSION_ID + ":\"" + alleleAccession + "\"")
+            .addFilterQuery(StatisticalResultDTO.PHENOTYPING_CENTER + ":\"" + phenotypingCenter + "\"")
+            .addFilterQuery(StatisticalResultDTO.PIPELINE_STABLE_ID + ":" + pipelineStableId)
+            .addFilterQuery(StatisticalResultDTO.PARAMETER_STABLE_ID + ":" + parameterStableId)
+            .addFilterQuery(StatisticalResultDTO.ZYGOSITY + ":" + zygosity.name())
             .setStart(0)
             .setRows(10)
         ;
 
         if(strain != null) {
-            query.addFilterQuery(StatisticalResultField.STRAIN_ACCESSION_ID + ":\"" + strain + "\"");
+            query.addFilterQuery(StatisticalResultDTO.STRAIN_ACCESSION_ID + ":\"" + strain + "\"");
         }
 
         if(sex != null) {
-            query.addFilterQuery(ObservationDTO.SEX + ":" + sex);
+            query.addFilterQuery(StatisticalResultDTO.SEX + ":" + sex);
         }
 
         if(metadataGroup==null) {
             // don't add a metadata group filter
         } else if (metadataGroup.isEmpty()) {
-            query.addFilterQuery(ObservationDTO.METADATA_GROUP + ":\"\"");
+            query.addFilterQuery(StatisticalResultDTO.METADATA_GROUP + ":\"\"");
         } else {
-            query.addFilterQuery(ObservationDTO.METADATA_GROUP + ":" + metadataGroup);
+            query.addFilterQuery(StatisticalResultDTO.METADATA_GROUP + ":" + metadataGroup);
         }
 
         response = solr.query(query);
@@ -366,7 +262,7 @@ public class StatisticalResultService {
         }
 
         SolrQuery q = new SolrQuery()
-                .setQuery(GenotypePhenotypeDTO.MARKER_ACCESSION_ID + ":\"" + accession + "\"").setSort(GenotypePhenotypeDTO.P_VALUE, SolrQuery.ORDER.asc)
+                .setQuery(StatisticalResultDTO.MARKER_ACCESSION_ID + ":\"" + accession + "\"").setSort(StatisticalResultDTO.P_VALUE, SolrQuery.ORDER.asc)
                 .setRows(10000);
         QueryResponse response = null;
 
