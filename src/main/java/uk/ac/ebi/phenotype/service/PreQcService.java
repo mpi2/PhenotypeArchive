54ac38bc904c4bfcb01dad5459ec8226ab14defa
jwgwarren <warren.jonathan@gmail.com>
2014-09-15 15:58:46 +0100

    removed service as requested by IT so project runs

--- src/main/java/uk/ac/ebi/phenotype/service/PreQcService.java
+++ src/main/java/uk/ac/ebi/phenotype/service/PreQcService.java
@@ -2,7 +2,7 @@ package uk.ac.ebi.phenotype.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
+
 public class PreQcService extends GenotypePhenotypeService {
 
 	public PreQcService(String solrUrl) {
