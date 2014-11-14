fe3cffc6cfb251c4bfb653ec7d04c19f8f54b1c7
ckchen-ebi <ckchen@ebi.ac.uk>
2014-11-14 10:51:23 +0000

    removed sysout

--- src/main/java/uk/ac/ebi/phenotype/web/controller/FileExportController.java
+++ src/main/java/uk/ac/ebi/phenotype/web/controller/FileExportController.java
@@ -479,7 +479,9 @@ public class FileExportController {
     }
 
     private List<String> composeImageDataTableRows(String query, JSONObject json, Integer iDisplayStart, Integer iDisplayLength, boolean showImgView, String solrParams, HttpServletRequest request) {
-        String mediaBaseUrl = config.get("mediaBaseUrl").replace("https:", "http:");
+       System.out.println("query: "+ query + " -- "+ solrParams);
+    	
+    	String mediaBaseUrl = config.get("mediaBaseUrl").replace("https:", "http:");
         
         List<String> rowData = new ArrayList();
 
