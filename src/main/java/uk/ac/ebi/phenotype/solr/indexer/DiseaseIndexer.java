cb5daa8d7e773f2380f9205b95cbb2f7199bc211
Jeremy Mason <jmason@ebi.ac.uk>
2014-12-01 11:18:57 +0000

    remove extra null check. Format

--- src/main/java/uk/ac/ebi/phenotype/solr/indexer/DiseaseIndexer.java
+++ src/main/java/uk/ac/ebi/phenotype/solr/indexer/DiseaseIndexer.java
@@ -11,7 +11,6 @@ import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
-import org.apache.solr.client.solrj.response.QueryResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,17 +34,9 @@ import java.util.*;
 
 
 /**
- * @author Matt
+ * @author Jeremy
  */
 public class DiseaseIndexer extends AbstractIndexer {
-
-
-	private static final Logger logger = LoggerFactory.getLogger(DiseaseIndexer.class);
-
-	// Map gene MGI ID to sanger allele bean
-	private static Map<String, GeneData> geneLookup = new HashMap<>();
-
-	private SolrServer phenodigmCore;
 
 	@Autowired
 	@Qualifier("geneIndexing")
@@ -57,6 +48,14 @@ public class DiseaseIndexer extends AbstractIndexer {
 
 	@Resource(name = "globalConfiguration")
 	private Map<String, String> config;
+
+	private static final Logger logger = LoggerFactory.getLogger(DiseaseIndexer.class);
+	public static final int MAX_DISEASES = 10000;
+
+	// Map disease ID to list of gene data objects
+	private static Map<String, GeneData> geneLookup = new HashMap<>();
+
+	private SolrServer phenodigmCore;
 
 
 	public DiseaseIndexer() {
@@ -75,6 +74,10 @@ public class DiseaseIndexer extends AbstractIndexer {
 
 		long startTime = new Date().getTime();
 
+		diseaseCore.deleteByQuery("*:*");
+		diseaseCore.commit();
+
+		// Fields from the phenodigm core to bring back
 		String fields = StringUtils.join(Arrays.asList(DiseaseBean.DISEASE_ID,
 			DiseaseBean.DISEASE_ID,
 			DiseaseBean.TYPE,
@@ -90,14 +93,8 @@ public class DiseaseIndexer extends AbstractIndexer {
 		SolrQuery query = new SolrQuery("*:*");
 		query.addFilterQuery("type:disease");
 		query.setFields(fields);
-		query.setRows(10000);
-
-
-		diseaseCore.deleteByQuery("*:*");
-		diseaseCore.commit();
-
-		QueryResponse response = phenodigmCore.query(query);
-		List<DiseaseBean> diseases = response.getBeans(DiseaseBean.class);
+		query.setRows(MAX_DISEASES);
+		List<DiseaseBean> diseases = phenodigmCore.query(query).getBeans(DiseaseBean.class);
 
 		int count = 0;
 		for (DiseaseBean phenDisease : diseases) {
@@ -124,85 +121,34 @@ public class DiseaseIndexer extends AbstractIndexer {
 			// Populate gene data
 			GeneData gene = geneLookup.get(phenDisease.getDiseaseId());
 
-			if (gene != null ) {
-				if (gene.MGI_ACCESSION_ID != null) {
-					disease.setMgiAccessionId(gene.MGI_ACCESSION_ID);
-				}
-				if (gene.MARKER_SYMBOL != null) {
-					disease.setMarkerSymbol(gene.MARKER_SYMBOL);
-				}
-				if (gene.MARKER_NAME != null) {
-					disease.setMarkerName(gene.MARKER_NAME);
-				}
-				if (gene.MARKER_SYNONYM != null) {
-					disease.setMarkerSynonym(gene.MARKER_SYNONYM);
-				}
-				if (gene.MARKER_TYPE != null) {
-					disease.setMarkerType(gene.MARKER_TYPE);
-				}
-				if (gene.HUMAN_GENE_SYMBOL != null) {
-					disease.setHumanGeneSymbol(gene.HUMAN_GENE_SYMBOL);
-				}
-				if (gene.STATUS != null) {
-					disease.setStatus(gene.STATUS);
-				}
-				if (gene.LATEST_PRODUCTION_CENTRE != null) {
-					disease.setLatestProductionCentre(gene.LATEST_PRODUCTION_CENTRE);
-				}
-				if (gene.LATEST_PHENOTYPING_CENTRE != null) {
-					disease.setLatestPhenotypingCentre(gene.LATEST_PHENOTYPING_CENTRE);
-				}
-				if (gene.LATEST_PHENOTYPE_STATUS != null) {
-					disease.setLatestPhenotypeStatus(gene.LATEST_PHENOTYPE_STATUS);
-				}
-				if (gene.LEGACY_PHENOTYPE_STATUS != null) {
-					disease.setLegacyPhenotypeStatus(gene.LEGACY_PHENOTYPE_STATUS);
-				}
-				if (gene.ALLELE_NAME != null) {
-					disease.setAlleleName(gene.ALLELE_NAME);
-				}
-				if (gene.MP_ID != null) {
-					disease.setMpId(gene.MP_ID);
-				}
-				if (gene.MP_TERM != null) {
-					disease.setMpTerm(gene.MP_TERM);
-				}
-				if (gene.MP_DEFINITION != null) {
-					disease.setMpTermDefinition(gene.MP_DEFINITION);
-				}
-				if (gene.MP_SYNONYM != null) {
-					disease.setMpTermSynonym(gene.MP_SYNONYM);
-				}
-				if (gene.TOP_LEVEL_MP_ID != null) {
-					disease.setTopLevelMpId(gene.TOP_LEVEL_MP_ID);
-				}
-				if (gene.TOP_LEVEL_MP_TERM != null) {
-					disease.setTopLevelMpTerm(gene.TOP_LEVEL_MP_TERM);
-				}
-				if (gene.TOP_LEVEL_MP_TERM_SYNONYM != null) {
-					disease.setTopLevelMpTermSynonym(gene.TOP_LEVEL_MP_TERM_SYNONYM);
-				}
-				if (gene.INTERMEDIATE_MP_ID != null) {
-					disease.setIntermediateMpId(gene.INTERMEDIATE_MP_ID);
-				}
-				if (gene.INTERMEDIATE_MP_TERM != null) {
-					disease.setIntermediateMpTerm(gene.INTERMEDIATE_MP_TERM);
-				}
-				if (gene.INTERMEDIATE_MP_TERM_SYNONYM != null) {
-					disease.setIntermediateMpTermSynonym(gene.INTERMEDIATE_MP_TERM_SYNONYM);
-				}
-				if (gene.CHILD_MP_ID != null) {
-					disease.setChildMpId(gene.CHILD_MP_ID);
-				}
-				if (gene.CHILD_MP_TERM != null) {
-					disease.setChildMpTerm(gene.CHILD_MP_TERM);
-				}
-				if (gene.CHILD_MP_TERM_SYNONYM != null) {
-					disease.setChildMpTermSynonym(gene.CHILD_MP_TERM_SYNONYM);
-				}
-				if (gene.ONTOLOGY_SUBSET != null) {
-					disease.setOntologySubset(gene.ONTOLOGY_SUBSET);
-				}
+			if (gene != null) {
+				disease.setMgiAccessionId(gene.MGI_ACCESSION_ID);
+				disease.setMarkerSymbol(gene.MARKER_SYMBOL);
+				disease.setMarkerName(gene.MARKER_NAME);
+				disease.setMarkerSynonym(gene.MARKER_SYNONYM);
+				disease.setMarkerType(gene.MARKER_TYPE);
+				disease.setHumanGeneSymbol(gene.HUMAN_GENE_SYMBOL);
+				disease.setStatus(gene.STATUS);
+				disease.setLatestProductionCentre(gene.LATEST_PRODUCTION_CENTRE);
+				disease.setLatestPhenotypingCentre(gene.LATEST_PHENOTYPING_CENTRE);
+				disease.setLatestPhenotypeStatus(gene.LATEST_PHENOTYPE_STATUS);
+				disease.setLegacyPhenotypeStatus(gene.LEGACY_PHENOTYPE_STATUS);
+				disease.setAlleleName(gene.ALLELE_NAME);
+				disease.setMpId(gene.MP_ID);
+				disease.setMpTerm(gene.MP_TERM);
+				disease.setMpTermDefinition(gene.MP_DEFINITION);
+				disease.setMpTermSynonym(gene.MP_SYNONYM);
+				disease.setTopLevelMpId(gene.TOP_LEVEL_MP_ID);
+				disease.setTopLevelMpTerm(gene.TOP_LEVEL_MP_TERM);
+				disease.setTopLevelMpTermSynonym(gene.TOP_LEVEL_MP_TERM_SYNONYM);
+				disease.setIntermediateMpId(gene.INTERMEDIATE_MP_ID);
+				disease.setIntermediateMpTerm(gene.INTERMEDIATE_MP_TERM);
+				disease.setIntermediateMpTermSynonym(gene.INTERMEDIATE_MP_TERM_SYNONYM);
+				disease.setChildMpId(gene.CHILD_MP_ID);
+				disease.setChildMpTerm(gene.CHILD_MP_TERM);
+				disease.setChildMpTermSynonym(gene.CHILD_MP_TERM_SYNONYM);
+				disease.setOntologySubset(gene.ONTOLOGY_SUBSET);
+
 			}
 			diseaseCore.addBean(disease, 60000);
 
@@ -216,6 +162,13 @@ public class DiseaseIndexer extends AbstractIndexer {
 	}
 
 
+	/**
+	 * Initialize the phenodigm core -- using a proxy if configured.
+	 * <p/>
+	 * A proxy is specified by supplying two JVM variables
+	 * - externalProxyHost the host (not including the protocol)
+	 * - externalProxyPort the integer port number
+	 */
 	private void initializeSolrCores() {
 
 		final String PHENODIGM_URL = config.get("phenodigm.solrserver");
@@ -242,6 +195,14 @@ public class DiseaseIndexer extends AbstractIndexer {
 	}
 
 
+	/**
+	 * Create lookup using disease ID as keys, and a GeneData structure that
+	 * contains the relevant gene information.
+	 * <p/>
+	 * Populates the geneLookup map
+	 *
+	 * @throws SolrServerException when solr does something unexpected
+	 */
 	private void populateGenesLookup() throws SolrServerException {
 
 		SolrQuery solrQuery = new SolrQuery();
@@ -257,64 +218,87 @@ public class DiseaseIndexer extends AbstractIndexer {
 						geneLookup.put(d, new GeneData());
 					}
 
-					if (gene.getMgiAccessionId() != null) { geneLookup.get(d).MGI_ACCESSION_ID.add(gene.getMgiAccessionId()); }
-					if (gene.getMarkerSymbol() != null) { geneLookup.get(d).MARKER_SYMBOL.add(gene.getMarkerSymbol()); }
-					if (gene.getMarkerName() != null) { geneLookup.get(d).MARKER_NAME.add(gene.getMarkerName()); }
-					if (gene.getMarkerSynonym() != null) { geneLookup.get(d).MARKER_SYNONYM.addAll(gene.getMarkerSynonym()); }
-					if (gene.getMarkerType() != null) { geneLookup.get(d).MARKER_TYPE.add(gene.getMarkerType()); }
-					if (gene.getHumanGeneSymbol() != null) { geneLookup.get(d).HUMAN_GENE_SYMBOL.addAll(gene.getHumanGeneSymbol()); }
-					if (gene.getStatus() != null) { geneLookup.get(d).STATUS.add(gene.getStatus()); }
-					if (gene.getLatestProductionCentre() != null) { geneLookup.get(d).LATEST_PRODUCTION_CENTRE.addAll(gene.getLatestProductionCentre()); }
-					if (gene.getLatestPhenotypingCentre() != null) { geneLookup.get(d).LATEST_PHENOTYPING_CENTRE.addAll(gene.getLatestPhenotypingCentre()); }
-					if (gene.getLatestPhenotypeStatus() != null) { geneLookup.get(d).LATEST_PHENOTYPE_STATUS.add(gene.getLatestPhenotypeStatus()); }
-					if (gene.getLegacy_phenotype_status() != null) { geneLookup.get(d).LEGACY_PHENOTYPE_STATUS.add(gene.getLegacy_phenotype_status()); }
-					if (gene.getAlleleName() != null) { geneLookup.get(d).ALLELE_NAME.addAll(gene.getAlleleName()); }
-					if (gene.getMpId() != null) { geneLookup.get(d).MP_ID.addAll(gene.getMpId()); }
-					if (gene.getMpTerm() != null) { geneLookup.get(d).MP_TERM.addAll(gene.getMpTerm()); }
-					if (gene.getMpDefinition() != null) { geneLookup.get(d).MP_DEFINITION.addAll(gene.getMpDefinition()); }
-					if (gene.getMpSynonym() != null) { geneLookup.get(d).MP_SYNONYM.addAll(gene.getMpSynonym()); }
-					if (gene.getTopLevelMpId() != null) { geneLookup.get(d).TOP_LEVEL_MP_ID.addAll(gene.getTopLevelMpId()); }
-					if (gene.getTopLevelMpTerm() != null) { geneLookup.get(d).TOP_LEVEL_MP_TERM.addAll(gene.getTopLevelMpTerm()); }
-					if (gene.getTopLevelMpSynonym() != null) { geneLookup.get(d).TOP_LEVEL_MP_TERM_SYNONYM.addAll(gene.getTopLevelMpSynonym()); }
-					if (gene.getIntermediateMpId() != null) { geneLookup.get(d).INTERMEDIATE_MP_ID.addAll(gene.getIntermediateMpId()); }
-					if (gene.getIntermediateMpTerm() != null) { geneLookup.get(d).INTERMEDIATE_MP_TERM.addAll(gene.getIntermediateMpTerm()); }
-					if (gene.getIntermediateMpSynonym() != null) { geneLookup.get(d).INTERMEDIATE_MP_TERM_SYNONYM.addAll(gene.getIntermediateMpSynonym()); }
-					if (gene.getChildMpId() != null) { geneLookup.get(d).CHILD_MP_ID.addAll(gene.getChildMpId()); }
-					if (gene.getChildMpTerm() != null) { geneLookup.get(d).CHILD_MP_TERM.addAll(gene.getChildMpTerm()); }
-					if (gene.getChildMpTermSynonym() != null) { geneLookup.get(d).CHILD_MP_TERM_SYNONYM.addAll(gene.getChildMpTermSynonym()); }
-					if (gene.getOntologySubset() != null) { geneLookup.get(d).ONTOLOGY_SUBSET.addAll(gene.getOntologySubset()); }
+					if (gene.getMgiAccessionId() != null) {
+						geneLookup.get(d).MGI_ACCESSION_ID.add(gene.getMgiAccessionId());
+					}
+					if (gene.getMarkerSymbol() != null) {
+						geneLookup.get(d).MARKER_SYMBOL.add(gene.getMarkerSymbol());
+					}
+					if (gene.getMarkerName() != null) {
+						geneLookup.get(d).MARKER_NAME.add(gene.getMarkerName());
+					}
+					if (gene.getMarkerSynonym() != null) {
+						geneLookup.get(d).MARKER_SYNONYM.addAll(gene.getMarkerSynonym());
+					}
+					if (gene.getMarkerType() != null) {
+						geneLookup.get(d).MARKER_TYPE.add(gene.getMarkerType());
+					}
+					if (gene.getHumanGeneSymbol() != null) {
+						geneLookup.get(d).HUMAN_GENE_SYMBOL.addAll(gene.getHumanGeneSymbol());
+					}
+					if (gene.getStatus() != null) {
+						geneLookup.get(d).STATUS.add(gene.getStatus());
+					}
+					if (gene.getLatestProductionCentre() != null) {
+						geneLookup.get(d).LATEST_PRODUCTION_CENTRE.addAll(gene.getLatestProductionCentre());
+					}
+					if (gene.getLatestPhenotypingCentre() != null) {
+						geneLookup.get(d).LATEST_PHENOTYPING_CENTRE.addAll(gene.getLatestPhenotypingCentre());
+					}
+					if (gene.getLatestPhenotypeStatus() != null) {
+						geneLookup.get(d).LATEST_PHENOTYPE_STATUS.add(gene.getLatestPhenotypeStatus());
+					}
+					if (gene.getLegacy_phenotype_status() != null) {
+						geneLookup.get(d).LEGACY_PHENOTYPE_STATUS.add(gene.getLegacy_phenotype_status());
+					}
+					if (gene.getAlleleName() != null) {
+						geneLookup.get(d).ALLELE_NAME.addAll(gene.getAlleleName());
+					}
+					if (gene.getMpId() != null) {
+						geneLookup.get(d).MP_ID.addAll(gene.getMpId());
+					}
+					if (gene.getMpTerm() != null) {
+						geneLookup.get(d).MP_TERM.addAll(gene.getMpTerm());
+					}
+					if (gene.getMpDefinition() != null) {
+						geneLookup.get(d).MP_DEFINITION.addAll(gene.getMpDefinition());
+					}
+					if (gene.getMpSynonym() != null) {
+						geneLookup.get(d).MP_SYNONYM.addAll(gene.getMpSynonym());
+					}
+					if (gene.getTopLevelMpId() != null) {
+						geneLookup.get(d).TOP_LEVEL_MP_ID.addAll(gene.getTopLevelMpId());
+					}
+					if (gene.getTopLevelMpTerm() != null) {
+						geneLookup.get(d).TOP_LEVEL_MP_TERM.addAll(gene.getTopLevelMpTerm());
+					}
+					if (gene.getTopLevelMpSynonym() != null) {
+						geneLookup.get(d).TOP_LEVEL_MP_TERM_SYNONYM.addAll(gene.getTopLevelMpSynonym());
+					}
+					if (gene.getIntermediateMpId() != null) {
+						geneLookup.get(d).INTERMEDIATE_MP_ID.addAll(gene.getIntermediateMpId());
+					}
+					if (gene.getIntermediateMpTerm() != null) {
+						geneLookup.get(d).INTERMEDIATE_MP_TERM.addAll(gene.getIntermediateMpTerm());
+					}
+					if (gene.getIntermediateMpSynonym() != null) {
+						geneLookup.get(d).INTERMEDIATE_MP_TERM_SYNONYM.addAll(gene.getIntermediateMpSynonym());
+					}
+					if (gene.getChildMpId() != null) {
+						geneLookup.get(d).CHILD_MP_ID.addAll(gene.getChildMpId());
+					}
+					if (gene.getChildMpTerm() != null) {
+						geneLookup.get(d).CHILD_MP_TERM.addAll(gene.getChildMpTerm());
+					}
+					if (gene.getChildMpTermSynonym() != null) {
+						geneLookup.get(d).CHILD_MP_TERM_SYNONYM.addAll(gene.getChildMpTermSynonym());
+					}
+					if (gene.getOntologySubset() != null) {
+						geneLookup.get(d).ONTOLOGY_SUBSET.addAll(gene.getOntologySubset());
+					}
 				}
 			}
 		}
-
-//		List<String> geneFields = Arrays.asList(
-//			GeneDTO.MGI_ACCESSION_ID,
-//			GeneDTO.MARKER_SYMBOL,
-//			GeneDTO.MARKER_NAME,
-//			GeneDTO.MARKER_SYNONYM,
-//			GeneDTO.MARKER_TYPE,
-//			GeneDTO.HUMAN_GENE_SYMBOL,
-//			GeneDTO.STATUS,
-//			GeneDTO.LATEST_PRODUCTION_CENTRE,
-//			GeneDTO.LATEST_PHENOTYPING_CENTRE,
-//			GeneDTO.LATEST_PHENOTYPE_STATUS,
-//			GeneDTO.LEGACY_PHENOTYPE_STATUS,
-//			GeneDTO.ALLELE_NAME,
-//			GeneDTO.MP_ID,
-//			GeneDTO.MP_TERM,
-//			GeneDTO.MP_DEFINITION,
-//			GeneDTO.MP_SYNONYM,
-//			GeneDTO.TOP_LEVEL_MP_ID,
-//			GeneDTO.TOP_LEVEL_MP_TERM,
-//			GeneDTO.TOP_LEVEL_MP_TERM_SYNONYM,
-//			GeneDTO.INTERMEDIATE_MP_ID,
-//			GeneDTO.INTERMEDIATE_MP_TERM,
-//			GeneDTO.INTERMEDIATE_MP_TERM_SYNONYM,
-//			GeneDTO.CHILD_MP_ID,
-//			GeneDTO.CHILD_MP_TERM,
-//			GeneDTO.CHILD_MP_TERM_SYNONYM,
-//			GeneDTO.ONTOLOGY_SUBSET);
-
 	}
 
 
@@ -328,43 +312,26 @@ public class DiseaseIndexer extends AbstractIndexer {
 		OptionSet options = parser.parse(args);
 		String context = (String) options.valuesOf("context").get(0);
 
-		logger.info("Using application context file {}", context);
-
-		// Wire up spring support for this application
-		DiseaseIndexer main = new DiseaseIndexer();
-
 		ApplicationContext applicationContext;
+		logger.info("Using application context file {}", context);
 
 		File f = new File(context);
 		if (f.exists() && !f.isDirectory()) {
-
 			try {
-
-				// Try context as a file resource
 				applicationContext = new FileSystemXmlApplicationContext("file:" + context);
-
 			} catch (RuntimeException e) {
-
-				logger.warn("An error occurred loading the file: {}", e.getMessage());
-
-				// Try context as a class path resource
+				logger.info("An error occurred loading the file: {}", e.getMessage());
 				applicationContext = new ClassPathXmlApplicationContext(context);
-
-				logger.warn("Using classpath app-config file: {}", context);
-
+				logger.info("Using classpath app-config file: {}", context);
 			}
-
 		} else {
-
-			// Try context as a class path resource
 			applicationContext = new ClassPathXmlApplicationContext(context);
-
-			logger.warn("Using classpath app-config file: {}", context);
-
+			logger.info("Using classpath app-config file: {}", context);
 		}
 
+		// Do the spring dependency injection on the main class and execute the run() method
+		DiseaseIndexer main = new DiseaseIndexer();
 		applicationContext.getAutowireCapableBeanFactory().autowireBeanProperties(main, AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE, true);
-
 		main.run();
 
 		logger.info("Process finished.  Exiting.");
@@ -378,6 +345,13 @@ public class DiseaseIndexer extends AbstractIndexer {
 	}
 
 
+	/*
+	 Internal class which collects the gene information per disease
+	 Uses sets to prevent duplicates since the frontend will only
+	 filter if a disease is associated to particular search facet
+	 dimensions (in other words we don't need to maintain the
+	 correlation between the sub gene information)
+	 */
 	private class GeneData {
 		Set<String> MGI_ACCESSION_ID = new HashSet<>();
 		Set<String> MARKER_SYMBOL = new HashSet<>();
