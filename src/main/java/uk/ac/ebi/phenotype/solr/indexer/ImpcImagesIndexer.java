a3c365f75f5b51b63feea77d4c67df863d417ace
jwgwarren <warren.jonathan@gmail.com>
2015-03-18 15:27:40 +0000

    exclued JAX ALZ images 386?

--- src/main/java/uk/ac/ebi/phenotype/solr/indexer/ImpcImagesIndexer.java
+++ src/main/java/uk/ac/ebi/phenotype/solr/indexer/ImpcImagesIndexer.java
@@ -129,9 +129,10 @@ public class ImpcImagesIndexer extends AbstractIndexer {
 					int omeroId = iBean.omeroId;
 					imageDTO.setOmeroId(omeroId);
 
-					if (omeroId == 0 || imageDTO.getProcedureStableId().equals(excludeProcedureStableId)) {
+					if (omeroId == 0 || imageDTO.getProcedureStableId().equals(excludeProcedureStableId) || (imageDTO.getParameterStableId().equals("IMPC_ALZ_075_001") && imageDTO.getPhenotypingCenter().equals("JAX"))) {
 						// Skip records that do not have an omero_id
-						logger.error("Skipping record for image record {} -- missing omero_id or excluded procedure", fullResFilePath);
+						System.out.println("skipping omeroId="+omeroId+"param and center"+imageDTO.getParameterStableId()+imageDTO.getPhenotypingCenter());
+						//logger.warn("Skipping record for image record {} -- missing omero_id or excluded procedure", fullResFilePath);
 						continue;
 					}
 
