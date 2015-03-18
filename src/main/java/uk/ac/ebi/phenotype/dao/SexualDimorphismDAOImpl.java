56556fa0c9a1e55d17c10e401d5ddfc2b156efe2
Jeremy Mason <jmason@ebi.ac.uk>
2015-03-18 10:24:02 +0000

    add classification tag to the sexdymorph reports

--- src/main/java/uk/ac/ebi/phenotype/dao/SexualDimorphismDAOImpl.java
+++ src/main/java/uk/ac/ebi/phenotype/dao/SexualDimorphismDAOImpl.java
@@ -1,87 +1,33 @@
 package uk.ac.ebi.phenotype.dao;
 
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.phenotype.chart.ChartUtils;
+import uk.ac.ebi.phenotype.pojo.ZygosityType;
+import uk.ac.ebi.phenotype.solr.indexer.StatisticalResultIndexer;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
-import org.hibernate.SessionFactory;
-import org.springframework.transaction.annotation.Transactional;
-
-import uk.ac.ebi.phenotype.chart.ChartUtils;
-import uk.ac.ebi.phenotype.pojo.ZygosityType;
-import uk.ac.ebi.phenotype.solr.indexer.StatisticalResultIndexer;
 
 
-public class SexualDimorphismDAOImpl  extends HibernateDAOImpl implements SexualDimorphismDAO {
-    	
+public class SexualDimorphismDAOImpl extends HibernateDAOImpl implements SexualDimorphismDAO {
+
 	public SexualDimorphismDAOImpl(SessionFactory sessionFactory) {
+
 		this.sessionFactory = sessionFactory;
 	}
 
+
 	@Transactional(readOnly = true)
 	public List<String[]> sexualDimorphismReportNoBodyWeight(String baseUrl) {
-		       
-        PreparedStatement statement = getSexualDimorphismReportQuery(false);
-        List<String[]> res = new ArrayList<>();
-        String[] temp = new String[1];
-        
-        try (Connection connection = getConnection()) {
-    			
-     		ResultSet results = statement.executeQuery();
-     		List<String> header = getSexualDimorphismHeader();
- 			
- 			res.add(header.toArray(temp));
-     		
-     		while (results.next()){
-     			List <String> row = new ArrayList<>();
-     			row.add(results.getString("gene_symbol"));
-     			row.add(results.getString("gene_acc"));
-     			row.add(results.getString("allele_symbol"));
-     			row.add(results.getString("allele_acc"));
-     			row.add(results.getString("experimental_zygosity"));
-     			row.add(results.getString("colony_id"));
-     			row.add(results.getString("parameter"));
-     			row.add(results.getString("dependent_variable"));
-     			row.add(results.getString("female_mutants"));
-     			row.add(results.getString("female_controls"));
-     			row.add(results.getString("male_mutants"));
-     			row.add(results.getString("male_controls"));
-     			row.add(results.getString("globalPValue"));
-     			row.add(results.getString("standardEffectSize"));
-				row.add(getEffectDifference(results.getString("standardEffectSize")).toString());
-     			row.add(results.getString("male_genotype_pvalue"));
-     			row.add(results.getString("male_genotype_estimate"));
-     			row.add(results.getString("male_genotype_stderr"));
-     			row.add(results.getString("female_genotype_pvalue"));
-     			row.add(results.getString("female_genotype_estimate"));
-     			row.add(results.getString("female_genotype_stderr"));
-     			String chartUrl = ChartUtils.getChartPageUrlPostQc(baseUrl, results.getString("gene_acc"), results.getString("allele_acc"),
-     				ZygosityType.valueOf(results.getString("experimental_zygosity")), results.getString("dependent_variable"), null, null);
-     			row.add(chartUrl);
-     			res.add(row.toArray(temp));
-     		}
-	        
-	    } catch(Exception e){
-	        	e.printStackTrace();
-	    }
-        return res;
-	}
 
-
-	@Override
-	@Transactional(readOnly = true)
-	public List<String[]> sexualDimorphismReportWithBodyWeight(String baseUrl) {
-
-		PreparedStatement statement = getSexualDimorphismReportQuery(true);
 		List<String[]> res = new ArrayList<>();
 		String[] temp = new String[1];
 
-		try (Connection connection = getConnection()) {
+		try (PreparedStatement statement = getSexualDimorphismReportQuery(false)) {
 
 			ResultSet results = statement.executeQuery();
 			List<String> header = getSexualDimorphismHeader();
@@ -102,6 +48,7 @@ public class SexualDimorphismDAOImpl  extends HibernateDAOImpl implements Sexual
 				row.add(results.getString("female_controls"));
 				row.add(results.getString("male_mutants"));
 				row.add(results.getString("male_controls"));
+				row.add(results.getString("classification_tag"));
 				row.add(results.getString("globalPValue"));
 				row.add(results.getString("standardEffectSize"));
 				row.add(getEffectDifference(results.getString("standardEffectSize")).toString());
@@ -111,8 +58,8 @@ public class SexualDimorphismDAOImpl  extends HibernateDAOImpl implements Sexual
 				row.add(results.getString("female_genotype_pvalue"));
 				row.add(results.getString("female_genotype_estimate"));
 				row.add(results.getString("female_genotype_stderr"));
-     			String chartUrl = ChartUtils.getChartPageUrlPostQc(baseUrl, results.getString("gene_acc"), results.getString("allele_acc"),
- 					ZygosityType.valueOf(results.getString("experimental_zygosity")), results.getString("dependent_variable"), null, null);
+				String chartUrl = ChartUtils.getChartPageUrlPostQc(baseUrl, results.getString("gene_acc"), results.getString("allele_acc"),
+					ZygosityType.valueOf(results.getString("experimental_zygosity")), results.getString("dependent_variable"), null, null);
 				row.add(chartUrl);
 				res.add(row.toArray(temp));
 			}
@@ -122,45 +69,99 @@ public class SexualDimorphismDAOImpl  extends HibernateDAOImpl implements Sexual
 		}
 		return res;
 	}
-	
-	
-	PreparedStatement getSexualDimorphismReportQuery(boolean withBodyWeight){
+
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<String[]> sexualDimorphismReportWithBodyWeight(String baseUrl) {
+
+		List<String[]> res = new ArrayList<>();
+		String[] temp = new String[1];
+
+		try (PreparedStatement statement = getSexualDimorphismReportQuery(true)) {
+
+			ResultSet results = statement.executeQuery();
+			List<String> header = getSexualDimorphismHeader();
+
+			res.add(header.toArray(temp));
+
+			while (results.next()) {
+				List<String> row = new ArrayList<>();
+				row.add(results.getString("gene_symbol"));
+				row.add(results.getString("gene_acc"));
+				row.add(results.getString("allele_symbol"));
+				row.add(results.getString("allele_acc"));
+				row.add(results.getString("experimental_zygosity"));
+				row.add(results.getString("colony_id"));
+				row.add(results.getString("parameter"));
+				row.add(results.getString("dependent_variable"));
+				row.add(results.getString("female_mutants"));
+				row.add(results.getString("female_controls"));
+				row.add(results.getString("male_mutants"));
+				row.add(results.getString("male_controls"));
+				row.add(results.getString("classification_tag"));
+				row.add(results.getString("globalPValue"));
+				row.add(results.getString("standardEffectSize"));
+				row.add(getEffectDifference(results.getString("standardEffectSize")).toString());
+				row.add(results.getString("male_genotype_pvalue"));
+				row.add(results.getString("male_genotype_estimate"));
+				row.add(results.getString("male_genotype_stderr"));
+				row.add(results.getString("female_genotype_pvalue"));
+				row.add(results.getString("female_genotype_estimate"));
+				row.add(results.getString("female_genotype_stderr"));
+				String chartUrl = ChartUtils.getChartPageUrlPostQc(baseUrl, results.getString("gene_acc"), results.getString("allele_acc"),
+					ZygosityType.valueOf(results.getString("experimental_zygosity")), results.getString("dependent_variable"), null, null);
+				row.add(chartUrl);
+				res.add(row.toArray(temp));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+
+	PreparedStatement getSexualDimorphismReportQuery(boolean withBodyWeight) {
+
 		String database;
 		PreparedStatement statement = null;
-		
-		if (withBodyWeight){
+
+		if (withBodyWeight) {
 			database = "logistic_regression.";
-		}else {
+		} else {
 			database = "";
 		}
-		
+
 		String command = "SELECT  gf.symbol as gene_symbol, gf.acc as gene_acc, allele_acc, allele.symbol as allele_symbol, "
-        + " experimental_zygosity, colony_id, allele_acc, pp.name as parameter, dependent_variable, female_mutants, female_controls, "
-        + " male_mutants, male_controls, null_test_significance as globalPValue, genotype_percentage_change as standardEffectSize, "
-        + " gender_male_ko_pvalue as male_genotype_pvalue, gender_male_ko_estimate as male_genotype_estimate, "
-        + " gender_male_ko_stderr_estimate male_genotype_stderr, gender_female_ko_pvalue as female_genotype_pvalue, "
-        + " gender_female_ko_estimate as female_genotype_estimate, gender_female_ko_stderr_estimate as female_genotype_stderr"
-        + " FROM " + database + "stats_unidimensional_results sur "
-        + " INNER JOIN " + database + "biological_model_allele bma ON bma.biological_model_id = sur.experimental_id"
-        + " INNER JOIN " + database + "biological_model_genomic_feature bmgf ON bmgf.biological_model_id = sur.experimental_id"
-        + " INNER JOIN " + database + "phenotype_parameter pp on pp.id = sur.parameter_id"
-        + " INNER JOIN " + database + "allele on allele.acc = bma.allele_acc"
-        + " INNER JOIN " + database + "genomic_feature gf on gf.acc = bmgf.gf_acc "
-        + " WHERE sur.status like \"SUCCESS\" AND classification_tag not in (\"Both genders equally\", \"No significant change\", \"If phenotype is significant - can not classify effect\", \"If phenotype is significant it is for the one sex tested\")"
-        + "	AND statistical_method not in (\"Wilcoxon rank sum test with continuity correction\") AND interaction_significance = 1 AND project_id not in (1,8)"
-        + " AND null_test_significance < 0.0001 LIMIT 100000;";
-		
+			+ " experimental_zygosity, colony_id, allele_acc, pp.name as parameter, dependent_variable, female_mutants, female_controls, "
+			+ " male_mutants, male_controls, null_test_significance as globalPValue, genotype_percentage_change as standardEffectSize, "
+			+ " gender_male_ko_pvalue as male_genotype_pvalue, gender_male_ko_estimate as male_genotype_estimate, "
+			+ " gender_male_ko_stderr_estimate male_genotype_stderr, gender_female_ko_pvalue as female_genotype_pvalue, "
+			+ " gender_female_ko_estimate as female_genotype_estimate, gender_female_ko_stderr_estimate as female_genotype_stderr, classification_tag "
+			+ " FROM " + database + "stats_unidimensional_results sur "
+			+ " INNER JOIN " + database + "biological_model_allele bma ON bma.biological_model_id = sur.experimental_id"
+			+ " INNER JOIN " + database + "biological_model_genomic_feature bmgf ON bmgf.biological_model_id = sur.experimental_id"
+			+ " INNER JOIN " + database + "phenotype_parameter pp on pp.id = sur.parameter_id"
+			+ " INNER JOIN " + database + "allele on allele.acc = bma.allele_acc"
+			+ " INNER JOIN " + database + "genomic_feature gf on gf.acc = bmgf.gf_acc "
+			+ " WHERE sur.status like \"SUCCESS\" AND classification_tag not in (\"Both genders equally\", \"No significant change\", \"If phenotype is significant - can not classify effect\", \"If phenotype is significant it is for the one sex tested\")"
+			+ "	AND statistical_method not in (\"Wilcoxon rank sum test with continuity correction\") AND interaction_significance = 1 AND project_id not in (1,8)"
+			+ " AND null_test_significance < 0.0001 LIMIT 100000;";
+
 		try (Connection connection = getConnection()) {
 			statement = connection.prepareStatement(command);
-		} catch(Exception e){
-	        	e.printStackTrace();
-	    }
-		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return statement;
 	}
-	
-	List <String> getSexualDimorphismHeader(){
-		List <String> header = new ArrayList<>();
+
+
+	List<String> getSexualDimorphismHeader() {
+
+		List<String> header = new ArrayList<>();
 		header.add("gene_symbol");
 		header.add("gene_acc");
 		header.add("allele_symbol");
@@ -173,6 +174,7 @@ public class SexualDimorphismDAOImpl  extends HibernateDAOImpl implements Sexual
 		header.add("female_contols");
 		header.add("male_mutants");
 		header.add("male_contols");
+		header.add("classification");
 		header.add("globalPValue");
 		header.add("standardEffectSize");
 		header.add("effect_percentage_difference");
@@ -185,12 +187,13 @@ public class SexualDimorphismDAOImpl  extends HibernateDAOImpl implements Sexual
 		header.add("graph");
 		return header;
 	}
-	
 
-	public Double getEffectDifference(String field){
+
+	public Double getEffectDifference(String field) {
+
 		Double female = StatisticalResultIndexer.getFemalePercentageChange(field);
 		Double male = StatisticalResultIndexer.getMalePercentageChange(field);
-		return Math.abs(female-male);
+		return Math.abs(female - male);
 	}
-	
+
 }
