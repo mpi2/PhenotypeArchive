6fa5d2addd457d05719f14a80ab4837fdaa8467d
Jeremy Mason <jmason@ebi.ac.uk>
2015-04-01 16:50:28 +0100

    fix the utilities test file t o incorporate the new non-static access of the utilities class

--- src/test/java/uk/ac/ebi/phenotype/data/impress/UtilitiesTest.java
+++ src/test/java/uk/ac/ebi/phenotype/data/impress/UtilitiesTest.java
@@ -1,9 +1,5 @@
 package uk.ac.ebi.phenotype.data.impress;
 
-import static org.junit.Assert.fail;
-
-import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +7,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-
 import uk.ac.ebi.phenotype.dao.PhenotypePipelineDAO;
 import uk.ac.ebi.phenotype.pojo.ObservationType;
 import uk.ac.ebi.phenotype.pojo.Parameter;
@@ -24,13 +19,16 @@ public class UtilitiesTest {
 
     @Autowired
     PhenotypePipelineDAO pDAO;
+
+    @Autowired
+    Utilities impressUtilities;
     
 
     @Test
     public void testCheckTypeParameterString() {
         Parameter p = pDAO.getParameterByStableId("ESLIM_003_001_006");
         String value= "2.092";
-        ObservationType oType = Utilities.checkType(p, value);
+        ObservationType oType = impressUtilities.checkType(p, value);
    
         System.out.println(oType);
         assert(oType.equals(ObservationType.time_series));
