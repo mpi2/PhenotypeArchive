b283c8adb3297edfe9cb783becaa3b7729aeeb6a
Jeremy Mason <jmason@ebi.ac.uk>
2015-04-07 16:22:35 +0100

    add method to get developmental term from stage/stageUnit impress embryo parameters

--- src/test/java/uk/ac/ebi/phenotype/data/impress/UtilitiesTest.java
+++ src/test/java/uk/ac/ebi/phenotype/data/impress/UtilitiesTest.java
@@ -7,9 +7,16 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.phenotype.dao.OntologyTermDAO;
 import uk.ac.ebi.phenotype.dao.PhenotypePipelineDAO;
 import uk.ac.ebi.phenotype.pojo.ObservationType;
+import uk.ac.ebi.phenotype.pojo.OntologyTerm;
 import uk.ac.ebi.phenotype.pojo.Parameter;
+import uk.ac.ebi.phenotype.pojo.StageUnitType;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:test-config.xml" })
@@ -19,6 +26,9 @@ public class UtilitiesTest {
 
     @Autowired
     PhenotypePipelineDAO pDAO;
+
+    @Autowired
+    OntologyTermDAO ontologyTermDAO;
 
     @Autowired
     Utilities impressUtilities;
@@ -32,6 +42,46 @@ public class UtilitiesTest {
    
         System.out.println(oType);
         assert(oType.equals(ObservationType.time_series));
+    }
+
+
+    @Test
+    public void testCheckStageConversion() throws Exception {
+
+        OntologyTerm embryoStage = ontologyTermDAO.getOntologyTermByAccession("EFO:0001367");
+        OntologyTerm adultStage = ontologyTermDAO.getOntologyTermByAccession("EFO:0002948");
+
+        List<String> goodStages = Arrays.asList("9.5", "12.5", "20");
+        List<StageUnitType> goodStageUnits = Arrays.asList(StageUnitType.DPC, StageUnitType.DPC, StageUnitType.THEILER);
+        List<OntologyTerm> goodTerms = Arrays.asList( embryoStage, embryoStage, embryoStage);
+
+        List<String> badStages = Arrays.asList( "9.5", "a", "20");
+        List<StageUnitType> badStageUnits = Arrays.asList(StageUnitType.THEILER, StageUnitType.THEILER, StageUnitType.DPC);
+        List<OntologyTerm> badTerms = Arrays.asList( embryoStage, embryoStage, embryoStage);
+
+        for (int i = 0; i<goodStages.size(); i++) {
+            String stage = goodStages.get(i);
+            StageUnitType stageUnit = goodStageUnits.get(i);
+
+            System.out.println("Testing :" + stage + " " + stageUnit.getStageUnitName());
+
+            // Need a method to convert impress input to represnetative EFO term
+            OntologyTerm term = impressUtilities.getStageTerm(stage, stageUnit);
+            org.junit.Assert.assertTrue(term.equals(goodTerms.get(i)));
+        }
+
+        for (int i = 0; i<badStages.size(); i++) {
+            String stage = badStages.get(i);
+            StageUnitType stageUnit = badStageUnits.get(i);
+
+            System.out.println("Testing bad case:" + stage + " " + stageUnit.getStageUnitName());
+
+            // Need a method to convert impress input to represnetative EFO term
+            OntologyTerm term = impressUtilities.getStageTerm(stage, stageUnit);
+            org.junit.Assert.assertTrue(term==null);
+        }
+
+
     }
 
 }
