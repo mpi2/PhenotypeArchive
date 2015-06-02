3b1dba1bf0871c9fa470dfb7bb3b68e030f1349d
jwgwarren <warren.jonathan@gmail.com>
2015-06-02 10:15:20 +0100

    excluded gross path for live

--- src/main/java/uk/ac/ebi/phenotype/solr/indexer/ImpcImagesIndexer.java
+++ src/main/java/uk/ac/ebi/phenotype/solr/indexer/ImpcImagesIndexer.java
@@ -64,7 +64,7 @@ public class ImpcImagesIndexer extends AbstractIndexer {
 
 	private Map<String, List<AlleleDTO>> alleles;
 	private Map<String, ImageBean> imageBeans;
-	String excludeProcedureStableId="";
+	String excludeProcedureStableId="IMPC_PAT_002";
 
 	private Map<String, String> parameterStableIdToMaTermIdMap;
 
